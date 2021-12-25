86e9c073e5939741009acec7d33eaba61a37bd5b
kohane27 <charuphoniu4173@gmail.com>
2021-12-25 17:19:23 +0800

    chore: clean-up

---
+++ lua/user/floaterm.lua
@@ -0,0 +1,30 @@
+local status_ok, floaterm = pcall(require, "floaterm")
+if not status_ok then
+	return
+end
+
+vim.api.nvim_exec(
+[[
+let g:floaterm_gitcommit='floaterm'
+let g:floaterm_width=0.9
+let g:floaterm_height=0.9
+let g:floaterm_wintitle=0
+let g:floaterm_wintype='float'
+let g:floaterm_position='center'
+let g:floaterm_autoclose=1
+let g:floaterm_autoinsert=1
+let g:floaterm_opener='tabe'
+
+" https://github.com/voldikss/vim-floaterm/issues/140
+"tnoremap <M-h> <C-\><C-n><C-w>h
+"tnoremap <M-j> <C-\><C-n><C-w>j
+"tnoremap <M-k> <C-\><C-n><C-w>k
+"tnoremap <M-l> <C-\><C-n><C-w>l
+
+"augroup FloatermCustomisations
+"    autocmd!
+"    autocmd ColorScheme * highlight FloatermBorder guibg=none
+"augroup END
+"
+]],
+true)
