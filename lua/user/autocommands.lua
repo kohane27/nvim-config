38c55789809e1f51fdadccd4ed951e18a739fbe4
kohane27 <kohane27@protonmail.com>
2021-12-29 10:42:27 +0800

    chore: clean-up

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -6,12 +6,6 @@ vim.cmd [[
     autocmd FileType qf set nobuflisted
   augroup end
 
-  augroup _git
-    autocmd!
-    autocmd FileType gitcommit setlocal wrap
-    autocmd FileType gitcommit setlocal spell
-  augroup end
-
   augroup _auto_resize
     autocmd!
     autocmd VimResized * tabdo wincmd = 
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -117,7 +117,7 @@ return packer.startup(function(use)
   -- use "rmagatti/auto-session"
   use "jremmen/vim-ripgrep"
   use "farmergreg/vim-lastplace"
-  use "Pocco81/AutoSave.nvim"
+  -- use "Pocco81/AutoSave.nvim"
   use "Shougo/echodoc.vim"
   use "mbbill/undotree"
   use "vimwiki/vimwiki"
