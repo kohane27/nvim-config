e2adbf0d282af17480f9f1e298c7d63141ebf801
kohane27 <kohane27@protonmail.com>
2022-01-26 22:47:05 +0800

    chore: clean-up

--- lua/user/lightspeed.lua
+++ lua/user/lightspeed.lua
@@ -3,14 +3,14 @@ if not status_ok then
 	return
 end
 
-lightspeed.setup {
-  exit_after_idle_msecs = { labeled = nil, unlabeled = 10000 },
-  -- s/x
-  grey_out_search_area = true,
-  highlight_unique_chars = true,
-  match_only_the_start_of_same_char_seqs = true,
-  jump_on_partial_input_safety_timeout = 10000,
-  -- f/t
-  limit_ft_matches = 8,
-  repeat_ft_with_target_char = false,
-}
+--lightspeed.setup {
+--  exit_after_idle_msecs = { labeled = nil, unlabeled = 10000 },
+--  -- s/x
+--  grey_out_search_area = true,
+--  highlight_unique_chars = true,
+--  match_only_the_start_of_same_char_seqs = true,
+--  jump_on_partial_input_safety_timeout = 10000,
+--  -- f/t
+--  limit_ft_matches = 8,
+--  repeat_ft_with_target_char = false,
+--}
--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -11,14 +11,9 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = true,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+        formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
     -- diagnostics.flake8
 	},
-    on_attach = function(client)
-        if client.resolved_capabilities.document_formatting then
-            vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-        end
-    end,
 })
