6e1fdb09a62972737ba278f8de9b80fdc1c8c411
kohane27 <charuphoniu4173@gmail.com>
2022-04-13 16:29:05 +0800

    chore: clean-up

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -82,8 +82,8 @@ xnoremap K 5k
 "nnoremap <A-k> :m .-2<CR>==
 "inoremap <A-j> <Esc>:m .+1<CR>==gi
 "inoremap <A-k> <Esc>:m .-2<CR>==gi
-"vnoremap <A-j> :m '>+1<CR>gv=gv
-"vnoremap <A-k> :m '<-2<CR>gv=gv
+vnoremap <C-j> :m '>+1<CR>gv=gv
+vnoremap <C-k> :m '<-2<CR>gv=gv
 
 nnoremap <A-c> : Bdelete<cr>
 
