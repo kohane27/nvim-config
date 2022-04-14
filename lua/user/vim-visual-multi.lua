23da19666626b82ce8e078893052d5c094038e73
kohane27 <kohane27@protonmail.com>
2022-04-14 09:10:20 +0800

    feat: use vim.cmd, don't include pcall in lua file
    Former-commit-id: 8ddd6f9be8b35ea98eb88c5ff67ea36bd97977ab
    Former-commit-id: 40bc0db525dfef481e40de874d98aacda55c52c2

--- lua/user/vim-visual-multi.lua
+++ lua/user/vim-visual-multi.lua
@@ -2,5 +2,5 @@ vim.cmd([[
     let g:VM_default_mappings = 0
     " To change any mapping you must first initialize the variable:
     let g:VM_maps = {}
-    let g:VM_maps['Find Under']         = '<leader>n'
+    " let g:VM_maps['Find Under']         = '<leader>n'
 ]])
