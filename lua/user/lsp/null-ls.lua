2a65301ebb305f95cd01353babaa5ca1cdaf30c4
kohane27 <charuphoniu4173@gmail.com>
2022-04-28 16:55:07 +0800

    feat: use shellcheck via null-ls

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -3,15 +3,26 @@ if not null_ls_status_ok then
 	return
 end
 
+local nls = require "null-ls"
+local nls_utils = require "null-ls.utils"
+local b = nls.builtins
+
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+local with_diagnostics_code = function(builtin)
+	return builtin.with({
+		diagnostics_format = "#{m} [#{c}]",
+	})
+end
 
 local sources = {
 	formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote" } }),
 	formatting.black.with({ extra_args = { "--fast" } }),
 	formatting.stylua,
+	null_ls.builtins.code_actions.shellcheck,
+	with_diagnostics_code(b.diagnostics.shellcheck),
 }
 
 null_ls.setup({
