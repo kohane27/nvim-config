cc001a1c69c7287dad59523e4bbee82120457449
kohane27 <kohane27@protonmail.com>
2022-05-02 11:28:41 +0800

    chore: clean-up
    Former-commit-id: 392ac189fa71f0f9b58b55f70a908d4e8e874a4b
    Former-commit-id: 9d479de03958882576c270925a84b5c839ea642c

--- lua/user/sandwitch.lua
+++ lua/user/sandwitch.lua
@@ -3,18 +3,21 @@ vim.cmd([[
     "disable Filetype tex
     let g:sandwich_no_tex_ftplugin = 1
 
-    " Text objects to select a text surrounded 
-    " by brackets or user-specified characters
-    xmap is <Plug>(textobj-sandwich-query-i)
-    xmap as <Plug>(textobj-sandwich-query-a)
-    omap is <Plug>(textobj-sandwich-query-i)
-    omap as <Plug>(textobj-sandwich-query-a)
-
     " Text objects to select 
     " the nearest surrounded text automatically
     xmap iss <Plug>(textobj-sandwich-auto-i)
     xmap ass <Plug>(textobj-sandwich-auto-a)
     omap iss <Plug>(textobj-sandwich-auto-i)
     omap ass <Plug>(textobj-sandwich-auto-a)
-]])
 
+    " Text objects to select a text surrounded 
+    " by brackets or user-specified characters
+    " das doesn't work
+    " xmap is <Plug>(textobj-sandwich-query-i)
+    " xmap as <Plug>(textobj-sandwich-query-a)
+    " omap is <Plug>(textobj-sandwich-query-i)
+    " omap as <Plug>(textobj-sandwich-query-a)
+
+    " remap to override again
+    xmap S <Plug>Lightspeed_S
+]])
