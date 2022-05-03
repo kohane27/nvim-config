ffe668590fe24995623c237dc8069ef77323e869
kohane27 <kohane27@protonmail.com>
2022-05-03 13:12:48 +0800

    fix: lua conflict language server
    Former-commit-id: ee9fde282dd93520ecef33ac276986efb5a347a7
    Former-commit-id: f7548ca2d4cfbb9391cf0e4ecd4b2bde7d344593

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	return
+    return
 end
 
 local nls_utils = require("null-ls.utils")
@@ -12,56 +12,56 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 local with_diagnostics_code = function(builtin)
-	return builtin.with({
-		diagnostics_format = "#{m} [#{c}]",
-	})
+    return builtin.with({
+        diagnostics_format = "#{m} [#{c}]",
+    })
 end
 
 local with_root_file = function(builtin, file)
-	return builtin.with({
-		condition = function(utils)
-			return utils.root_has_file(file)
-		end,
-	})
+    return builtin.with({
+        condition = function(utils)
+            return utils.root_has_file(file)
+        end,
+    })
 end
 
 local sources = {
 
-	-- formatting
-	formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote" } }),
-	formatting.black.with({ extra_args = { "--fast" } }),
-	formatting.stylua,
-	builtins.formatting.shfmt,
-	builtins.formatting.isort,
-	with_root_file(builtins.formatting.stylua, "stylua.toml"),
+    -- formatting
+    formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote" } }),
+    formatting.black.with({ extra_args = { "--fast" } }),
+    -- formatting.stylua,
+    builtins.formatting.shfmt,
+    builtins.formatting.isort,
+    with_root_file(builtins.formatting.stylua, "stylua.toml"),
 
-	-- diagnostics
-	-- builtins.diagnostics.write_good,
-	-- builtins.diagnostics.markdownlint,
-	-- builtins.diagnostics.eslint_d,
-	-- builtins.diagnostics.flake8,
-	-- builtins.diagnostics.tsc,
-	with_diagnostics_code(builtins.diagnostics.shellcheck),
-	-- with_root_file(builtins.diagnostics.selene, "selene.toml"),
+    -- diagnostics
+    -- builtins.diagnostics.write_good,
+    -- builtins.diagnostics.markdownlint,
+    -- builtins.diagnostics.eslint_d,
+    -- builtins.diagnostics.flake8,
+    -- builtins.diagnostics.tsc,
+    with_diagnostics_code(builtins.diagnostics.shellcheck),
+    -- with_root_file(builtins.diagnostics.selene, "selene.toml"),
 
-	-- code actions
-	builtins.code_actions.gitsigns,
-	builtins.code_actions.gitrebase,
+    -- code actions
+    builtins.code_actions.gitsigns,
+    builtins.code_actions.gitrebase,
 }
 
 null_ls.setup({
-	debug = false,
-	sources = sources,
-	root_dir = nls_utils.root_pattern(".git"),
-	-- format on save
-	on_attach = function(client)
-		-- if client.resolved_capabilities.document_formatting then
-		--     vim.cmd([[
-		--     augroup LspFormatting
-		--         autocmd! * <buffer>
-		--         autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-		--     augroup END
-		--     ]])
-		-- end
-	end,
+    debug = false,
+    sources = sources,
+    root_dir = nls_utils.root_pattern(".git"),
+    -- format on save
+    on_attach = function(client)
+        -- if client.resolved_capabilities.document_formatting then
+        --     vim.cmd([[
+        --     augroup LspFormatting
+        --         autocmd! * <buffer>
+        --         autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+        --     augroup END
+        --     ]])
+        -- end
+    end,
 })
