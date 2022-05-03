3794f8045447d6601f7c9d338b8b727870a4f311
kohane27 <kohane27@protonmail.com>
2022-05-03 15:02:13 +0800

    chore: clean-up
    Former-commit-id: 5af8f824613aea661733cbe860524c471747e8c5
    Former-commit-id: 207a6704e1f656b2d774adf3f30b28c29cab1d3e

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,11 +1,11 @@
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = '*',
 })
 
 vim.cmd([[
@@ -111,4 +111,17 @@ set laststatus=3
 
 " Uncomment to prevent non-normal modes showing in powerline and below powerline.
 set noshowmode
+
+if has("persistent_undo")
+   let target_path = expand('~/.undodir')
+
+    " create the directory and any parent directories
+    " if the location does not exist.
+    if !isdirectory(target_path)
+        call mkdir(target_path, "p", 0700)
+    endif
+
+    let &undodir=target_path
+    set undofile
+endif
 ]])
