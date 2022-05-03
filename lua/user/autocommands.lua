48f6aa8cea9dff7bd215b994fed240b6bfcc9582
kohane27 <charuphoniu4173@gmail.com>
2022-05-03 11:47:59 +0800

    chore: clean-up

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -15,7 +15,7 @@ vim.cmd([[
 " from https://github.com/kyazdani42/nvim-tree.lua#tips--reminders
 autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
 
-" format on save except c for qmk
+" format on save except c and markdown
 " autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
 let ftToIgnore = ['c', 'markdown']
     autocmd BufWritePre * if index(ftToIgnore, &ft) < 0 | lua vim.lsp.buf.formatting_sync()
