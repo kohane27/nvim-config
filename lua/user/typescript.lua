d03910a560a095f1d24720057785958277b202cc
kohane27 <charuphoniu4173@gmail.com>
2022-05-05 13:43:09 +0800

    feat: use typescript.lua

---
+++ lua/user/typescript.lua
@@ -0,0 +1,10 @@
+local status_ok, typescript = pcall(require, "typescript")
+if not status_ok then
+	print("typescript failing")
+end
+
+typescript.setup({
+    disable_commands = false, -- prevent the plugin from creating Vim commands
+    disable_formatting = false, -- disable tsserver's formatting capabilities
+    debug = false, -- enable debug logging for commands
+})
