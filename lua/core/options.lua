cd66b1ff0e08da6d597006ef00f39ee3eeeee8ca
kohane27 <charuphoniu4173@gmail.com>
2022-05-08 16:45:05 +0800

    chore: clean-up options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,10 +1,9 @@
 -----------------------------------------------------------
 -- General Neovim settings and configuration
------------------------------------------------------------
-
 -- Default options are not included
 -- See: https://neovim.io/doc/user/vim_diff.html
 -- [2] Defaults - *nvim-defaults*
+-----------------------------------------------------------
 
 local g = vim.g       -- Global variables
 local opt = vim.opt   -- Set options (global/buffer/windows-scoped)
@@ -21,13 +20,21 @@ opt.completeopt = 'menuone,noinsert,noselect'  -- Autocomplete options
 -- Neovim UI
 -----------------------------------------------------------
 opt.number = true           -- Show line number
+opt.relativenumber = true   -- Show relative line number
 opt.showmatch = true        -- Highlight matching parenthesis
 opt.foldmethod = 'marker'   -- Enable folding (default 'foldmarker')
+
+-- let g:markdown_folding = 0
+-- set foldlevelstart=999
+-- " folding using treesitter
+-- set foldmethod=expr
+-- set foldexpr=nvim_treesitter#foldexpr()
+
 opt.colorcolumn = '80'      -- Line lenght marker at 80 columns
 opt.splitright = true       -- Vertical split to the right
 opt.splitbelow = true       -- Horizontal split to the bottom
 opt.ignorecase = true       -- Ignore case letters when search
-opt.smartcase = true        -- Ignore lowercase for the whole pattern
+opt.smartcase = false        -- Ignore lowercase for the whole pattern
 opt.linebreak = true        -- Wrap on word boundary
 opt.termguicolors = true    -- Enable 24-bit RGB colors
 opt.laststatus=3            -- Set global statusline
@@ -44,7 +51,6 @@ opt.smartindent = true      -- Autoindent new lines
 -- Memory, CPU
 -----------------------------------------------------------
 opt.hidden = true           -- Enable background buffers
-opt.history = 100           -- Remember N lines in history
 opt.lazyredraw = true       -- Faster scrolling
 opt.synmaxcol = 240         -- Max column for syntax highlight
 opt.updatetime = 700        -- ms to wait for trigger an event
@@ -81,3 +87,20 @@ for _, plugin in pairs(disabled_built_ins) do
   g["loaded_" .. plugin] = 1
 end
 
+
+vim.cmd([[
+
+if has("persistent_undo")
+   let target_path = expand('~/.undodir')
+
+    " create the directory and any parent directories
+    " if the location does not exist.
+    if !isdirectory(target_path)
+        call mkdir(target_path, "p", 0700)
+    endif
+
+    let &undodir=target_path
+    set undofile
+endif
+
+]])
