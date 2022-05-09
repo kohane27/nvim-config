855b12b32dbca9a3ded87b264fa61824b401444b
kohane27 <kohane27@protonmail.com>
2022-05-09 19:51:37 +0800

    chore: lsp_format
    Former-commit-id: 7c36b33a0aa69e18c8a2b0e749c0f23e05bee53a
    Former-commit-id: 3df07538a3d0fd7ad4dd4f74cc577466e29e4ee7

---
+++ lua/lsp/lsp_format.lua
@@ -0,0 +1,17 @@
+local lsp_status_ok, lspformat = pcall(require, 'lsp-format')
+if not lsp_status_ok then
+  print("lsp_format failing")
+end
+
+lspformat.setup {
+  markdown = {
+    exclude = { "null-ls" }
+  },
+  c = {
+    exclude = { "null-ls" }
+  }
+}
+
+-- below code is in lsp_config.lua
+-- local on_attach = function(client)
+--     require "lsp-format".on_attach(client)
