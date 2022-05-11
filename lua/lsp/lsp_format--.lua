ae3bab8ceaf1ee4cd973eee11d557cce9926f77f
kohane27 <kohane27@protonmail.com>
2022-05-11 15:06:54 +0800

    fix: formatting issues
    Former-commit-id: f6799fd70a6487f6e61f9b7178138e1e79ac53e4
    Former-commit-id: 3f568accef5b82ceec88e29d025f462c30b0625c

--- lua/lsp/lsp_format--.lua
+++ lua/lsp/lsp_format--.lua
@@ -6,26 +6,20 @@ end
 -- BUG: saving removes my changes
 -- on save reverted changes if LSP is not yet attached
 
-lspformat.setup {
-  sync = true,
-  -- markdown = {
-  --   exclude = { "null-ls" }
-  -- },
-  -- lua = {
-  --   sync = true,
-  -- },
-  -- c = {
-  --   exclude = { "null-ls" }
-  -- }
-}
+-- lspformat.setup {
+--   sync = true,
+--   -- markdown = {
+--   --   exclude = { "null-ls" }
+--   -- },
+--   -- lua = {
+--   --   sync = true,
+--   -- },
+--   -- c = {
+--   --   exclude = { "null-ls" }
+--   -- }
+-- }
 
 -- below code is in lsp_config.lua
 -- local on_attach = function(client)
 --     require "lsp-format".on_attach(client)
 
-
--- vim.cmd([[
--- " format on save except the following
--- let ftToIgnore = ['c', 'markdown']
--- autocmd BufWritePre * if index(ftToIgnore, &ft) < 0 | lua vim.lsp.buf.formatting_sync()
--- ]])
