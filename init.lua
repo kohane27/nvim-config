fd2d59b19b664079583378c5189d16363fdd48d3
kohane27 <charuphoniu4173@gmail.com>
2022-05-27 11:17:46 +0800

    chore: clean-up

--- init.lua
+++ init.lua
@@ -57,6 +57,7 @@ require("plugins/registers")
 -- require("plugins.nvim-bqf")
 require("plugins.cutlass")
 require("plugins.open-browser")
+require("plugins.vim-markdown")
 
 -- require("plugins.yanky") -- BUG freezes nvim
 require("plugins.yoink") -- BUG THIS one too
