04465e722a612ae502f2aec932a80266c9506dde
kohane27 <kohane27@protonmail.com>
2022-06-13 11:31:40 +0800

    feat: remap of delete backwards till whitespace
    Former-commit-id: 2a5b3ff34a4fd674d946a29c8a9831b97f82e657
    Former-commit-id: d38111c423e6d7dc66f2c7154c1d5dfd3d2866e6

--- lua/plugins/sniprun.lua
+++ lua/plugins/sniprun.lua
@@ -3,28 +3,29 @@ if not status_ok then
   return
 end
 
--- stylua: ignore start
-
 vim.api.nvim_set_keymap("v", "<leader>rs", "<cmd>SnipRun<CR>", { noremap = true, silent = true })
 
 -- vim.cmd([[
--- vnoremap <leader>rs :<Plug>SnipRun<CR>
+-- vnoremap <leader>rs <Plug>SnipRun
 -- ]])
+
+-- stylua: ignore start
 
 sniprun.setup({
   display = {
-    "VirtualTextOk",              --# display ok results as virtual text
-    "VirtualTextErr",             --# display error results as virtual text
-    -- "Classic",                    --# display results in the command-line  area
+    "Classic",                    --# display results in the command-line  area
+    -- "VirtualTextOk",              --# display ok results as virtual text
+    -- "VirtualTextErr",             --# display error results as virtual text
     -- "TerminalWithCode",        --# display results and code history in a vertical split
   },
 
   --# customize highlight groups (setting this overrides colorscheme)
-  snipruncolors = {
-    SniprunVirtualTextOk   =  {bg="#5ad5e0",fg="#000000",ctermbg="Cyan",cterfg="Black"},
-    SniprunFloatingWinOk   =  {fg="#5ad5e0",ctermfg="Cyan"},
-    SniprunVirtualTextErr  =  {bg="#7d1b1b",fg="#000000",ctermbg="DarkRed",cterfg="Black"},
-    SniprunFloatingWinErr  =  {fg="#7d1b1b",ctermfg="DarkRed"},
-  },
+  -- snipruncolors = {
+  --   SniprunVirtualTextOk   =  {bg="#66eeff",fg="#000000",ctermbg="Cyan",cterfg="Black"},
+  --   SniprunFloatingWinOk   =  {fg="#66eeff",ctermfg="Cyan"},
+  --   SniprunVirtualTextErr  =  {bg="#881515",fg="#000000",ctermbg="DarkRed",cterfg="Black"},
+  --   SniprunFloatingWinErr  =  {fg="#881515",ctermfg="DarkRed"},
+  -- },
 })
--- stylua: ignore end
+
+-- stylua: ignore start
