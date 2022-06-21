6483ef2ae5cbbfa321726ea1d870982f2e9e78ae
kohane27 <kohane27@protonmail.com>
2022-06-21 13:11:36 +0800

    chore: get rid of yanky.nvim
    Former-commit-id: 6343fd75bb2c1df21a4de05a26880fac5f71462f
    Former-commit-id: 92ac6935534f0b8b812eae456793833d55c23a51

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -36,13 +36,13 @@ local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
 local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 
 -- Highlight on yank
--- augroup('YankHighlight', { clear = true })
--- autocmd('TextYankPost', {
---   group = 'YankHighlight',
---   callback = function()
---     vim.highlight.on_yank({ higroup = 'IncSearch', timeout = '300' })
---   end
--- })
+augroup("YankHighlight", { clear = true })
+autocmd("TextYankPost", {
+  group = "YankHighlight",
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = "300" })
+  end,
+})
 
 -- Enable spellchecking in text and gitcommit files
 autocmd("FileType", {
