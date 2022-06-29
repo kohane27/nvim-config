e506fc8eb062d0f27009960dbbecac986654c35c
kohane27 <kohane27@protonmail.com>
2022-06-29 19:33:24 +0800

    chore: whichkey for `fzf-lua`
    Former-commit-id: 3072ab114f57e797230a81551f80fadb860cc247
    Former-commit-id: 72039d81fb3295fad1253850ed9553be8a8269e6

--- lua/plugins/vim-visual-multi.lua
+++ lua/plugins/vim-visual-multi.lua
@@ -1,12 +1,19 @@
+-- vim-visual-*m*ulti remap
+-- create a column of cursors from visual mode
+vim.api.nvim_set_keymap(
+  "n",
+  "<A-m>",
+  ":call vm#commands#add_cursor_down(0, v:count1)<cr>",
+  { noremap = true, silent = true }
+)
+
 vim.cmd([[
     " all mappings disabled except <C-n>
     let g:VM_default_mappings = 0
     " To change any mapping you must first initialize the variable:
     let g:VM_maps = {}
-    " let g:VM_maps['Add Cursor Down']    = '<C-Down>'
+    let g:VM_maps['Find Under'] = "<leader>n" 
 
-" create a column of cursors from visual mode
-" map("n", "<A-m>", ":call vm#commands#add_cursor_down(0, v:count1)<cr>")
 
 " exit VM with <C-C>
 fun! VM_Start()
