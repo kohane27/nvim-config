ca198e4ef31f816683b85edecd189356c48c25b9
kohane27 <kohane27@protonmail.com>
2022-06-29 15:25:05 +0800

    chore: clean up ignored files
    Former-commit-id: 094e37fd6df96d70cffeb43b475a4eb49f2faa6d
    Former-commit-id: 300ac0b6d3b3fe93cace58151aa5738db9a27c8c

--- lua/plugins/old/surround--.lua
+++
@@ -1,9 +0,0 @@
--- make it compatible with lightspeed
-vim.cmd([[
-let g:surround_no_mappings = 1
-nmap ds       <Plug>Dsurround
-nmap cs       <Plug>Csurround
-nmap ys       <Plug>Ysurround
-nmap yss      <Plug>Yssurround
-xmap gs       <Plug>VSurround
-]])
--- lua/plugins/old/vim-auto-save--.lua
+++
@@ -1,5 +0,0 @@
-vim.cmd([[
-let g:auto_save        = 1
-let g:auto_save_silent = 1
-let g:auto_save_events = ["InsertLeave", "TextChanged"]
-]])
