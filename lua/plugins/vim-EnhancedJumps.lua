3a9b6ac90fe712b12277db59d8c75ab088064c9e
kohane27 <charuphoniu4173@gmail.com>
2022-07-01 20:46:41 +0800

    feat(EnhancedJumps): functions

--- lua/plugins/vim-EnhancedJumps.lua
+++ lua/plugins/vim-EnhancedJumps.lua
@@ -5,14 +5,30 @@ let g:EnhancedJumps_CaptureJumpMessages = 0
 let g:EnhancedJumps_no_mappings = 1
 
 " only local jumps
-nnoremap <C-o>          <Plug>EnhancedJumpsLocalOlder
-nnoremap <C-i>          <Plug>EnhancedJumpsLocalNewer
+" nnoremap <C-o>          <Plug>EnhancedJumpsLocalOlder
+" nnoremap <C-i>          <Plug>EnhancedJumpsLocalNewer
+nnoremap <silent> <C-o> :call EnhancedJumpsLocalOlder()<cr>
+nnoremap <silent> <C-i> :call EnhancedJumpsLocalNewer()<cr>
 
 " only remote jumps
-nnoremap <A-o>   <Plug>EnhancedJumpsRemoteOlder
-nnoremap <A-i>   <Plug>EnhancedJumpsRemoteNewer
+nnoremap <silent> <A-o>   <Plug>EnhancedJumpsRemoteOlder
+nnoremap <silent> <A-i>   <Plug>EnhancedJumpsRemoteNewer
 
-" both local and remote jumps
-" nnoremap <C-w>{      <Plug>EnhancedJumpsSwitchRemoteOlder
-" nnoremap <C-w>}      <Plug>EnhancedJumpsSwitchRemoteNewer
+function! EnhancedJumpsLocalOlder()
+    try
+     :execute "normal \<Plug>EnhancedJumpsLocalOlder<cr>"
+     :execute "normal zz<cr>"
+    catch /^Vim\%((\a\+)\)\=:E21/
+        call SendMessage(v:exception, v:errmsg)
+    endtry
+endfunction
+
+function! EnhancedJumpsLocalNewer()
+    try
+     :execute "normal \<Plug>EnhancedJumpsLocalNewer<cr>"
+     :execute "normal zz<cr>"
+    catch /^Vim\%((\a\+)\)\=:E21/
+        call SendMessage(v:exception, v:errmsg)
+    endtry
+endfunction
 ]])
