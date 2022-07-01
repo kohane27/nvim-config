46797088e279b9adf864c513798d7220b128b0a1
kohane27 <charuphoniu4173@gmail.com>
2022-07-01 13:35:05 +0800

    chore: `EnhancedJumps` config

--- lua/plugins/beacon.lua
+++ lua/plugins/beacon.lua
@@ -1,5 +1,5 @@
 vim.cmd([[
 let g:beacon_timeout = 500
-let g:beacon_minimal_jump = 4 " so J K would flash
+let g:beacon_minimal_jump = 10
 let g:beacon_ignore_filetypes = [ "toggleterm", "NvimTree", "qf", "help", "TelescopePrompt", "harpoon", "fzf"]
 ]])
--- lua/plugins/vim-EnhancedJumps.lua
+++ lua/plugins/vim-EnhancedJumps.lua
@@ -1,3 +1,18 @@
 vim.cmd([[
+" turn off the capture of jump messages on another buffer
+let g:EnhancedJumps_CaptureJumpMessages = 0
+" disable all mappings
+let g:EnhancedJumps_no_mappings = 1
 
+" only local jumps
+nnoremap <C-o>          <Plug>EnhancedJumpsLocalOlder
+nnoremap <C-i>          <Plug>EnhancedJumpsLocalNewer
+
+" only remote jumps
+nnoremap <A-o>   <Plug>EnhancedJumpsRemoteOlder
+nnoremap <A-i>   <Plug>EnhancedJumpsRemoteNewer
+
+" both local and remote jumps
+" nnoremap <C-w>{      <Plug>EnhancedJumpsSwitchRemoteOlder
+" nnoremap <C-w>}      <Plug>EnhancedJumpsSwitchRemoteNewer
 ]])
