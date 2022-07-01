f18ce641c546b840d4c60c095ea66d55bf592b47
kohane27 <kohane27@protonmail.com>
2022-07-01 15:16:37 +0800

    fix: `neoscroll` pre-hook for setting marks
    Former-commit-id: a8d32e3d79ec132e87dcef6fbd5636a8f5b3722a
    Former-commit-id: e4920a7cf07c620c22a6295d5e19932d349fdf66

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -4,17 +4,19 @@ if not status_ok then
 end
 
 neoscroll.setup({
-  -- Set any options as needed
+  pre_hook = function(info)
+    if info == "setmark" then
+      vim.cmd("normal m'")
+    end
+  end,
 })
 
 local t = {}
 -- Syntax: t[keys] = {function, {function arguments}}
-t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "200" } }
-t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "200" } }
-t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "350" } }
-t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "350" } }
-t["<C-y>"] = { "scroll", { "-0.10", "false", "100" } }
-t["<C-e>"] = { "scroll", { "0.10", "false", "100" } }
+t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "200", "quadratic", [['setmark']] } }
+t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "200", "quadratic", [['setmark']] } }
+t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "350", "quadratic", [['setmark']] } }
+t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "350", "quadratic", [['setmark']] } }
 t["zt"] = { "zt", { "200" } }
 t["zz"] = { "zz", { "200" } }
 t["zb"] = { "zb", { "200" } }
