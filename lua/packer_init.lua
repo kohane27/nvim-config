9f55472cdeedb4fdd01918eec80faced3596c836
kohane27 <kohane27@protonmail.com>
2022-08-10 09:06:52 +0800

    refactor: update `auto-save`
    Former-commit-id: 415d867e9b1ad80d6d59d2ca3fc59c87a45dbbf0
    Former-commit-id: 14adfec10fc547a90dfb4e8a4b865065d571c16d

--- lua/packer_init.lua
+++ lua/packer_init.lua
@@ -250,13 +250,50 @@ return packer.startup(function(use)
 
   -- Colorschemes/theme
   use("lunarvim/colorschemes")
-  use("NTBBloodbath/doom-one.nvim")
+  --use("NTBBloodbath/doom-one.nvim")
   use("marko-cerovac/material.nvim")
-  use("EdenEast/nightfox.nvim")
+  -- use("EdenEast/nightfox.nvim")
   use("navarasu/onedark.nvim")
   use("bluz71/vim-moonfly-colors")
-  use("bluz71/vim-nightfly-guicolors")
+  -- use("bluz71/vim-nightfly-guicolors")
   use("projekt0n/github-nvim-theme")
+  use({
+    "NTBBloodbath/doom-one.nvim",
+    setup = function()
+      -- Add color to cursor
+      vim.g.doom_one_cursor_coloring = false
+      -- Set :terminal colors
+      vim.g.doom_one_terminal_colors = true
+      -- Enable italic comments
+      vim.g.doom_one_italic_comments = false
+      -- Enable TS support
+      vim.g.doom_one_enable_treesitter = true
+      -- Color whole diagnostic text or only underline
+      vim.g.doom_one_diagnostics_text_color = false
+      -- Enable transparent background
+      vim.g.doom_one_transparent_background = false
+
+      -- Pumblend transparency
+      vim.g.doom_one_pumblend_enable = false
+      vim.g.doom_one_pumblend_transparency = 20
+
+      -- Plugins integration
+      vim.g.doom_one_plugin_neorg = true
+      vim.g.doom_one_plugin_barbar = false
+      vim.g.doom_one_plugin_telescope = false
+      vim.g.doom_one_plugin_neogit = true
+      vim.g.doom_one_plugin_nvim_tree = true
+      vim.g.doom_one_plugin_dashboard = true
+      vim.g.doom_one_plugin_startify = true
+      vim.g.doom_one_plugin_whichkey = true
+      vim.g.doom_one_plugin_indent_blankline = true
+      vim.g.doom_one_plugin_vim_illuminate = true
+      vim.g.doom_one_plugin_lspsaga = false
+    end,
+    config = function()
+      vim.cmd("colorscheme doom-one")
+    end,
+  })
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
