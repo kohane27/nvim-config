f710344a68708e243bd91aab34940880f6939514
kohane27 <charuphoniu4173@gmail.com>
2022-08-18 09:37:38 +0800

    `auto-save`: ignore `packer_init.lua`

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -22,18 +22,19 @@ autosave.setup({
     local fn = vim.fn
     local utils = require("auto-save.utils.data")
     if
-      fn.getbufvar(buf, "&modifiable") == 1
-      and utils.not_in(fn.getbufvar(buf, "&filetype"), {
-        "lua",
-        "javascript",
-        "typescript",
-        "javascriptreact",
-        "typescriptreact",
-        "tsx",
-        "jsx",
-        "svelte",
-        "vue",
-      })
+      vim.fn.expand("%") ~= "packer_init.lua"
+      or fn.getbufvar(buf, "&modifiable") == 1
+        and utils.not_in(fn.getbufvar(buf, "&filetype"), {
+          "lua",
+          "javascript",
+          "typescript",
+          "javascriptreact",
+          "typescriptreact",
+          "tsx",
+          "jsx",
+          "svelte",
+          "vue",
+        })
     then
       return true -- met condition(s), can save
     end
