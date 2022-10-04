c3759c23e0713996b02b150afbf00b4205911b74
kohane27 <charuphoniu4173@gmail.com>
2022-10-04 13:04:29 +0800

    fix: update 0.8 barbar API

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -42,12 +42,20 @@ barbar.setup({
 
 -- offset for nvim-tree
 local nvim_tree_events = require("nvim-tree.events")
-local bufferline_state = require("bufferline.state")
+local bufferline_api = require("bufferline.api")
 
-nvim_tree_events.on_tree_open(function()
-  bufferline_state.set_offset(31, "File Tree")
+local function get_tree_size()
+  return require("nvim-tree.view").View.width
+end
+
+nvim_tree_events.subscribe("TreeOpen", function()
+  bufferline_api.set_offset(get_tree_size())
 end)
 
-nvim_tree_events.on_tree_close(function()
-  bufferline_state.set_offset(0)
+nvim_tree_events.subscribe("Resize", function()
+  bufferline_api.set_offset(get_tree_size())
+end)
+
+nvim_tree_events.subscribe("TreeClose", function()
+  bufferline_api.set_offset(0)
 end)
