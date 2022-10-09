6e60a80ce056d88cb86c769529d19ec4daee83a3
kohane27 <kohane27@protonmail.com>
2022-10-09 21:39:31 +0800

    fix: config to true to fix pasting issue
    Former-commit-id: 0034d285c0b50fe4313f5a913878007c8511c348
    Former-commit-id: 35c8ab42993fc7b56db92ae1f0b59b512f726b4d

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -4,6 +4,11 @@ if not status_ok then
 end
 
 tmux.setup({
+  copy_sync = {
+    -- TMUX >= 3.2: all yanks (and deletes) will get redirected to system
+    -- clipboard by tmux
+    redirect_to_clipboard = true,
+  },
   navigation = {
     -- C-hjkl
     enable_default_keybindings = false,
