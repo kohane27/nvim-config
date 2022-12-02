12f7964818d5128039e6af8b7e708a8b67bdefa0
kohane27 <kohane27@protonmail.com>
2022-12-02 22:02:24 +0800

    feat: use `leap-spooky`
    Former-commit-id: 3ed114d380f113b8c188dc28706f33539f60dde3
    Former-commit-id: 349d5a398e6a576015a0745c48c7be18d6183831

---
+++ lua/plugins/leap-spooky.lua
@@ -0,0 +1,20 @@
+local status_ok, spooky = pcall(require, "leap-spooky")
+if not status_ok then
+  return
+end
+
+spooky.setup({
+  affixes = {
+    -- These will generate mappings for all native text objects, like:
+    -- (ir|ar|iR|aR|im|am|iM|aM){obj}.
+    -- Special line objects will also be added, by repeating the affixes.
+    -- E.g. `yrr<leap>` and `ymm<leap>` will yank a line in the current
+    -- window.
+    -- You can also use 'rest' & 'move' as mnemonics.
+    remote = { window = "r", cross_window = "R" },
+    magnetic = { window = "m", cross_window = "M" },
+  },
+  -- If this option is set to true, the yanked text will automatically be pasted
+  -- at the cursor position if the unnamed register is in use.
+  paste_on_remote_yank = false,
+})
