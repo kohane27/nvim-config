5f022e836ef9f02db3ef863c76dbc726204188d2
kohane27 <charuphoniu4173@gmail.com>
2022-12-02 20:52:36 +0800

    chore: update

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -3,4 +3,4 @@ if not status_ok then
   return
 end
 
-colorizer.setup()
+colorizer.setup({})
--- lua/plugins/nvim-lastplace.lua
+++ lua/plugins/nvim-lastplace.lua
@@ -3,4 +3,5 @@ if not status_ok then
   print("nvim-lastplace failing")
 end
 
+-- TODO https://github.com/ethanholz/nvim-lastplace/issues/16
 lastplace.setup({})
