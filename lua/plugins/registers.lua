3ae466116e17680c6ac355b82cd2fd237b2592b8
kohane27 <charuphoniu4173@gmail.com>
2022-12-03 10:05:48 +0800

    feat: update `registers.nvim`

--- lua/plugins/registers.lua
+++ lua/plugins/registers.lua
@@ -4,6 +4,10 @@ if not status_ok then
 end
 
 registers.setup({
+  -- Show a line at the bottom with registers that aren't filled
+  show_empty = false,
+  -- Show registers in this order
+  show = '+"-/_=#%.0123456789abcdefghijklmnopqrstuvwxyz:',
   bind_keys = {
     -- Show the window when pressing " in normal mode, applying the selected register as part of a motion
     normal = registers.show_window({ mode = "motion" }),
@@ -12,11 +16,23 @@ registers.setup({
     -- Show the window when pressing <C-R> in insert mode, inserting the selected register
     insert = registers.show_window({ mode = "insert" }),
 
-    -- Move the cursor in the registers window down when pressing <C-N>
+    -- Move the cursor in the registers window down
     ctrl_n = registers.move_cursor_down(),
-    -- Move the cursor in the registers window up when pressing <C-P>
+    -- Move the cursor in the registers window up
     ctrl_p = registers.move_cursor_up(),
     -- Clear the register of the highlighted line when pressing <DEL>
     delete = registers.clear_highlighted_register(),
+  },
+  symbols = {
+    -- The character to show when a register will be applied in a char-wise fashion
+    register_type_charwise = "",
+    -- The character to show when a register will be applied in a line-wise fashion
+    register_type_linewise = "",
+    -- The character to show when a register will be applied in a block-wise fashion
+    register_type_blockwise = "",
+  },
+  window = {
+    border = "single",
+    transparency = 10,
   },
 })
