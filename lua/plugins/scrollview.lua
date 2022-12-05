5acad78790059c5b9fb7ffb91cd7a4971b26d094
kohane27 <charuphoniu4173@gmail.com>
2022-12-05 21:20:01 +0800

    chore: cleanup

---
+++ lua/plugins/scrollview.lua
@@ -0,0 +1,11 @@
+local status_ok, scrollview = pcall(require, "scrollview")
+if not status_ok then
+  print("scrollview failing")
+end
+
+scrollview.setup({
+  on_startup = false,
+  auto_mouse = true,
+  current_only = true,
+  excluded_filetypes = { "nerdtree", "nvim-tree" },
+})
