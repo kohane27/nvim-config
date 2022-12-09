cef9c7fa7adcf89b925f3d7222a2ea6505baf782
kohane27 <charuphoniu4173@gmail.com>
2022-12-09 22:04:37 +0800

    fix: remove duplicate indent-line

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,17 +1,49 @@
 local status_ok, indent_blankline = pcall(require, "indent_blankline")
 if not status_ok then
-  print("indent_blankline failing")
+  return
 end
 
 indent_blankline.setup({
+  char = "│",
+  -- char = "▏",
+  show_first_indent_level = true,
+  use_treesitter = true,
   show_current_context = true,
   show_current_context_start = true,
   show_end_of_line = true,
   show_trailing_blankline_indent = false,
-  char = "▏",
-  use_treesitter = true,
-  show_first_indent_level = false,
+  -- show_first_indent_level = false,
+  context_patterns = {
+    "class",
+    "return",
+    "function",
+    "method",
+    "^if",
+    "^while",
+    "jsx_element",
+    "^for",
+    "^object",
+    "^table",
+    "block",
+    "arguments",
+    "if_statement",
+    "else_clause",
+    "jsx_element",
+    "jsx_self_closing_element",
+    "try_statement",
+    "catch_clause",
+    "import_statement",
+    "operation_type",
+  },
   filetype_exclude = {
+    "startify",
+    "dashboard",
+    "packer",
+    "neogitstatus",
+    "markdown",
+    "NvimTree",
+    "Trouble",
+    "vim",
     "help",
     "dashboard",
     "git",
