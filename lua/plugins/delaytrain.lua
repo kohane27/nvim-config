0ad26071d1d2213dbb6e6f6ad25085043f0d12fb
kohane27 <charuphoniu4173@gmail.com>
2022-12-10 09:51:23 +0800

    feat: use `delaytrain`

---
+++ lua/plugins/delaytrain.lua
@@ -0,0 +1,13 @@
+local status_ok, delaytrain = pcall(require, "delaytrain")
+if not status_ok then
+  return
+end
+
+delaytrain.setup({
+  delay_ms = 3000, -- How long repeated usag of a key should be prevented
+  grace_period = 3, -- How many repeated keypresse are allowed
+  keys = { -- Which keys (in which modes) should be delayed
+    ["n"] = { "w", "b", "j", "k" },
+    -- ["n"] = { "w", "b", "k", "l", "d", "t", "r", "e", "n" },
+  },
+})
