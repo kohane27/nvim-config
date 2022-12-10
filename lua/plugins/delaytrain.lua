35fa87faa7dd64f15bec090c6e180d74256bfe9a
kohane27 <charuphoniu4173@gmail.com>
2022-12-10 11:43:38 +0800

    chore: cleanup

--- lua/plugins/delaytrain.lua
+++ lua/plugins/delaytrain.lua
@@ -7,7 +7,7 @@ delaytrain.setup({
   delay_ms = 3000, -- How long repeated usag of a key should be prevented
   grace_period = 3, -- How many repeated keypresse are allowed
   keys = { -- Which keys (in which modes) should be delayed
-    ["n"] = { "w", "b", "j", "k" },
+    ["n"] = { "w", "b" },
     -- ["n"] = { "w", "b", "k", "l", "d", "t", "r", "e", "n" },
   },
 })
