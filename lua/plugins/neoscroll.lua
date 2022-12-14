582a225f24fe3e63793a9552f55cc91b2bc1c1be
kohane27 <kohane27@protonmail.com>
2022-12-14 18:00:02 +0800

    fix: center after scrolling
    Former-commit-id: 591d31eeb1561f2fbd7e6b2f1831fbf9dd9f947f
    Former-commit-id: 201279994d391fdf3ff926c26098006f89aa04f0

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -11,17 +11,29 @@ neoscroll.setup({
     end
   end,
   post_hook = function(info)
-    if info == "hook" then
-      vim.cmd("normal zz")
+    if info ~= "center" then
+      return
     end
+
+    -- The `defer_fn` is a bit of a hack.
+    -- We use it so that `neoscroll.init.scroll` will be false when we call `neoscroll.zz`
+    -- As long as we don't input another neoscroll mapping in the timeout,
+    -- we should be able to center the cursor.
+    vim.defer_fn(function()
+      neoscroll.zz(100, "sine")
+    end, 10)
   end,
+  -- post_hook = function(info)
+  --   if info == "hook" then
+  --     vim.cmd("normal zz")
+  --   end
+  -- end,
 })
 
 local t = {}
 -- Syntax: t[keys] = {function, {function arguments}}
--- - vim.wo.scroll
-t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "300", "quadratic", [['hook']] } }
-t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "300", "quadratic", [['hook']] } }
+t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "230", "sine", "'center'" } }
+t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "230", "sine", "'center'" } }
 t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "350", "quadratic", [['hook']] } }
 t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "350", "quadratic", [['hook']] } }
 t["zt"] = { "zt", { "200" } }
