164a33395220b7be868cc6407d669e4db54a6301
kohane27 <kohane27@protonmail.com>
2022-12-16 15:50:39 +0800

    fix: legendary: lazyloading
    Former-commit-id: d4fa5173836e7cd39c6cc784754ccbe076738bf2
    Former-commit-id: d201be04a14fe8cde410cc7d85f07ae240482ce9

--- lua/plugins/portal.lua
+++ lua/plugins/portal.lua
@@ -4,9 +4,31 @@ if not status_ok then
 end
 
 portal.setup({
+  ---@type "debug" | "info" | "warn" | "error"
+  log_level = "warn",
+
+  ---The default queries used when searching the jumplist. An entry can
+  ---be a name of a registered query item, an anonymous predicate, or
+  ---a well-formed query item. See Queries section for more information.
+  ---@type Portal.QueryLike[]
   query = { "modified", "different", "valid" },
 
+  ---An ordered list of keys that will be used for labelling available jumps.
+  ---Labels will be applied in same order as `query`.
+  ---@type string[]
   labels = { "j", "k", "h", "l" },
+
+  ---Keys used for exiting portal selection. To disable a key, set its value
+  ---to `nil` or `false`.
+  ---@type table<string, boolean | nil>
+  escape = {
+    ["<esc>"] = true,
+  },
+
+  ---The jumplist is fixed at 100 items, which has the possibility to impact
+  ---portal performance. Set this to a value less than 100 to limit the number
+  ---of jumps in the jumplist that will be queried.
+  lookback = 100,
 
   portal = {
     title = {
@@ -29,7 +51,7 @@ portal.setup({
 
     body = {
       -- When a portal is empty, render an empty buffer body
-      render_empty = true,
+      render_empty = false,
 
       --- The raw window options used for the portal body window
       options = {
