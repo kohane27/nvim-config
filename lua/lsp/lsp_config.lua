fc1284119cbed3658edba8b36eeec48f7cef6e16
kohane27 <kohane27@protonmail.com>
2022-12-18 16:42:21 +0800

    fix: no auto diagnostics in hover window
    Former-commit-id: 705cef902b995af2892f799f6fa3420a7c8ca666
    Former-commit-id: 7c4734533ba5dec4b4c87ad98ca23bf5e62b824a

--- lua/lsp/lsp_config.lua
+++ lua/lsp/lsp_config.lua
@@ -16,10 +16,10 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.s
   border = "rounded",
 })
 
-vim.cmd([[
-"" show line diagnostics automatically in hover window
-autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, { focus = false })
-]])
+-- vim.cmd([[
+-- "" show line diagnostics automatically in hover window
+-- autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, { focus = false })
+-- ]])
 
 -- Add additional capabilities supported by nvim-cmp
 -- https://github.com/neovim/nvim-lspconfig/wiki/Autocompletion
