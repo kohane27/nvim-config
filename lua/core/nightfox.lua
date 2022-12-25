c409970fb1700767dadf136847164a50260a9441
kohane27 <charuphoniu4173@gmail.com>
2022-12-25 19:34:54 +0800

    fix(nightfox): visual color fix

--- lua/core/nightfox.lua
+++ lua/core/nightfox.lua
@@ -3,4 +3,25 @@ if not status_ok then
   print("nightfox failing")
 end
 
-nightfox.setup({})
+nightfox.setup({
+  groups = {
+    all = {
+      Visual = { bg = "#6183bb" }, -- Visual mode selection
+    },
+  },
+  options = {
+    dim_inactive = false,
+    styles = {
+      comments = "italic",
+      conditionals = "NONE",
+      constants = "bold",
+      functions = "NONE",
+      keywords = "bold",
+      numbers = "NONE",
+      operators = "NONE",
+      strings = "NONE",
+      types = "italic,bold",
+      variables = "NONE",
+    },
+  },
+})
