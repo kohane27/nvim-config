0a6d9b3416eae0b305a881c0ca314907b29105ef
kohane27 <charuphoniu4173@gmail.com>
2022-12-25 17:15:50 +0800

    fix(nightfox): blend with lualine

--- lua/core/nightfox.lua
+++ lua/core/nightfox.lua
@@ -3,21 +3,4 @@ if not status_ok then
   print("nightfox failing")
 end
 
-nightfox.setup({
-  modules = {
-    barbar = true,
-    cmp = true,
-    diagnostic = true,
-    gitgutter = true,
-    gitsigns = true,
-    illuminate = true,
-    lsp_saga = true,
-    lsp_trouble = true,
-    native_lsp = true,
-    nvimtree = true,
-    symbol_outline = true,
-    telescope = true,
-    treesitter = true,
-    tsrainbow = true,
-  },
-})
+nightfox.setup({})
