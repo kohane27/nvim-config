086e4226ae40477e197029d41a23aee80fc944a3
kohane27 <charuphoniu4173@gmail.com>
2022-12-25 21:34:00 +0800

    fix(neoclip): settings

--- lua/plugins/nvim-neoclip.lua
+++ lua/plugins/nvim-neoclip.lua
@@ -6,31 +6,22 @@ end
 neoclip.setup({
   history = 1000,
   enable_persistent_history = true,
-  length_limit = 1048576,
-  continuous_sync = false,
-  db_path = vim.fn.stdpath("data") .. "/databases/neoclip.sqlite3",
   default_register = '"',
-  default_register_macros = "q",
   enable_macro_history = false,
-  content_spec_column = false,
   on_paste = {
-    set_reg = false,
-  },
-  on_replay = {
     set_reg = false,
   },
   keys = {
     telescope = {
       i = {
-        select = "<cr>",
-        paste = "<c-p>",
-        paste_behind = "<c-k>",
-        delete = "<c-d>", -- delete an entry
+        select = nil,
+        paste = "<cr>",
+        delete = nil, -- delete an entry
       },
       n = {
-        select = "<cr>",
-        paste = "p",
-        --- It is possible to map to more than one key.
+        select = nil,
+        paste = "<cr>",
+        --- It's possible to map to more than one key.
         -- paste = { 'p', '<c-p>' },
         paste_behind = "P",
         delete = "d",
