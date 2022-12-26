3d001045ad5d9aa42c27829188173fc473ce15bf
kohane27 <charuphoniu4173@gmail.com>
2022-12-26 12:59:10 +0800

    chore: cleanup

--- lua/plugins/beacon.lua
+++ lua/plugins/beacon.lua
@@ -1,8 +1,8 @@
 vim.cmd([[
-let g:beacon_timeout = 500
-let g:beacon_size = 60
-let g:beacon_minimal_jump = 20
-let g:beacon_ignore_filetypes = ["toggleterm", "NvimTree", "qf", "help", "TelescopePrompt", "harpoon", "fzf", "portal"]
-highlight Beacon guibg=#3a567d
+let g:beacon_timeout = 650
+let g:beacon_size = 70
+let g:beacon_minimal_jump = 10
+let g:beacon_ignore_filetypes = ["toggleterm", "NvimTree", "qf", "help", "TelescopePrompt", "harpoon", "grapple", "fzf", "portal"]
+" highlight Beacon guibg=#3a567d
  " base = "#3a567d", " bright = "#617897", " dim = "#2e4564"
 ]])
