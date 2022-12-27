d40f63b1e0c92fbb7db5d5598f15addfdd55ffe4
kohane27 <charuphoniu4173@gmail.com>
2022-12-27 21:52:19 +0800

    feat: configure `auto-session`

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -8,15 +8,17 @@ if not status_ok_2 then
   print("session-lens not working")
 end
 
+-- https://github.com/rmagatti/auto-session#recommended-sessionoptions-config
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
 auto_session.setup({
   log_level = "error",
-  -- cwd_change_handling = {
-  --   restore_upcoming_session = true, -- already the default, no need to specify like this, only here as an example
-  --   pre_cwd_changed_hook = nil, -- already the default, no need to specify like this, only here as an example
-  --   post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
-  --     require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
-  --   end,
-  -- },
+  auto_save_enabled = true, -- nil by default
+  auto_restore_enabled = true, -- nil by default
+  auto_session_suppress_dirs = { "~/", "/", "~/Downloads" },
+  post_cwd_changed_hook = function()
+    require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
+  end,
 })
 
 session_lens.setup({
