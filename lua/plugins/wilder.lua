8b04bae181e41b9576f68c647d021914f53fc395
kohane27 <kohane27@protonmail.com>
2022-12-27 21:36:46 +0800

    fix(wilder): configure
    Former-commit-id: 28f9f132a1df49947ae37dcec2bcd21da69f58cf
    Former-commit-id: 96635236e08336f13dbe163f0471337456885e04

--- lua/plugins/wilder.lua
+++ lua/plugins/wilder.lua
@@ -17,38 +17,78 @@ wilder.setup({
 -- disable Python remote plugin
 wilder.set_option("use_python_remote_plugin", 0)
 
-wilder.set_option("pipeline", {
-  wilder.branch(
-    wilder.cmdline_pipeline({
-      fuzzy = 1,
-      fuzzy_filter = wilder.lua_fzy_filter(),
+local popupmenu_renderer = wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+  border = "rounded",
+  empty_message = wilder.popupmenu_empty_message_with_spinner(),
+  highlighter = wilder.lua_fzy_highlighter(),
+  highlights = {
+    accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1 }, { a = 1 }, { foreground = "#f4468f" } }),
+  },
+  left = {
+    " ",
+    wilder.popupmenu_devicons(),
+    wilder.popupmenu_buffer_flags({
+      flags = " a + ",
+      icons = { ["+"] = "", a = "", h = "" },
     }),
-    wilder.vim_search_pipeline()
-  ),
+  },
+  right = {
+    " ",
+    wilder.popupmenu_scrollbar(),
+  },
+}))
+
+local wildmenu_renderer = wilder.wildmenu_renderer({
+  highlighter = wilder.lua_fzy_highlighter(),
+  highlights = {
+    accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1 }, { a = 1 }, { foreground = "#f4468f" } }),
+  },
+  separator = " · ",
+  left = { " ", wilder.wildmenu_spinner(), " " },
+  right = { " ", wilder.wildmenu_index() },
 })
 
 wilder.set_option(
   "renderer",
   wilder.renderer_mux({
-    [":"] = wilder.popupmenu_renderer({
-      highlighter = wilder.lua_fzy_highlighter(),
-      highlights = {
-        accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1 }, { a = 1 }, { foreground = "#f4468f" } }),
-      },
-      left = {
-        " ",
-        wilder.popupmenu_devicons(),
-      },
-      right = {
-        " ",
-        wilder.popupmenu_scrollbar(),
-      },
-    }),
-    ["/"] = wilder.wildmenu_renderer({
-      highlighter = wilder.lua_fzy_highlighter(),
-    }),
+    [":"] = popupmenu_renderer,
+    ["/"] = wildmenu_renderer,
+    substitute = wildmenu_renderer,
   })
 )
+
+-- wilder.set_option("pipeline", {
+--   wilder.branch(
+--     wilder.cmdline_pipeline({
+--       fuzzy = 1,
+--       fuzzy_filter = wilder.lua_fzy_filter(),
+--     }),
+--     wilder.vim_search_pipeline()
+--   ),
+-- })
+--
+-- wilder.set_option(
+--   "renderer",
+--   wilder.renderer_mux({
+--     [":"] = wilder.popupmenu_renderer({
+--       highlighter = wilder.lua_fzy_highlighter(),
+--       highlights = {
+--         accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1 }, { a = 1 }, { foreground = "#f4468f" } }),
+--       },
+--       left = {
+--         " ",
+--         wilder.popupmenu_devicons(),
+--       },
+--       right = {
+--         " ",
+--         wilder.popupmenu_scrollbar(),
+--       },
+--     }),
+--     ["/"] = wilder.wildmenu_renderer({
+--       highlighter = wilder.lua_fzy_highlighter(),
+--     }),
+--   })
+-- )
 
 vim.cmd([[
 " previous and next command key mapping to be compatible
