1c4ec30d69441134e641cf02fdb077409527a8a9
kohane27 <charuphoniu4173@gmail.com>
2022-12-27 14:47:16 +0800

    fix(leap): less noise when jumping

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -9,7 +9,16 @@ leap.set_default_keymaps(true)
 leap.setup({
   case_sensitive = false,
   safe_labels = {}, -- disable auto-jumping to the first match
+  max_phase_one_targets = 0, -- first char won't show possible matches
+  max_highlighted_traversal_targets = 10,
 })
+
+-- vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
+-- vim.api.nvim_set_hl(0, "LeapMatch", {
+--   fg = "white",
+--   bold = true,
+--   nocombine = true,
+-- })
 
 -- Bidirectional search
 local function leap_current_window()
@@ -18,5 +27,18 @@ end
 vim.keymap.set("n", "s", leap_current_window, { silent = true })
 vim.keymap.set("x", "s", leap_current_window, { silent = true })
 
--- Greying out the search area
--- vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
+-- mark it before jumping
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LeapEnter",
+  callback = function()
+    vim.cmd("normal m'")
+  end,
+})
+
+-- center cursor after jumping
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LeapLeave",
+  callback = function()
+    vim.cmd("normal zz")
+  end,
+})
