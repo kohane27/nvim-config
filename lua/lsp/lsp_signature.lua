bd32c59eb80856b3a0f695be818f628dba61be48
kohane27 <charuphoniu4173@gmail.com>
2022-12-29 01:05:13 +0800

    fix: toggle lsp_signature

--- lua/lsp/lsp_signature.lua
+++ lua/lsp/lsp_signature.lua
@@ -6,12 +6,11 @@ end
 lsp_signature.setup({
   bind = true, -- mandatory
   hint_enable = true,
-  hint_prefix = "💡 ",
+  hint_prefix = "🐼 ",
   handler_opts = {
     border = "rounded", -- double, rounded, single, shadow, none
   },
-
-  padding = "", -- character to pad on left and right of signature can be ' ', or '|'  etc
-  timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
-  toggle_key = nil, -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
+  padding = " ", -- character to pad on left and right of signature can be ' ', or '|'  etc
+  toggle_key = "<M-x>", -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
+  select_signature_key = "<M-n>", -- cycle to next signature, e.g. '<M-n>' function overloading
 })
