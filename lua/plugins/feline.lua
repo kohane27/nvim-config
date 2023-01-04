957606d318035dde3fb05d12ef74306e7b49c957
kohane27 <charuphoniu4173@gmail.com>
2023-01-04 18:11:15 +0800

    feat: configure `feline`

--- lua/plugins/feline.lua
+++ lua/plugins/feline.lua
@@ -3,198 +3,87 @@ if not status_ok then
   print("feline not working")
 end
 
-local one_monokai = {
-  fg = "#abb2bf",
-  bg = "#1e2024",
-  green = "#98c379",
-  yellow = "#e5c07b",
-  purple = "#c678dd",
-  orange = "#d19a66",
-  peanut = "#f6d5a4",
-  red = "#e06c75",
-  aqua = "#61afef",
-  darkblue = "#282c34",
-  dark_red = "#f75f5f",
+local kanagawa = {
+  fg = "#c8d3f5",
+  bg = "#1F1F28",
+  bg_dark = "#222436",
+  fg_dark = "#a9b1d6",
 }
 
-local c = {
-  gitBranch = {
-    provider = "git_branch",
+local provide = function(provider, fg_color, bg_color, style, left_sep, right_sep, enabled)
+  provider = provider or " "
+  fg_color = fg_color or "fg"
+  bg_color = bg_color or "bg"
+  style = style or "NONE"
+  left_sep = left_sep or ""
+  right_sep = right_sep or ""
+  enabled = enabled or true
+  return {
+    provider = provider,
     hl = {
-      fg = "peanut",
-      -- bg = "darkblue",
-      style = "bold",
+      fg = fg_color,
+      bg = bg_color,
+      style = style,
     },
-    left_sep = "block",
-    right_sep = "block",
-  },
-  gitDiffAdded = {
-    provider = "git_diff_added",
-    hl = {
-      fg = "green",
-      -- bg = "darkblue",
-    },
-    left_sep = "block",
-    right_sep = "block",
-  },
-  gitDiffRemoved = {
-    provider = "git_diff_removed",
-    hl = {
-      fg = "red",
-      -- bg = "darkblue",
-    },
-    left_sep = "block",
-    right_sep = "block",
-  },
-  gitDiffChanged = {
-    provider = "git_diff_changed",
-    hl = {
-      fg = "fg",
-      -- bg = "darkblue",
-    },
-    left_sep = "block",
-    right_sep = "right_filled",
-  },
-  separator = {
-    provider = "",
-  },
-  fileinfo = {
-    provider = {
-      name = "file_info",
-      opts = {
-        type = "relative-short",
-      },
-    },
-    hl = {
-      style = "bold",
-    },
-    left_sep = " ",
-    right_sep = " ",
-  },
-  diagnostic_errors = {
-    provider = "diagnostic_errors",
-    hl = {
-      fg = "red",
-    },
-  },
-  diagnostic_warnings = {
-    provider = "diagnostic_warnings",
-    hl = {
-      fg = "yellow",
-    },
-  },
-  diagnostic_hints = {
-    provider = "diagnostic_hints",
-    hl = {
-      fg = "aqua",
-    },
-  },
-  diagnostic_info = {
-    provider = "diagnostic_info",
-  },
-  lsp_client_names = {
-    provider = "lsp_client_names",
-    -- hl = {
-    --   fg = "purple",
-    --   bg = "darkblue",
-    --   style = "bold",
-    -- },
-    left_sep = "left_filled",
-    right_sep = "block",
-  },
-  file_type = {
-    provider = {
-      name = "file_type",
-      opts = {
-        filetype_icon = true,
-        case = "titlecase",
-      },
-    },
-    -- hl = {
-    --   fg = "red",
-    --   bg = "darkblue",
-    --   style = "bold",
-    -- },
-    left_sep = "block",
-    right_sep = "block",
-  },
-  file_encoding = {
-    provider = "file_encoding",
-    -- hl = {
-    --   fg = "orange",
-    --   bg = "darkblue",
-    --   style = "italic",
-    -- },
-    left_sep = "block",
-    right_sep = "block",
-  },
-  position = {
-    provider = "position",
-    -- hl = {
-    --   fg = "green",
-    --   bg = "darkblue",
-    --   style = "bold",
-    -- },
-    left_sep = "block",
-    right_sep = "block",
-  },
-  line_percentage = {
-    provider = "line_percentage",
-    -- hl = {
-    --   fg = "aqua",
-    --   bg = "darkblue",
-    --   style = "bold",
-    -- },
-    left_sep = "block",
-    right_sep = "block",
-  },
-  scroll_bar = {
-    provider = "scroll_bar",
-    hl = {
-      fg = "yellow",
-      style = "bold",
-    },
-  },
-}
+    left_sep = left_sep,
+    right_sep = right_sep,
+    enabled = enabled,
+  }
+end
 
 local left = {
-  c.gitBranch,
-  c.gitDiffAdded,
-  c.gitDiffRemoved,
-  c.gitDiffChanged,
-  c.separator,
-}
-
-local middle = {
-  c.fileinfo,
-  c.diagnostic_errors,
-  c.diagnostic_warnings,
-  c.diagnostic_info,
-  c.diagnostic_hints,
+  provide("git_branch", "#957FB8", "bg", "bold", "block", "block"),
+  provide("git_diff_added", "#76946A"),
+  provide("git_diff_removed", "#C34043"),
+  provide("git_diff_changed", "#DCA561"),
+  provide("diagnostic_errors", "#E82424"),
+  provide("diagnostic_warnings", "#FF9E3B"),
+  provide("diagnostic_hints", "#658594"),
+  provide("diagnostic_info", "#6A9589"),
 }
 
 local right = {
-  c.lsp_client_names,
-  c.file_type,
-  c.file_encoding,
-  c.position,
-  c.line_percentage,
+  provide("lsp_client_names", "fg_dark", "bg", "NONE", "block", "block"),
+  provide(
+    { name = "file_type", opts = { filetype_icon = true, case = "titlecase" } },
+    "fg_dark",
+    "bg",
+    "NONE",
+    "block",
+    "block"
+  ),
+  provide("position", "fg_dark", "bg", "NONE", "block", "block"),
+  provide("line_percentage", "fg_dark", "bg", "NONE", "block", "block"),
+}
+
+local left_inactive = {
+  provide(
+    { name = "file_type", opts = { filetype_icon = true, case = "titlecase" } },
+    "fg",
+    "bg",
+    "NONE",
+    "block",
+    "block"
+  ),
+}
+
+local right_inactive = {
+  provide("position", "fg_dark", "bg", "NONE", "block", "block"),
+  provide("line_percentage", "fg_dark", "bg", "NONE", "block", "block"),
 }
 
 local components = {
   active = {
     left,
-    middle,
     right,
   },
   inactive = {
-    left,
-    middle,
-    right,
+    left_inactive,
+    right_inactive,
   },
 }
 
 feline.setup({
   components = components,
-  theme = one_monokai,
+  theme = kanagawa,
 })
