c5ae9cd18670acf0d64438fd7beddc8c1a41420d
kohane27 <kohane27@protonmail.com>
2023-01-06 20:40:40 +0800

    fix: remove broken delimiter rules
    Former-commit-id: 956dcde007a2aa2d6154b6ce7154b1ed5f1c67ea
    Former-commit-id: b756e44eaa264d4689024e6e7c61594b041982ee

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -4,26 +4,16 @@ if not status_ok then
 end
 
 nvim_surround.setup({
-  surrounds = {
-    ["("] = {
-      add = function()
-        return { { "(" }, { ")" } }
-      end,
-    },
-    ["{"] = {
-      add = function()
-        return { { "{" }, { "}" } }
-      end,
-    },
-    ["<"] = {
-      add = function()
-        return { { "<" }, { ">" } }
-      end,
-    },
-    ["["] = {
-      add = function()
-        return { { "[" }, { "]" } }
-      end,
-    },
+  keymaps = {
+    insert = nil,
+    insert_line = nil,
+    normal = "ys",
+    normal_cur = "yss",
+    normal_line = nil,
+    normal_cur_line = nil,
+    visual = "S",
+    visual_line = nil,
+    delete = "ds",
+    change = "cs",
   },
 })
