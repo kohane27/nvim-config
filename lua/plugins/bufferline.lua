bd5fe2a2a0b493a9f2edc1984eaf80ddc5e7a2a8
kohane27 <kohane27@protonmail.com>
2023-01-10 10:17:27 +0800

    chore: cleanup
    Former-commit-id: 113a7007d988bed4920801c377a4bb3efa172e62
    Former-commit-id: 22133e5f4e3d8d6c541745cc9996bf25601a936b

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,11 +1,16 @@
 local status_ok, bufferline = pcall(require, "bufferline")
 if not status_ok then
   print("bufferline not working")
-
 end
 
 bufferline.setup({
   options = {
+    buffer_close_icon = " ",
+    modified_icon = " ",
+    close_icon = " ",
+    truncate_names = true,
+    max_prefix_length = 15,
+    max_name_length = 18,
     color_icons = true,
     show_buffer_icons = true,
     always_show_bufferline = true,
