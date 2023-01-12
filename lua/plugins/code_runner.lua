150c04a70ce3c590390403cad88135116d1c5f29
kohane27 <kohane27@protonmail.com>
2023-01-12 14:39:58 +0800

    fix(code_runner): runtime error
    Former-commit-id: 4df3ae0fd3885628a3f42243478ba2117c0f6c72
    Former-commit-id: 4cebe3d5259bb263659131069bc0dfe7cb799d79

--- lua/plugins/code_runner.lua
+++ lua/plugins/code_runner.lua
@@ -6,13 +6,25 @@ end
 code_runner.setup({
   focus = false,
   filetype = {
+    javascript = "node",
+    typescript = "node --no-warnings --experimental-specifier-resolution=node --loader ts-node/esm",
+    -- typescript = "deno run",
+    typescriptreact = "yarn dev$end",
+
     -- java = "cd $dir && javac $fileName && java $fileNameWithoutExt",
     -- java = 'cd $dir && cd ../../../../../ && mvn -nsu -e -q compile -T 4 exec:java -Dexec.mainClass="com.prac.App"',
     java = "mvn -nsu -e -q compile -T 4 exec:java",
-    python = "python3 -u",
-    javascript = "node",
-    typescript = "node --no-warnings --experimental-specifier-resolution=node --loader ts-node/esm",
-    rust = "cd $dir && rustc $fileName && $dir/$fileNameWithoutExt",
-    sh = "cd $dir && ./$fileName",
+    -- java = "cd $dir && javac $fileName && java $fileNameWithoutExt",
+
+    sh = "bash",
+    -- sh = "cd $dir && ./$fileName",
+
+    python = "python -u",
+    rust = "cd $dir && rustc $fileName && $dir$fileNameWithoutExt",
+    kotlin = "cd $dir && kotlinc-native $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt.kexe",
+    c = "cd $dir && gcc $fileName -o $fileNameWithoutExt && $dir/$fileNameWithoutExt",
+    cpp = "cd $dir && g++ $fileName -o $fileNameWithoutExt && $dir/$fileNameWithoutExt",
   },
+  filetype_path = vim.fn.expand("~/.config/nvim/code_runner.json"),
+  project_path = vim.fn.expand("~/.config/nvim/project_manager.json"),
 })
