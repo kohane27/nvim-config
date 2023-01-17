ae369fc5ebc16b6900d1e646d07e09a6852de6a0
kohane27 <charuphoniu4173@gmail.com>
2023-01-17 21:16:56 +0800

    fix: BR

--- lua/lsp/lspsaga.lua
+++ lua/lsp/lspsaga.lua
@@ -3,9 +3,8 @@ if not status_ok then
   print("saga not working")
 end
 
-saga.init_lsp_saga({
-  border_style = "rounded",
-  code_action_lightbulb = {
+saga.setup({
+  lightbulb = {
     enable = true,
     enable_in_insert = false,
     sign = true,
@@ -18,11 +17,35 @@ saga.init_lsp_saga({
     show_file = false,
     click_support = false,
   },
-  finder_action_keys = {
+  finder = {
     open = { "o", "<CR>" },
     vsplit = "<C-v>",
     split = "<C-s>",
     tabe = "<C-t>",
     quit = { "q", "<ESC>" },
+  },
+  ui = {
+    -- currently only round theme
+    theme = "round",
+    title = true,
+    -- single, double, rounded, solid, shadow
+    border = "rounded",
+    winblend = 0,
+    colors = {
+      --float window normal background color
+      normal_bg = "#16161D",
+      --title background color
+      title_bg = "#afd700",
+      red = "#C34043",
+      magenta = "#b33076",
+      orange = "#FFA066",
+      yellow = "#DCA561",
+      green = "#76946A",
+      cyan = "#9CABCA",
+      blue = "#7E9CD8",
+      purple = "#CBA6F7",
+      white = "#727169",
+      black = "#16161D",
+    },
   },
 })
