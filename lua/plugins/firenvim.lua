8ef0480c22155f6fd5bf431d907b7651ec0ec227
kohane27 <charuphoniu4173@gmail.com>
2023-02-04 14:57:11 +0800

    fix: enable `firenvim`

---
+++ lua/plugins/firenvim.lua
@@ -0,0 +1,40 @@
+vim.cmd([[
+let g:firenvim_config = {
+    \ 'globalSettings': {
+        \ 'alt': 'all',
+        \ '<C-w>': 'default',
+        \ '<C-n>': 'default',
+    \  },
+    \ 'localSettings': {
+        \ '.*': {
+            \ 'cmdline': 'neovim',
+            \ 'content': 'text',
+            \ 'priority': 0,
+            \ 'selector': 'textarea',
+            \ 'takeover': 'never',
+        \ },
+    \ }
+\ }
+if exists('g:started_by_firenvim')
+
+    " xremap's config.yml
+    " C-w: C-KEY_BACKSPACE
+    " imap <C-BS> <C-W>
+
+    colorscheme nightfox
+    " nightfly or material also ok
+    set guifont=FiraCode_Nerd_Font_Mono:h11
+
+    " don't run Startify
+    let g:startify_disable_at_vimenter = 1
+    " let g:startify_new_tab = 1
+
+    " don't run null-ls
+    let g:null_ls_disable = 1
+
+    " don't use auto-session
+    let g:auto_session_enabled = v:false
+
+    au BufEnter github.com_*.txt set filetype=markdown
+endif
+]])
