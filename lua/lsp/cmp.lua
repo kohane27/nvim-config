d8e40b862f857b431370b0e6fb0d72cdd4c99943
kohane27 <kohane27@protonmail.com>
2023-03-03 15:24:12 +0800

    fix: cmp for luasnip
    Former-commit-id: 9a7d13bce411659cc27e7de89d6cb950a5af61e7
    Former-commit-id: 40ec2a0250399a4f874beeb5978b794ceb0acea0

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -24,26 +24,25 @@ cmp.setup({
     end,
   },
   mapping = {
-    -- ["<C-p>"] = cmp.mapping.select_prev_item(),
-    -- ["<C-n>"] = cmp.mapping.select_next_item(),
-    -- ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-    -- ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
 
+    ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+    ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+
+    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+
+    -- ["<C-Space>"] = cmp.mapping(cmp.mapping.abort(), { "i", "c" }),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-    ["<C-y>"] = cmp.config.disable, -- remove the default mapping
+    ["<C-y>"] = cmp.config.disable, -- remove default mapping
     ["<C-e>"] = cmp.config.disable,
-    -- ["<C-e>"] = cmp.mapping({
-    --   i = cmp.mapping.abort(),
-    --   c = cmp.mapping.close(),
-    -- }),
 
     -- Accept currently selected item. If none selected, `select` first item.
     -- Set `select` to `false` to only confirm explicitly selected items.
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    ["<C-n>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expandable() then
+
+    -- Tab and S-Tab for `luasnip`
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if luasnip.expandable() then
         luasnip.expand()
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
@@ -53,10 +52,8 @@ cmp.setup({
         fallback()
       end
     end, { "i", "s" }),
-    ["<C-p>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(-1) then
         luasnip.jump(-1)
       else
         fallback()
