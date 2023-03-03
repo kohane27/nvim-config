7c149f35a417d5fb81d265ffa11ade4672c4be0b
kohane27 <charuphoniu4173@gmail.com>
2023-03-03 12:38:54 +0800

    fix(lspsaga): remove diagnostic on insert

--- lua/lsp/lspsaga.lua
+++ lua/lsp/lspsaga.lua
@@ -4,6 +4,9 @@ if not status_ok then
 end
 
 saga.setup({
+  diagnostic = {
+    on_insert = false,
+  },
   lightbulb = {
     enable = true,
     enable_in_insert = false,
@@ -25,24 +28,8 @@ saga.setup({
     quit = { "q", "<ESC>" },
   },
   ui = {
-    -- currently only round theme
-    theme = "round",
     title = true,
     border = "rounded", -- single, double, rounded, solid, shadow
     winblend = 0,
-    colors = {
-      normal_bg = "#1F1F28", -- float window normal background color
-      title_bg = "#afd700", --title background color
-      red = "#C34043",
-      magenta = "#b33076",
-      orange = "#FFA066",
-      yellow = "#DCA561",
-      green = "#76946A",
-      cyan = "#9CABCA",
-      blue = "#7E9CD8",
-      purple = "#CBA6F7",
-      white = "#727169",
-      black = "#16161D",
-    },
   },
 })
