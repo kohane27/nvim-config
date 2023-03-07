c7aed91bb4b563272e3924eceb835689b608a1ff
kohane27 <charuphoniu4173@gmail.com>
2023-03-07 16:23:22 +0800

    chore(autopairs): disable adding spaces between parentheses

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -13,32 +13,32 @@ local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local cmp = require("cmp")
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
--- add spaces between parentheses
--- | Before | Insert  |  After   |
--- |--------|---------|----------|
--- | `(|)`  | `space` | `( | )`  |
--- | `( | )`| `)`     | `(  )|`  |
-local brackets = { { "(", ")" }, { "[", "]" }, { "{", "}" } }
-local Rule = require("nvim-autopairs.rule")
-npairs.add_rules({
-  Rule(" ", " "):with_pair(function(opts)
-    local pair = opts.line:sub(opts.col - 1, opts.col)
-    return vim.tbl_contains({
-      brackets[1][1] .. brackets[1][2],
-      brackets[2][1] .. brackets[2][2],
-      brackets[3][1] .. brackets[3][2],
-    }, pair)
-  end),
-})
-for _, bracket in pairs(brackets) do
-  npairs.add_rules({
-    Rule(bracket[1] .. " ", " " .. bracket[2])
-      :with_pair(function()
-        return false
-      end)
-      :with_move(function(opts)
-        return opts.prev_char:match(".%" .. bracket[2]) ~= nil
-      end)
-      :use_key(bracket[2]),
-  })
-end
+-- -- add spaces between parentheses
+-- -- | Before | Insert  |  After   |
+-- -- |--------|---------|----------|
+-- -- | `(|)`  | `space` | `( | )`  |
+-- -- | `( | )`| `)`     | `(  )|`  |
+-- local brackets = { { "(", ")" }, { "[", "]" }, { "{", "}" } }
+-- local Rule = require("nvim-autopairs.rule")
+-- npairs.add_rules({
+--   Rule(" ", " "):with_pair(function(opts)
+--     local pair = opts.line:sub(opts.col - 1, opts.col)
+--     return vim.tbl_contains({
+--       brackets[1][1] .. brackets[1][2],
+--       brackets[2][1] .. brackets[2][2],
+--       brackets[3][1] .. brackets[3][2],
+--     }, pair)
+--   end),
+-- })
+-- for _, bracket in pairs(brackets) do
+--   npairs.add_rules({
+--     Rule(bracket[1] .. " ", " " .. bracket[2])
+--       :with_pair(function()
+--         return false
+--       end)
+--       :with_move(function(opts)
+--         return opts.prev_char:match(".%" .. bracket[2]) ~= nil
+--       end)
+--       :use_key(bracket[2]),
+--   })
+-- end
