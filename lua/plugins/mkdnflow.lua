1fe7297f600993347312d6ff0d40c5f20790e304
kohane27 <kohane27@protonmail.com>
2023-03-07 21:33:19 +0800

    feat: configure `mkdnflow`
    Former-commit-id: 16879ed82337b213c4224093c76b5f87b15885df
    Former-commit-id: 265aecccd1f50d40c1e5d9b1b99aa9b469413e49

---
+++ lua/plugins/mkdnflow.lua
@@ -0,0 +1,16 @@
+local status_ok, mkdnflow = pcall(require, "mkdnflow")
+if not status_ok then
+  print("mkdnflow not working")
+end
+
+mkdnflow.setup({
+  links = {
+    style = "markdown",
+    name_is_source = false,
+    conceal = true,
+    context = 1,
+  },
+  to_do = {
+    symbols = { " ", "X" },
+  },
+})
