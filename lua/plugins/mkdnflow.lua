4665357b2189a16611b4e353e2962534593d31ad
kohane27 <kohane27@protonmail.com>
2023-03-12 13:00:48 +0800

    chore: cleanup
    Former-commit-id: a93ae5aa3d7df338165ff145ec140d78a0360cf0
    Former-commit-id: 1088f6e8c3407c358e1289018954970c2b4d972c

--- lua/plugins/mkdnflow.lua
+++ lua/plugins/mkdnflow.lua
@@ -4,7 +4,18 @@ if not status_ok then
 end
 
 mkdnflow.setup({
+  links = {
+    implicit_extension = nil,
+    transform_implicit = true,
+    transform_explicit = function(text)
+      -- TODO fix
+      -- [18. Managing State.md](/Courses/React Docs/18. Managing State.md)
+      text = text:gsub("<", " ")
+      text = text:gsub(">", " ")
+      return text
+    end,
+  },
   to_do = {
-    symbols = { " ", "X" },
+    symbols = { " ", "x" },
   },
 })
