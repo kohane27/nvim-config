d1840172383fdab255288b850636b2c14439fde6
kohane27 <kohane27@protonmail.com>
2023-03-14 15:40:37 +0800

    fix(LuaSnip): attempt to fix duplication
    Former-commit-id: f28edbdacb79657d3762151359676403a07e9d6f
    Former-commit-id: 288e7a9b5c15657150f1c1769120afb554c43750

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,9 +1,7 @@
 local ls = require("luasnip")
 
 ls.config.set_config({
-  history = false, -- TODO: maybe fixed duplication bug?
-  region_check_events = "CursorHold,InsertLeave", -- fix duplication
-  delete_check_events = "TextChanged,InsertEnter", -- fix duplication
-  -- Update more often, :h events for more info.
-  update_events = "TextChanged,TextChangedI",
+  history = false,
+  region_check_events = "CursorHold,InsertLeave",
+  delete_check_events = "TextChanged,InsertEnter",
 })
