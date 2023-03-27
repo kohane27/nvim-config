a5bb9dc58948f2d424bab4d42ebb0b6592995eaf
kohane27 <charuphoniu4173@gmail.com>
2023-03-27 15:25:54 +0800

    fix(luasnip): duplication bug

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,5 +1,18 @@
 local ls = require("luasnip")
 
+local unlinkgrp = vim.api.nvim_create_augroup("UnlinkSnippetOnModeChange", { clear = true })
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+  group = unlinkgrp,
+  pattern = { "s:n", "i:*" },
+  desc = "Forget the current snippet when leaving the insert mode",
+  callback = function(evt)
+    if ls.session and ls.session.current_nodes[evt.buf] and not ls.session.jump_active then
+      ls.unlink_current()
+    end
+  end,
+})
+
 ls.config.set_config({
   history = false,
   region_check_events = "CursorHold,InsertLeave",
