1e8cfa1c49748a0b5ecdc1230096ee968bb2c364
kohane27 <kohane27@protonmail.com>
2023-04-08 14:46:20 +0800

    chore(portal): configure
    Former-commit-id: 3b394bf731da84e1210d7eb24ab031cc45f5391b
    Former-commit-id: fff054af2701b603f89ce7e96110d9c410e39773

--- lua/plugins/portal.lua
+++ lua/plugins/portal.lua
@@ -4,16 +4,22 @@ if not status_ok then
 end
 
 portal.setup({
-    ---max. number of results returned
-    max_results = 6,
+  max_results = 6,
 
-    ---An ordered list of keys for labelling portals.
-    ---Labels will be applied in order
-    labels = { "j", "k", "h", "l", "a", "s", "d", "f" },
+  ---An ordered list of keys for labelling portals.
+  ---Labels will be applied in order
+  -- labels = { "j", "k", "h", "l", "a", "s", "d", "f" },
 
   --- Keys for exiting portal selection
   escape = {
     ["<esc>"] = true,
     ["<C-c>"] = true,
+  },
+
+  window_options = {
+    relative = "cursor",
+    width = 80,
+    height = 5,
+    col = 5,
   },
 })
