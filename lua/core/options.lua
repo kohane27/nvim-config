ef660e0ecf9e69741e0114c5120544a6efb64cd7
kohane27 <kohane27@protonmail.com>
2023-04-14 18:23:06 +0800

    fix: remove `filetype.nvim`
    Former-commit-id: 0defbf2d57e571354dc0ca5e3df46cb0c3dc3b2d
    Former-commit-id: 7209f70264d283db54e2e64895646d7fa085da31

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -44,8 +44,8 @@ opt.autowriteall     = true
 opt.timeoutlen       = 450                         -- time to wait for a mapped sequence to complete
 opt.writebackup      = true                        -- disable editing a file that is being edited
 opt.list             = true
-g.did_load_filetypes = 0                           -- use filetype.lua instead of filetype.vim
-g.do_filetype_lua    = 1
+g.do_filetype_lua    = true                        -- use filetype.lua instead of filetype.vim
+g.did_load_filetypes = false
 g.rehash256          = 1
 
 -- disable tilde on end of buffer
