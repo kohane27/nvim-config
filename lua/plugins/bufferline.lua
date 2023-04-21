1a5df628d382ec7a6d5df9b73d2d14ddc2fd2b8d
kohane27 <charuphoniu4173@gmail.com>
2023-04-21 11:03:21 +0800

    BREAKING CHANGE: bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -11,12 +11,16 @@ bufferline.setup({
     truncate_names = true,
     max_prefix_length = 15,
     max_name_length = 18,
+
     color_icons = true,
     show_buffer_icons = true,
     always_show_bufferline = true,
     show_buffer_close_icons = true,
-    show_buffer_default_icon = true,
     show_close_icon = false,
+    get_element_icon = function(element)
+      local icon, hl = require("nvim-web-devicons").get_icon_by_filetype(element.filetype, { default = false })
+      return icon, hl
+    end,
     show_tab_indicators = true,
     show_duplicate_prefix = false,
     offsets = {
