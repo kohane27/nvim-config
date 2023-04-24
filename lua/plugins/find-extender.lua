f938c3704fc26465cd19f0c02005e87dd584122e
kohane27 <charuphoniu4173@gmail.com>
2023-04-24 18:47:23 +0800

    fix(find-extender): use same color as leap

--- lua/plugins/find-extender.lua
+++ lua/plugins/find-extender.lua
@@ -1,15 +1,14 @@
 local status_ok, find_extender = pcall(require, "find-extender")
 if not status_ok then
-  print("find_extender not working")
+  print("find_extender not working find find ")
 end
 
 find_extender.setup({
   movments = {
     min_matches = 2,
-    highlight_match = { fg = "#c0caf5", bg = "#545c7e" },
+    highlight_match = { fg = "#000000", bg = "#CCFF88" },
     lh = {
       enable = false,
-      cursor_hl = { fg = "#545c7e", bg = "#ff9e64" },
     },
     leap = {
       enable = true,
