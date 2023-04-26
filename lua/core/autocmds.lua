6ebb040795fe31e4f5badc365d98097d25cbbed0
kohane27 <kohane27@protonmail.com>
2023-04-26 08:02:40 +0800

    fix: read substitutes from file
    Former-commit-id: 9d8080051b3daf0495c29a0856e3f3bc8f634ffd
    Former-commit-id: 24f652405968cb34a100b181718047929e5dd975

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -22,9 +22,10 @@ vim.cmd([[
     autocmd!
     if exists('$TMUX')
 
+      let data = readfile($HOME .'/.config/nvim/substitutes--.txt')
      " let cwd = substitute(getcwd(), '^.*/', '', '')
       let cwd = fnamemodify(getcwd(), ':t')
-      let shortened_cwd = ***REMOVED***
+      let shortened_cwd = substitute(cwd, data[0], "", "")
       autocmd BufReadPost,FileReadPost,BufNewFile,BufEnter * call system("tmux rename-window " . shortened_cwd . "/" . expand("%:t"))
       " autocmd BufReadPost,FileReadPost,BufNewFile,BufEnter * call system("tmux rename-window " . expand("%:t"))
 
