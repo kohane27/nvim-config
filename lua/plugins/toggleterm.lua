30d991dfacc663674c52a8b461b70fe5694d2190
kohane27 <charuphoniu4173@gmail.com>
2023-04-26 11:41:47 +0800

    chore: configure `toggleterm`

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,35 +4,45 @@ if not status_ok then
 end
 
 toggleterm.setup({
-  size = 10,
+  size = 12,
   open_mapping = [[<c-\>]],
   hide_numbers = true,
-  shade_terminals = false,
-  shading_factor = 1,
   start_in_insert = true,
   insert_mappings = true,
   direction = "horizontal", -- 'vertical' | 'horizontal' | 'tab' | 'float'
   close_on_exit = true,
+  shade_terminals = true,
+  shading_factor = 1,
   shade_filetypes = { "none", "fzf" },
-  float_opts = {
-    border = "curved",
-    winblend = 0,
-    highlights = {
-      border = "Normal",
-      background = "Normal",
-    },
-  },
+  -- float_opts = {
+  --   border = "double",
+  --   winblend = 0,
+  --   highlights = {
+  --     border = "Normal",
+  --     background = "Normal",
+  --   },
+  -- },
 })
 
 function _G.set_terminal_keymaps()
-  local opts = { noremap = true }
-  vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<C-c>", [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<A-h>", [[<C-\><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<A-j>", [[<C-\><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<A-k>", [[<C-\><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<A-l>", [[<C-\><C-n><C-W>l]], opts)
+  local opts = { buffer = 0, noremap = true, silent = true }
+
+  -- escape
+  vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "<C-c>", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+  -- vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+
+  -- navigate
+  vim.keymap.set("t", "<A-h>", [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set("t", "<A-j>", [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set("t", "<A-k>", [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set("t", "<A-l>", [[<Cmd>wincmd l<CR>]], opts)
+
+  -- force close without prompting
+  vim.keymap.set("t", "<A-c>", [[<Cmd>q!<CR>]], opts)
+  -- vim.keymap.set("t", "<A-c>", [[<C-\><C-n><C-W>c]], opts)
 end
 
--- mappings for only toggle term
+--  mappings only for toggleterm
 vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
