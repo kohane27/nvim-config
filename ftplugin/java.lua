28c88c654ac020b20920d415fad5e3d1ff92ee86
kohane27 <kohane27@protonmail.com>
2023-05-04 23:19:13 +0800

    fix: use latest java lsp server
    Former-commit-id: a8152e8d3b892e1c8ecba0f5e3809d87be7ab5d4
    Former-commit-id: 4d277560fa60f0f31ebbca14702b3e6997848708

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -2,7 +2,10 @@ local jdtls = require("jdtls")
 local root_markers = { "mvnw", "gradlew", ".git" }
 local root_dir = require("jdtls.setup").find_root(root_markers)
 local home = os.getenv("HOME")
-local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+-- local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local workspace_dir = home .. "/.local/share/eclipse/" .. project_name
 
 -- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
 local config = {
@@ -26,17 +29,17 @@ local config = {
     "-jar",
     vim.fn.glob(
       home
-        .. "/.config/nvim/jdtls/jdt-language-server-1.9.0-202203031534/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar"
+        .. "/.config/nvim/jdtls/jdt-language-server-latest/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar"
     ),
 
     -- 💀
     "-configuration",
-    vim.fn.glob(home .. "/.config/nvim/jdtls/jdt-language-server-1.9.0-202203031534/config_linux"),
+    vim.fn.glob(home .. "/.config/nvim/jdtls/jdt-language-server-latest/config_linux"),
 
     -- 💀
     -- See `data directory configuration` section in the README
     "-data",
-    workspace_folder,
+    workspace_dir,
   },
 
   -- 💀
