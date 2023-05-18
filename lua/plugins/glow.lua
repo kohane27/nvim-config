b6679034844ef4ef58422e0cb03760707ca5a71a
kohane27 <kohane27@protonmail.com>
2023-05-18 22:09:56 +0800

    feat: preview md window on the right
    Former-commit-id: 9d0d71302f4cc2ef389c8e6aea589b395c145ee8
    Former-commit-id: 53fc062cb183129bf893505bee8249909bd4ed8d

--- lua/plugins/glow.lua
+++ lua/plugins/glow.lua
@@ -4,30 +4,3 @@ if not status_ok then
 end
 
 glow.setup({})
-
-local function sanitize_shell_string(input)
-  local sanitized = input:gsub("'", "'\\''")
-  return "'" .. sanitized .. "'"
-end
-
-local function markdown()
-  local buf = vim.api.nvim_create_buf(false, true)
-
-  local current_buf = vim.api.nvim_get_current_buf()
-  local current_win = vim.api.nvim_get_current_win()
-
-  local lines = vim.api.nvim_buf_get_lines(current_buf, 0, -1, false)
-  local markdown = table.concat(lines, "\n")
-  local sanitized = sanitize_shell_string(markdown)
-
-  vim.cmd("rightbelow vert sbuffer " .. buf)
-
-  local new_win = vim.api.nvim_get_current_win()
-  vim.api.nvim_win_set_option(new_win, "number", false)
-  vim.api.nvim_win_set_option(new_win, "cursorline", false)
-  vim.api.nvim_win_set_option(new_win, "relativenumber", false)
-
-  vim.fn.termopen("glow <( echo " .. sanitized .. ")\n")
-end
-
-vim.keymap.set("n", "<leader>m", markdown, {})
