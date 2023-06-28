c155b05162fcde05fad7381d0a86fce42c368e00
kohane27 <charuphoniu4173@gmail.com>
2023-06-28 17:41:06 +0800

    fix: use custom prompt

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -15,7 +15,7 @@ chatgpt.setup({
   chat = {
     keymaps = {
       close = "<A-c>",
-      new_session = "<C-w>n",
+      new_session = "<A-n>",
       cycle_windows = "<A-l>",
       -- cycle_windows = "<C-w><Space>",
       rename_session = "<Space>rn",
@@ -38,14 +38,14 @@ chatgpt.setup({
     submit = "<C-Enter>",
     submit_n = "<Enter>",
   },
-  -- openai_params = {
-  --   model = "gpt-3.5-turbo",
-  --   frequency_penalty = 0,
-  --   presence_penalty = 0,
-  --   max_tokens = 3000,
-  --   temperature = 0,
-  --   top_p = 1,
-  --   n = 1,
-  -- },
-  predefined_chat_gpt_prompts = "https://gist.githubusercontent.com/kohane27/0758d6575aeec909227493224c80c75d/raw/9a3917075ee34f05b503861bcfe53e2348e2999c/prompt_v3.csv",
+  openai_params = {
+    model = "gpt-3.5-turbo",
+    frequency_penalty = 0,
+    presence_penalty = 0,
+    max_tokens = 3000,
+    temperature = 0,
+    top_p = 1,
+    n = 1,
+  },
+  predefined_chat_gpt_prompts = "https://raw.githubusercontent.com/kohane27/nvim-config/main/gpt_prompt.csv",
 })
