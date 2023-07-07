f0c2e3fcbd4ca9852d51b5f58f7c6aef12557bb2
kohane27 <charuphoniu4173@gmail.com>
2023-07-07 11:09:27 +0800

    fix: update rainbow plugin

---
+++ lua/plugins/rainbow-delimiters.lua
@@ -0,0 +1,24 @@
+local status_ok, rainbow_delimiters = pcall(require, "rainbow-delimiters")
+if not status_ok then
+  print("rainbow-delimiters not working")
+end
+
+vim.g.rainbow_delimiters = {
+  strategy = {
+    [""] = rainbow_delimiters.strategy["global"],
+    vim = rainbow_delimiters.strategy["local"],
+  },
+  query = {
+    [""] = "rainbow-delimiters",
+    lua = "rainbow-blocks",
+  },
+  highlight = {
+    "RainbowDelimiterRed",
+    "RainbowDelimiterYellow",
+    "RainbowDelimiterBlue",
+    "RainbowDelimiterOrange",
+    "RainbowDelimiterGreen",
+    "RainbowDelimiterViolet",
+    "RainbowDelimiterCyan",
+  },
+}
