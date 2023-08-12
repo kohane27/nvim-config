b85c24b549ece8ea16a2df26a4b1d3c4da79b3a1
kohane27 <kohane27@protonmail.com>
2023-08-12 11:19:57 +0800

    fix: configure trouble.nvim
    Former-commit-id: dd6c6726db1544806c9b9968f3f7a887fb259c02
    Former-commit-id: 3ffb38a79f0d53254769af58694b864b05fb5dfd

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -3,4 +3,24 @@ if not status_ok then
   print("trouble not working")
 end
 
-trouble.setup({})
+trouble.setup({
+  height = 10, -- height of the trouble list when position is top or bottom
+  width = 50, -- width of the list when position is left or right
+  action_keys = {
+    -- map to {} to remove a mapping
+    -- close = {},
+    jump = { "<cr>", "<tab>" }, -- jump to the diagnostic or open / close folds
+    open_split = { "<c-w>s" }, -- open buffer in new split
+    open_vsplit = { "<c-w>v" }, -- open buffer in new vsplit
+    open_tab = { "<c-w>t" }, -- open buffer in new tab
+    jump_close = { "o" }, -- jump to the diagnostic and close the list
+    toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
+    switch_severity = "s", -- switch "diagnostics" severity filter level to HINT / INFO / WARN / ERROR
+    hover = "K", -- opens a small popup with the full multiline message
+    preview = "p", -- preview the diagnostic location
+    close_folds = { "zM", "zm" }, -- close all folds
+    open_folds = { "zR", "zr" }, -- open all folds
+    toggle_fold = { "zA", "za" }, -- toggle fold of current file
+  },
+  win_config = { border = "rounded" },
+})
