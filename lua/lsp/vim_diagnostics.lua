34d3ebcd0314c178167171ebb0c64531e7e89eec
kohane27 <charuphoniu4173@gmail.com>
2023-10-26 17:55:10 +0800

    fix: remove deprecated nerd font

--- lua/lsp/vim_diagnostics.lua
+++ lua/lsp/vim_diagnostics.lua
@@ -3,7 +3,7 @@
 local signs = {
   { name = "DiagnosticSignError", text = "" },
   { name = "DiagnosticSignWarn", text = "" },
-  { name = "DiagnosticSignHint", text = "" },
+  { name = "DiagnosticSignHint", text = "󰌵" },
   { name = "DiagnosticSignInfo", text = "" },
 }
 
