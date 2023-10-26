c953381438fe4301f091a7f89ad3be060da73750
kohane27 <kohane27@protonmail.com>
2023-10-26 17:55:10 +0800

    fix: remove deprecated nerd font
    Former-commit-id: 34d3ebcd0314c178167171ebb0c64531e7e89eec
    Former-commit-id: f1505771944f134d3a83b5a93ee859b598b2d677

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -22,7 +22,7 @@ ufo.setup({
   -- adding folded line number instead of "..."
   fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
     local newVirtText = {}
-    local suffix = ("  %d "):format(endLnum - lnum)
+    local suffix = (" 󰁂 %d "):format(endLnum - lnum)
     local sufWidth = vim.fn.strdisplaywidth(suffix)
     local targetWidth = width - sufWidth
     local curWidth = 0
