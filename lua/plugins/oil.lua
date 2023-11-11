c93a3424d603f36b054bb3bcd8e17b1c334dc39e
kohane27 <kohane27@protonmail.com>
2023-11-11 20:07:37 +0800

    chore: configure `oil.nvim`
    Former-commit-id: 980d6a70c742b8655dfb6262caf02394f84ad0d2
    Former-commit-id: 9edc7ad14e81b6bd356930bfadffd927bf73b786

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -35,7 +35,7 @@ oil.setup({
   -- Selecting a new/moved/renamed file or directory will prompt you to save changes first
   prompt_save_on_select_new_entry = true,
   -- Oil will automatically delete hidden buffers after this delay
-  -- set to false to disable cleanup entirely
+  -- set to false to disable cleanup
   -- Note: the cleanup process only starts when none of the oil buffers are currently displayed
   cleanup_delay_ms = 1000,
 
@@ -48,32 +48,49 @@ oil.setup({
     ["<C-w>v"] = "actions.select_vsplit",
     ["<C-w>s"] = "actions.select_split",
     ["<C-w>t"] = "actions.select_tab",
-    ["<C-p>"] = "actions.preview",
     ["<C-q>"] = "actions.close",
-    ["<C-l>"] = "actions.refresh",
-    ["-"] = "actions.parent",
-    ["_"] = "actions.open_cwd",
-    ["`"] = "actions.cd",
-    ["~"] = "actions.tcd",
+    ["<C-r>"] = "actions.refresh",
+    ["<C-h>"] = "actions.parent",
+    ["<C-c>"] = "actions.open_cwd",
+    ["L"] = "actions.cd",
+    ["~"] = false,
     ["gs"] = "actions.change_sort",
+    -- doesn't work in a floating window
+    ["<C-p>"] = false,
     ["gx"] = false,
-    ["g."] = "actions.toggle_hidden",
+    ["g."] = false,
     ["g\\"] = "actions.toggle_trash",
   },
   view_options = {
     show_hidden = true,
     sort = {
+      -- sort order can be "asc" or "desc"
       -- see :help oil-columns to see which columns are sortable
       { "type", "asc" },
       { "name", "asc" },
     },
+  },
+  -- floating window in oil.open_float
+  float = {
+    padding = 2,
+    max_width = 40,
+    max_height = 0,
+    border = "rounded",
+    win_options = {
+      winblend = 0,
+    },
+    -- This is the config that will be passed to nvim_open_win.
+    -- Change values here to customize the layout
+    override = function(conf)
+      return conf
+    end,
   },
   -- Configuration for the actions floating preview window
   preview = {
     -- Width dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
     -- min_width and max_width can be a single value or a list of mixed integer/float types.
     -- max_width = {100, 0.8} means "the lesser of 100 columns or 80% of total"
-    max_width = 0.4,
+    max_width = 0.9,
     -- min_width = {40, 0.4} means "the greater of 40 columns or 40% of total"
     min_width = { 40, 0.4 },
     -- optionally define an integer/float for the exact width of the preview window
@@ -81,17 +98,28 @@ oil.setup({
     -- Height dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
     -- min_height and max_height can be a single value or a list of mixed integer/float types.
     -- max_height = {80, 0.9} means "the lesser of 80 columns or 90% of total"
-    max_height = 0.4,
+    max_height = 0.9,
     -- min_height = {5, 0.1} means "the greater of 5 columns or 10% of total"
     min_height = { 5, 0.1 },
+    -- optionally define an integer/float for the exact height of the preview window
+    height = nil,
+    border = "rounded",
+    win_options = {
+      winblend = 0,
+    },
   },
   -- Configuration for the floating progress window
   progress = {
-    max_width = 0.4,
-    min_width = 0.4,
+    max_width = 0.9,
+    min_width = { 40, 0.4 },
     width = nil,
-    max_height = 0.5,
-    min_height = 0.1,
+    max_height = { 10, 0.9 },
+    min_height = { 5, 0.1 },
     height = nil,
+    border = "rounded",
+    minimized_border = "none",
+    win_options = {
+      winblend = 0,
+    },
   },
 })
