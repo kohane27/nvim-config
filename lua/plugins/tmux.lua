320b8062b6321edf5357c302fbe19720575c0c4a
kohane27 <kohane27@protonmail.com>
2023-12-02 22:25:04 +0800

    fix: move tmux reisze to hydra

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -18,8 +18,8 @@ tmux.setup({
     -- C-S-hjkl
     enable_default_keybindings = false,
     -- sets resize steps for x axis
-    resize_step_x = 4,
+    resize_step_x = 6,
     -- sets resize steps for y axis
-    resize_step_y = 4,
+    resize_step_y = 6,
   },
 })
