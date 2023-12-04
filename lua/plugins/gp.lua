ffb60b049e9f9989b09e0a2a5f3912b98d243677
kohane27 <kohane27@protonmail.com>
2023-12-04 18:23:48 +0800

    feat: `gp.nvim` for in-place edit
    Former-commit-id: ac5dfc3d5cfb7aa162169493486d3e2be16130f1
    Former-commit-id: 86a7af426a9f7c8946df0478a5b848d98ebeefeb

--- lua/plugins/gp.lua
+++ lua/plugins/gp.lua
@@ -7,8 +7,30 @@ gp.setup({
   openai_api_key = os.getenv("OPENAI_API_KEY"),
   cmd_prefix = "Gp",
   curl_params = { "--proxy", "http://127.0.0.1:8888" },
-
-  chat_model = { model = "gpt-4-1106-preview", temperature = 1.1, top_p = 1 },
-  chat_topic_gen_model = "gpt-4-1106-preview",
-  command_model = { model = "gpt-4-1106-preview", temperature = 1.1, top_p = 1 },
+  agents = {
+    {
+      name = "ChatGPT4",
+      chat = true,
+      command = false,
+      model = { model = "gpt-4-1106-preview", temperature = 1.1, top_p = 1 },
+      system_prompt = "You are a general AI assistant.\n\n"
+        .. "The user provided the additional info about how they would like you to respond:\n\n"
+        .. "- If you're unsure don't guess and say you don't know instead.\n"
+        .. "- Ask question if you need clarification to provide better answer.\n"
+        .. "- Think deeply and carefully from first principles step by step.\n"
+        .. "- Zoom out first to see the big picture and then zoom in to details.\n"
+        .. "- Use Socratic method to improve your thinking and coding skills.\n"
+        .. "- Don't elide any code from your output if the answer requires coding.\n"
+        .. "- Take a deep breath; You've got this!\n",
+    },
+    {
+      name = "CodeGPT4",
+      chat = false,
+      command = true,
+      model = { model = "gpt-4-1106-preview", temperature = 0.8, top_p = 1 },
+      system_prompt = "You are an AI working as a code editor.\n\n"
+        .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
+        .. "START AND END YOUR ANSWER WITH:\n\n```",
+    },
+  },
 })
