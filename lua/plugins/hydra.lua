507f2b602ecd8e6634e3f718b1d7f74bf9588c5d
kohane27 <kohane27@protonmail.com>
2023-12-10 23:29:13 +0800

    feat: more commands moved to `hydra.nvim`
    Former-commit-id: 9e15812e3fab7ec8b44dc8976ac02a8078717862
    Former-commit-id: b6a27550d6752a111537569c5ab015baea48096c

--- lua/plugins/hydra.lua
+++ lua/plugins/hydra.lua
@@ -3,9 +3,9 @@ if not status_ok then
   print("hydra not working")
 end
 
--- 5. Text object motions          *object-motions*
--- disable `[[`?
--- add `((` for func and `[[` for class
+--  ╭──────────────────────────────────────────────────────────╮
+--  │ Functions start with ;                                   │
+--  ╰──────────────────────────────────────────────────────────╯
 
 hydra({
   name = "TODO",
@@ -27,11 +27,11 @@ hydra({
 hydra({
   name = "Hunk",
   mode = "n",
-  body = ";h",
+  body = ";g",
   config = {
     invoke_on_body = true,
     on_enter = function()
-      vim.cmd("Gitsigns next_hunk")
+      vim.api.nvim_command("Gitsigns next_hunk")
     end,
   },
   heads = {
@@ -48,48 +48,12 @@ hydra({
   config = {
     invoke_on_body = true,
     on_enter = function()
-      vim.cmd("Lspsaga diagnostic_jump_next")
+      vim.api.nvim_command("Lspsaga diagnostic_jump_next")
     end,
   },
   heads = {
     { "j", "<cmd>Lspsaga diagnostic_jump_next<CR>" },
     { "k", "<cmd>Lspsaga diagnostic_jump_prev<CR>" },
-    { "<Esc>", nil, { exit = true, nowait = true } },
-  },
-})
-
-hydra({
-  name = "Grapple",
-  mode = "n",
-  body = ";g",
-  config = {
-    invoke_on_body = true,
-    on_enter = function()
-      -- not really good to default cycle_forward
-      vim.cmd('lua require("grapple").cycle_forward()')
-    end,
-  },
-  heads = {
-    { "j", '<cmd>lua require("grapple").cycle_forward()<CR>' },
-    { "k", '<cmd>lua require("grapple").cycle_backward()<CR>' },
-    { "<Esc>", nil, { exit = true, nowait = true } },
-  },
-})
-
-hydra({
-  name = "changelist",
-  mode = "n",
-  body = ";c",
-  config = {
-    invoke_on_body = true,
-    on_enter = function()
-      -- TODO: fix
-      vim.cmd("Gitsigns next_hunk")
-    end,
-  },
-  heads = {
-    { "j", "g;zz" },
-    { "k", "g,zz" },
     { "<Esc>", nil, { exit = true, nowait = true } },
   },
 })
@@ -101,8 +65,11 @@ hydra({
   config = {
     invoke_on_body = true,
     on_enter = function()
-      -- TODO: fix
-      vim.cmd("cnext")
+      local status, err = pcall(vim.api.nvim_command, "cnext")
+      if not status then
+        -- Handle the error if "cnext" failed
+        print("Error executing cnext: " .. err)
+      end
     end,
   },
   heads = {
@@ -113,19 +80,187 @@ hydra({
 })
 
 hydra({
-  name = "loclist",
+  name = "changelist",
   mode = "n",
-  body = ";l",
+  body = ";c",
   config = {
     invoke_on_body = true,
     on_enter = function()
-      -- TODO: fix
-      vim.cmd("Gitsigns next_hunk")
+      -- TODO: make sure g; g, below are correct
+      vim.api.nvim_command("normal! g;zz")
     end,
   },
   heads = {
-    { "j", "<cmd>lnext<CR>" },
-    { "k", "<cmd>lprevious<CR>" },
+    { "j", "g,zz" },
+    { "k", "g;zz" },
+    { "<Esc>", nil, { exit = true, nowait = true } },
+  },
+})
+
+--  ╭──────────────────────────────────────────────────────────╮
+--  │ Code Navigation starts with ,                            │
+--  ╰──────────────────────────────────────────────────────────╯
+
+hydra({
+  name = "Function's Start",
+  mode = "n",
+  body = ",f",
+  config = {
+    invoke_on_body = true,
+    on_enter = function()
+      require("nvim-treesitter.textobjects.move").goto_next_start("@function.outer")
+    end,
+  },
+  heads = {
+    {
+      "j",
+      function()
+        require("nvim-treesitter.textobjects.move").goto_next_start("@function.outer")
+      end,
+    },
+    {
+      "k",
+      function()
+        require("nvim-treesitter.textobjects.move").goto_previous_start("@function.outer")
+      end,
+    },
+    { "<Esc>", nil, { exit = true, nowait = true } },
+  },
+})
+
+hydra({
+  name = "Function's End",
+  mode = "n",
+  body = ",F",
+  config = {
+    invoke_on_body = true,
+    on_enter = function()
+      require("nvim-treesitter.textobjects.move").goto_next_end("@function.outer")
+    end,
+  },
+  heads = {
+    {
+      "j",
+      function()
+        require("nvim-treesitter.textobjects.move").goto_next_end("@function.outer")
+      end,
+    },
+    {
+      "k",
+      function()
+        require("nvim-treesitter.textobjects.move").goto_previous_end("@function.outer")
+      end,
+    },
+    { "<Esc>", nil, { exit = true, nowait = true } },
+  },
+})
+
+hydra({
+  name = "Class's Start",
+  mode = "n",
+  body = ",c",
+  config = {
+    invoke_on_body = true,
+    on_enter = function()
+      require("nvim-treesitter.textobjects.move").goto_next_start("@class.outer")
+    end,
+  },
+  heads = {
+    {
+      "j",
+      function()
+        require("nvim-treesitter.textobjects.move").goto_next_start("@class.outer")
+      end,
+    },
+    {
+      "k",
+      function()
+        require("nvim-treesitter.textobjects.move").goto_previous_start("@class.outer")
+      end,
+    },
+    { "<Esc>", nil, { exit = true, nowait = true } },
+  },
+})
+
+hydra({
+  name = "Class's End",
+  mode = "n",
+  body = ",C",
+  config = {
+    invoke_on_body = true,
+    on_enter = function()
+      require("nvim-treesitter.textobjects.move").goto_next_end("@class.outer")
+    end,
+  },
+  heads = {
+    {
+      "j",
+      function()
+        require("nvim-treesitter.textobjects.move").goto_next_end("@class.outer")
+      end,
+    },
+    {
+      "k",
+      function()
+        require("nvim-treesitter.textobjects.move").goto_previous_end("@class.outer")
+      end,
+    },
+    { "<Esc>", nil, { exit = true, nowait = true } },
+  },
+})
+
+-- TODO: try @statement.outer
+
+hydra({
+  name = "Block's Start",
+  mode = "n",
+  body = ",b",
+  config = {
+    invoke_on_body = true,
+    on_enter = function()
+      require("nvim-treesitter.textobjects.move").goto_next_start("@block.outer")
+    end,
+  },
+  heads = {
+    {
+      "j",
+      function()
+        require("nvim-treesitter.textobjects.move").goto_next_start("@block.outer")
+      end,
+    },
+    {
+      "k",
+      function()
+        require("nvim-treesitter.textobjects.move").goto_previous_start("@block.outer")
+      end,
+    },
+    { "<Esc>", nil, { exit = true, nowait = true } },
+  },
+})
+
+hydra({
+  name = "Block's End",
+  mode = "n",
+  body = ",B",
+  config = {
+    invoke_on_body = true,
+    on_enter = function()
+      require("nvim-treesitter.textobjects.move").goto_next_end("@block.outer")
+    end,
+  },
+  heads = {
+    {
+      "j",
+      function()
+        require("nvim-treesitter.textobjects.move").goto_next_end("@block.outer")
+      end,
+    },
+    {
+      "k",
+      function()
+        require("nvim-treesitter.textobjects.move").goto_previous_end("@block.outer")
+      end,
+    },
     { "<Esc>", nil, { exit = true, nowait = true } },
   },
 })
