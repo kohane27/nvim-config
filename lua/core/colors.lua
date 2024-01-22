dfca1a82ae5d1e48803a2c86b717229469beaef1
kohane27 <kohane27@protonmail.com>
2024-01-22 15:19:14 +0800

    fix: TS parser for theme

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -15,6 +15,31 @@ vim.opt.fillchars:append({
 })
 
 require("kanagawa").setup({
+  -- https://github.com/rebelot/kanagawa.nvim/issues/197
+  overrides = function(colors)
+    return {
+      -- update kanagawa to handle new treesitter highlight captures
+      ["@string.regexp"] = { link = "@string.regex" },
+      ["@variable.parameter"] = { link = "@parameter" },
+      ["@exception"] = { link = "@exception" },
+      ["@string.special.symbol"] = { link = "@symbol" },
+      ["@markup.strong"] = { link = "@text.strong" },
+      ["@markup.italic"] = { link = "@text.emphasis" },
+      ["@markup.heading"] = { link = "@text.title" },
+      ["@markup.raw"] = { link = "@text.literal" },
+      ["@markup.quote"] = { link = "@text.quote" },
+      ["@markup.math"] = { link = "@text.math" },
+      ["@markup.environment"] = { link = "@text.environment" },
+      ["@markup.environment.name"] = { link = "@text.environment.name" },
+      ["@markup.link.url"] = { link = "Special" },
+      ["@markup.link.label"] = { link = "Identifier" },
+      ["@comment.note"] = { link = "@text.note" },
+      ["@comment.warning"] = { link = "@text.warning" },
+      ["@comment.danger"] = { link = "@text.danger" },
+      ["@diff.plus"] = { link = "@text.diff.add" },
+      ["@diff.minus"] = { link = "@text.diff.delete" },
+    }
+  end,
   transparent = false, -- do not set background color
   dimInactive = false, -- dim inactive window `:h hl-NormalNC`
   colors = {
