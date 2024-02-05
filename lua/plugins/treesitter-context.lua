92bb8b312e6f0c3fac786f06d342d89c1349e0b9
kohane27 <kohane27@protonmail.com>
2024-02-05 14:26:09 +0800

    chore: update

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -2,6 +2,6 @@ return {
   "nvim-treesitter/nvim-treesitter-context",
   event = "VeryLazy",
   config = function()
-    require("treesitter-context").setup()
+    require("treesitter-context").setup({ enable = false })
   end,
 }
