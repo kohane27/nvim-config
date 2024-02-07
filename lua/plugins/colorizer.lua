61722e579019b0847f8f0c97339a1dcab97f38b0
kohane27 <kohane27@protonmail.com>
2024-02-07 13:50:25 +0800

    chore: update colorizer
    Former-commit-id: ecbd872eae98e45e00161a2c7b501452becc2f63
    Former-commit-id: 447d60c7f10c5fb892dad52e07a10a92b2084566

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,5 +1,27 @@
 return {
   "NvChad/nvim-colorizer.lua",
-  lazy = false,
-  config = true,
+  event = { "BufReadPost", "BufNewFile" },
+  opts = {
+    filetypes = {
+      "typescript",
+      "typescriptreact",
+      "javascript",
+      "javascriptreact",
+      "css",
+      "html",
+      "astro",
+      "lua",
+    },
+    user_default_options = {
+      names = false,
+      rgb_fn = true,
+      hsl_fn = true,
+      tailwind = "both",
+    },
+    buftypes = {},
+  },
+
+  config = function(_, opts)
+    require("colorizer").setup(opts)
+  end,
 }
