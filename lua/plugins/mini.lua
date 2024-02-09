4620aece57d1f2e7d8efd694765ca30027a54106
kohane27 <kohane27@protonmail.com>
2024-02-09 17:45:28 +0800

    fix: combine mini modules
    Former-commit-id: c1204d61f507c5ebc1f9cf23e7886211a43ae04d
    Former-commit-id: eb4f5916cf2b602dd686ea2add0e3a1938b6ea09

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,22 @@
+return {
+  "echasnovski/mini.nvim",
+  version = false,
+  config = function()
+    require("mini.comment").setup({
+      options = {
+        custom_commentstring = function()
+          return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
+        end,
+      },
+    })
+
+    require("mini.misc").setup({
+      -- restore cursor position on file reopen
+      require("mini.misc").setup_restore_cursor(),
+      -- automated change of current directory
+      require("mini.misc").setup_auto_root(),
+    })
+
+    require("mini.trailspace").setup()
+  end,
+}
