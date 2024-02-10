8ab8b8acc5e9d03f85465f3f73817bf458b6d011
kohane27 <kohane27@protonmail.com>
2024-02-10 18:29:30 +0800

    chore: fix color
    Former-commit-id: 7eeee2f5f72c7cb54f68cd7eb2ed14a7c4039a71
    Former-commit-id: bd00741d94b21a0b6b982bf3fcdbd55d26214aac

--- lua/plugins/git-conflict.lua
+++ lua/plugins/git-conflict.lua
@@ -1,6 +1,12 @@
 -- TODO: learn to use it properly
--- add GitConflictListQf
+-- TODO: add GitConflictListQf
+-- NOTE: source code changed
+-- local DEFAULT_ANCESTOR_BG_COLOR = 2244681 -- #223249 (waveBlue1)
 return {
   "akinsho/git-conflict.nvim",
-  config = true,
+  config = function()
+    require("git-conflict").setup({
+      disable_diagnostics = true,
+    })
+  end,
 }
