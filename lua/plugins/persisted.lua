51de1f9592fa91ddbd06cad50e132a3886bf6cc0
kohane27 <kohane27@protonmail.com>
2024-02-15 21:21:25 +0800

    feat: close tree before session saving

--- lua/plugins/persisted.lua
+++ lua/plugins/persisted.lua
@@ -6,5 +6,14 @@ return {
       autoload = true, -- automatically load the session for the cwd on Neovim startup
       follow_cwd = false, -- change session file name to match current working directory if it changes
     })
+
+    -- close nvim-tree before saving
+    vim.api.nvim_create_autocmd({ "User" }, {
+      pattern = "PersistedSavePre",
+      group = vim.api.nvim_create_augroup("PersistedHooks", {}),
+      callback = function()
+        vim.cmd("NvimTreeClose")
+      end,
+    })
   end,
 }
