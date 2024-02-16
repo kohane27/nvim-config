f417d8b00389e40b944aa5c4b7a99a27a304a218
kohane27 <kohane27@protonmail.com>
2024-02-16 09:12:04 +0800

    chore: set up noice
    Former-commit-id: 3d8e3f7670792c0e71776f823f0874d7de58e29c
    Former-commit-id: 75fc3d14c9ede98630e453d9c22d5d5b68fe92a7

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,41 +1,131 @@
 return {
   "folke/noice.nvim",
-  event = "VeryLazy",
+  lazy = false, -- else flashes when opening
   dependencies = {
     "MunifTanjim/nui.nvim",
     "rcarriga/nvim-notify",
     "hrsh7th/nvim-cmp",
   },
   config = function()
-    -- TODO: not working
-    local message_filters = {
-      -- Filter for "X lines yanked" messages
-      {
-        event = "msg_show",
-        pattern = "%d+ lines yanked",
-        hide = true,
-      },
-      -- Filter for "X more lines" messages
-      {
-        event = "msg_show",
-        pattern = "%d+ more lines",
-        hide = true,
-      },
-      -- Filter for "X lines less; before #Y  Z seconds ago" messages
-      {
-        event = "msg_show",
-        pattern = "%d+ line[s]? less; before #%d+%s+%d+ seconds ago",
-        hide = true,
-      },
-      -- Filter for "filename  X lines, Y bytes written" messages
-      {
-        event = "msg_show",
-        pattern = ".+ %d+L, %d+B written$",
-        hide = true,
-      },
-    }
     require("noice").setup({
-      filter = message_filters,
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "^%d+ changes?; after #%d+" },
+              { find = "^%d+ changes?; before #%d+" },
+              { find = "^Hunk %d+ of %d+$" },
+              { find = "^%d+ fewer lines;?" },
+              { find = "^%d+ more lines?;?" },
+              { find = "^%d+ line less;?" },
+              { find = "^Already at newest change" },
+              { kind = "wmsg" },
+              { kind = "emsg", find = "E486" },
+              { kind = "quickfix" },
+            },
+          },
+          view = "mini",
+        },
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "^%d+ lines .ed %d+ times?$" },
+              { find = "^%d+ lines yanked$" },
+              { kind = "emsg", find = "E490" },
+              { kind = "search_count" },
+            },
+          },
+          opts = { skip = true },
+        },
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              -- hydra.nvim
+              { find = "SmoothScroll" },
+              { find = "Diagnostics" },
+              { find = "TODO" },
+              { find = "changelist" },
+              { find = "Hunk" },
+              { find = "Block's Start" },
+              { find = "Block's End" },
+              { find = "Class's Start" },
+              { find = "Class's End" },
+              { find = "Function's Start" },
+              { find = "Function's End" },
+            },
+          },
+          view = "mini",
+        },
+        {
+          filter = {
+            event = "notify",
+            any = {
+              { find = "^No code actions available$" },
+              { find = "^No information available$" },
+            },
+          },
+          view = "mini",
+        },
+      },
+      views = {
+        -- Clean cmdline_popup + palette
+        cmdline_popup = {
+          position = {
+            row = 10,
+            col = "50%",
+          },
+          border = {
+            style = "rounded",
+            -- padding = { 2, 3 },
+          },
+          size = {
+            min_width = 60,
+            width = "auto",
+            height = "auto",
+          },
+          win_options = {
+            winhighlight = { NormalFloat = "NormalFloat", FloatBorder = "FloatBorder" },
+          },
+        },
+        cmdline_popupmenu = {
+          relative = "editor",
+          position = {
+            row = 13,
+            col = "50%",
+          },
+          size = {
+            width = 60,
+            height = "auto",
+            max_height = 15,
+          },
+          border = {
+            style = "none",
+            padding = { 0, 3 },
+          },
+          win_options = {
+            winhighlight = { NormalFloat = "NormalFloat", FloatBorder = "NoiceCmdlinePopupBorder" },
+          },
+        },
+        hover = {
+          border = {
+            style = "single",
+          },
+        },
+        confirm = {
+          border = {
+            style = "single",
+          },
+        },
+        popup = {
+          border = {
+            style = "single",
+          },
+        },
+      },
       messages = { view_search = false }, -- using `nvim-hlslens`
       lsp = {
         progress = { enabled = false },
@@ -48,7 +138,7 @@ return {
       },
       presets = {
         bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = false, -- cmdline not at the top
+        command_palette = true, -- cmdline not at the top
         long_message_to_split = true, -- long messages will be sent to a split
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = false, -- add a border to hover docs and signature help
