602ec36037d07c07f9aa51c538ec4f6f2e0de938
kohane27 <kohane27@protonmail.com>
2024-02-17 23:02:33 +0800

    feat: move functions to utils.lua
    Former-commit-id: 409c28b12e7f7e5db2e97397ee6fa56ea6961f5b
    Former-commit-id: c50fbb873357fa215d15d61c6319caeb90e3eddc

--- lua/plugins/legendary.lua
+++ lua/plugins/legendary.lua
@@ -1,193 +1,3 @@
-local function get_current_buffer_content()
-  local vim = vim -- Access the global vim object
-  local bufnr = vim.api.nvim_get_current_buf() -- Get the current buffer number
-  -- Check if the buffer is loaded to avoid errors
-  if vim.api.nvim_buf_is_loaded(bufnr) then
-    local filename = vim.api.nvim_buf_get_name(bufnr)
-    local filetype = vim.api.nvim_buf_get_option(bufnr, "filetype")
-    local content = table.concat(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false), "\n")
-    local relative_filename = vim.fn.fnamemodify(filename, ":.")
-    local formatted_content = string.format("%s:\n\n```%s\n%s\n```\n", relative_filename, filetype, content)
-    -- Store the formatted content in the clipboard
-    vim.fn.setreg("+", formatted_content)
-    require("notify")("Current buffer content stored in clipboard")
-  end
-end
-
-local function get_all_buffer_content()
-  local buffers_info = {}
-  local vim = vim -- Access the global vim object
-  -- Iterate over all buffers
-  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-    -- Check if the buffer is loaded to avoid errors
-    if vim.api.nvim_buf_is_loaded(bufnr) then
-      local filetype = vim.api.nvim_buf_get_option(bufnr, "filetype")
-      if filetype ~= "markdown" and filetype ~= "incline" then
-        local filename = vim.api.nvim_buf_get_name(bufnr)
-        -- Only proceed if the filename is not empty
-        if filename ~= "" then
-          local relative_filename = vim.fn.fnamemodify(filename, ":.")
-          local content = table.concat(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false), "\n")
-          -- Only add to buffers_info if there is content
-          if content ~= "" then
-            -- Format the output
-            table.insert(buffers_info, string.format("%s:\n\n```%s\n%s\n```\n", relative_filename, filetype, content))
-          end
-        end
-      end
-    end
-  end
-  -- Concatenate all buffer information
-  local result = table.concat(buffers_info, "\n")
-  -- Store the result in the clipboard
-  vim.fn.setreg("+", result)
-  require("notify")("All Buffer content stored in clipboard")
-end
-
-local function get_visual_selection()
-  vim.cmd('noau normal! "vy"')
-  local text = vim.fn.getreg("v")
-  vim.fn.setreg("v", {})
-  text = string.gsub(text, "\n", "")
-  if #text > 0 then
-    return text
-  else
-    return ""
-  end
-end
-
-local function gp_choose_agent(opts)
-  local pickers = require("telescope.pickers")
-  local finders = require("telescope.finders")
-  local conf = require("telescope.config").values
-  local action_state = require("telescope.actions.state")
-  local actions = require("telescope.actions")
-  opts = opts or {}
-
-  local agents = {
-    {
-      description = "React Software Engineer",
-      command = "React-Software-Engineer",
-    },
-    {
-      description = "Task Breakdown Helper",
-      command = "Software-Engineering-Task-Breakdown-Helper",
-    },
-    { description = "Arch Linux Expert", command = "Arch-Linux-Expert" },
-    { description = "Neovim Expert", command = "Neovim-Expert" },
-  }
-
-  -- Create the picker
-  pickers
-    .new(opts, {
-      prompt_title = "Choose an Agent",
-      finder = finders.new_table({
-        results = agents,
-        entry_maker = function(entry)
-          return {
-            value = entry,
-            display = entry.description,
-            ordinal = entry.description,
-          }
-        end,
-      }),
-      sorter = conf.generic_sorter(opts),
-      attach_mappings = function(prompt_bufnr)
-        actions.select_default:replace(function()
-          local selection = action_state.get_selected_entry()
-          actions.close(prompt_bufnr)
-          local command = selection.value.command
-          vim.api.nvim_exec("GpChatNew vsplit", false)
-          vim.api.nvim_exec("GpAgent " .. command, false)
-        end)
-        return true
-      end,
-      initial_mode = "normal",
-      layout_config = {
-        width = 0.6,
-        height = 0.6,
-      },
-    })
-    :find()
-end
-
-local function live_grep_from_project_git_root()
-  if vim.fn.getcwd() == os.getenv("HOME") then
-    return print("Current directory is home. Exiting")
-  end
-  local function is_git_repo()
-    vim.fn.system("git rev-parse --is-inside-work-tree")
-    return vim.v.shell_error == 0
-  end
-  local function get_git_root()
-    local dot_git_path = vim.fn.finddir(".git", ".;")
-    return vim.fn.fnamemodify(dot_git_path, ":h")
-  end
-  local opts = {}
-  if is_git_repo() then
-    opts = { cwd = get_git_root() }
-  end
-  -- If in visual mode, get the selected text and add it to opts
-  if vim.fn.mode() == "v" or vim.fn.mode() == "V" or vim.fn.mode() == "^V" then
-    opts.default_text = get_visual_selection()
-  end
-  -- require("telescope.builtin").live_grep(opts)
-  require("telescope").extensions.egrepify.egrepify(opts)
-end
-
-local function find_files_from_project_git_root()
-  if vim.fn.getcwd() == os.getenv("HOME") then
-    return print("Current directory is home. Exiting")
-  end
-  local function is_git_repo()
-    vim.fn.system("git rev-parse --is-inside-work-tree")
-    return vim.v.shell_error == 0
-  end
-  local function get_git_root()
-    local dot_git_path = vim.fn.finddir(".git", ".;")
-    return vim.fn.fnamemodify(dot_git_path, ":h")
-  end
-  local opts = {}
-  if is_git_repo() then
-    opts = {
-      cwd = get_git_root(),
-    }
-  end
-  require("telescope.builtin").find_files(opts)
-end
-
-local function markdown_preview()
-  local buf = vim.api.nvim_create_buf(false, true)
-  local current_buf = vim.api.nvim_get_current_buf()
-
-  local lines = vim.api.nvim_buf_get_lines(current_buf, 0, -1, false)
-  local markdownm = table.concat(lines, "\n")
-
-  local sanitized = markdownm:gsub("'", "'\\''")
-  local sanitized_final = "'" .. sanitized .. "'"
-
-  vim.cmd("rightbelow vert sbuffer " .. buf)
-
-  local new_win = vim.api.nvim_get_current_win()
-  vim.api.nvim_win_set_option(new_win, "number", false)
-  vim.api.nvim_win_set_option(new_win, "cursorline", false)
-  vim.api.nvim_win_set_option(new_win, "relativenumber", false)
-
-  vim.fn.termopen("glow <( echo " .. sanitized_final .. ")\n")
-end
-
--- local function close_all_but_current_buffer()
---   for _, e in ipairs(require("bufferline").get_elements().elements) do
---     vim.schedule(function()
---       if e.id == vim.api.nvim_get_current_buf() then
---         return
---       else
---         vim.cmd("bd " .. e.id)
---       end
---     end)
---   end
--- end
-
 return {
   "mrjones2014/legendary.nvim",
   event = "VeryLazy",
@@ -239,7 +49,7 @@ return {
         {
           "<C-g>",
           function()
-            find_files_from_project_git_root()
+            require("core.utils").find_files_from_project_git_root()
           end,
           description = "Telescope: Find Files",
         },
@@ -270,7 +80,7 @@ return {
         {
           "<leader>fg",
           function()
-            live_grep_from_project_git_root()
+            require("core.utils").live_grep_from_project_git_root()
           end,
           description = "Telescope: Find Text",
           mode = { "n", "x" },
@@ -289,7 +99,7 @@ return {
               require("telescope.builtin").current_buffer_fuzzy_find()
             end,
             v = function()
-              local text = get_visual_selection()
+              local text = require("core.utils").get_visual_selection()
               require("telescope.builtin").current_buffer_fuzzy_find({ default_text = text })
             end,
           },
@@ -558,21 +368,21 @@ return {
         {
           "<leader>sp",
           function()
-            get_current_buffer_content()
+            require("core.utils").get_current_buffer_content()
           end,
           description = "GPT: Get Current Buffer Content",
         },
         {
           "<leader>sP",
           function()
-            get_all_buffer_content()
+            require("core.utils").get_all_buffer_content()
           end,
           description = "GPT: Get All Buffer Content",
         },
         {
           "<leader>sf",
           function()
-            gp_choose_agent()
+            require("core.utils").gp_choose_agent()
           end,
           description = "GPT: Choose an Agent",
         },
