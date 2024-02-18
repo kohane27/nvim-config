610afe7b2f497b8ad03d0319ed42e8d4753e3343
kohane27 <kohane27@protonmail.com>
2024-02-18 15:53:27 +0800

    chore: update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,34 +1,36 @@
 return {
   "rebelot/kanagawa.nvim",
+  -- treesitter wrong syntax error; markdown missing highlight
+  commimt = "7b411f9e66c6f4f6bd9771f3e5affdc468bcbbd2",
   lazy = false, -- load immediately when starting Neovim
   priority = 1000, -- Load the colorscheme before other non-lazy-loaded plugins
   opts = {
     -- https://github.com/rebelot/kanagawa.nvim/issues/197
-    -- overrides = function(colors)
-    --   return {
-    --     -- update kanagawa to handle new treesitter highlight captures
-    --     ["@comment.todo"] = { link = "@text.todo" },
-    --     ["@string.regexp"] = { link = "@string.regex" },
-    --     ["@variable.parameter"] = { link = "@parameter" },
-    --     ["@exception"] = { link = "@exception" },
-    --     ["@string.special.symbol"] = { link = "@symbol" },
-    --     ["@markup.strong"] = { link = "@text.strong" },
-    --     ["@markup.italic"] = { link = "@text.emphasis" },
-    --     ["@markup.heading"] = { link = "@text.title" },
-    --     ["@markup.raw"] = { link = "@text.literal" },
-    --     ["@markup.quote"] = { link = "@text.quote" },
-    --     ["@markup.math"] = { link = "@text.math" },
-    --     ["@markup.environment"] = { link = "@text.environment" },
-    --     ["@markup.environment.name"] = { link = "@text.environment.name" },
-    --     ["@markup.link.url"] = { link = "Special" },
-    --     ["@markup.link.label"] = { link = "Identifier" },
-    --     ["@comment.note"] = { link = "@text.note" },
-    --     ["@comment.warning"] = { link = "@text.warning" },
-    --     ["@comment.danger"] = { link = "@text.danger" },
-    --     ["@diff.plus"] = { link = "@text.diff.add" },
-    --     ["@diff.minus"] = { link = "@text.diff.delete" },
-    --   }
-    -- end,
+    overrides = function(colors)
+      return {
+        -- update kanagawa to handle new treesitter highlight captures
+        ["@comment.todo"] = { link = "@text.todo" },
+        ["@string.regexp"] = { link = "@string.regex" },
+        ["@variable.parameter"] = { link = "@parameter" },
+        ["@exception"] = { link = "@exception" },
+        ["@string.special.symbol"] = { link = "@symbol" },
+        ["@markup.strong"] = { link = "@text.strong" },
+        ["@markup.italic"] = { link = "@text.emphasis" },
+        ["@markup.heading"] = { link = "@text.title" },
+        ["@markup.raw"] = { link = "@text.literal" },
+        ["@markup.quote"] = { link = "@text.quote" },
+        ["@markup.math"] = { link = "@text.math" },
+        ["@markup.environment"] = { link = "@text.environment" },
+        ["@markup.environment.name"] = { link = "@text.environment.name" },
+        ["@markup.link.url"] = { link = "Special" },
+        ["@markup.link.label"] = { link = "Identifier" },
+        ["@comment.note"] = { link = "@text.note" },
+        ["@comment.warning"] = { link = "@text.warning" },
+        ["@comment.danger"] = { link = "@text.danger" },
+        ["@diff.plus"] = { link = "@text.diff.add" },
+        ["@diff.minus"] = { link = "@text.diff.delete" },
+      }
+    end,
     transparent = false, -- do not set background color
     dimInactive = false, -- dim inactive window `:h hl-NormalNC`
     colors = {
