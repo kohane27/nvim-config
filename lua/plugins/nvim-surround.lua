1ae85cd99ab848dc19065aedbd9bcb20fca3e82b
kohane27 <kohane27@protonmail.com>
2024-02-19 22:22:27 +0800

    feat: use `mini.ai`

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -1,10 +1,7 @@
 return {
   "kylechui/nvim-surround",
   dependencies = {
-    -- fix: https://github.com/kylechui/nvim-surround/issues/153
-    -- TODO: maybe replace with mini.ai?
-    -- and more text objects
-    "wellle/targets.vim",
+    -- `mini.ai` fixed https://github.com/kylechui/nvim-surround/issues/153
   },
   event = "VeryLazy",
   opts = {
