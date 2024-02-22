5d0d99cf74479bc910a30db8c043604eaaea2aa0
kohane27 <kohane27@protonmail.com>
2024-02-22 09:04:15 +0800

    chore: cleanup
    chore: cleanup
    
    chore: cleanup

---
+++ lua/plugins/mkdnflow.lua
@@ -0,0 +1,24 @@
+-- `mkdnflow` conflicting with `vim-wiki`
+return {
+  "jakewvincent/mkdnflow.nvim",
+  ft = "markdown",
+  event = "VeryLazy",
+  -- lazy = false,
+  opts = {
+    -- links = {
+    --   implicit_extension = nil,
+    --   transform_implicit = true,
+    --   transform_explicit = function(text)
+    --     text = text:gsub("<", " ")
+    --     text = text:gsub(">", " ")
+    --     return text
+    --   end,
+    -- },
+    to_do = {
+      symbols = { " ", "x" },
+    },
+  },
+  config = function(_, opts)
+    require("mkdnflow").setup(opts)
+  end,
+}
