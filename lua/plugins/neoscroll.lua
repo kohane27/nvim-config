b1a1da514cc5a48c369934f44db8c7cf3c2b18ce
kohane27 <kohane27@protonmail.com>
2024-05-25 19:56:07 +0800

    chore: clean up
    Former-commit-id: 8b23038fb5cc1bc6418bf3291224e5292469c6ff
    Former-commit-id: d599b83afb0ad99de24a9ead548cf837348de7ad

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -5,25 +5,13 @@ return {
   opts = {
     mappings = { "<C-u>", "<C-d>", "zz" },
     hide_cursor = false,
-    stop_eof = true, -- Stop at <EOF> when scrolling downwards
-    respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-    cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-    easing_function = nil, -- Default easing function
-    performance_mode = false, -- Disable "Performance Mode" on all buffers.
   },
   config = function(_, opts)
     require("neoscroll").setup(opts)
-
-    -- local t = {}
-    -- -- Syntax: t[keys] = {function, {function arguments}}
-    -- t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "400", "sine", [['hook']] } }
-    -- t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "400", "sine", [['hook']] } }
-    -- t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "350", "quadratic", [['hook']] } }
-    -- t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "500", "quadratic", [['hook']] } }
-    -- t["zt"] = { "zt", { "250" } }
-    -- t["zz"] = { "zz", { "250" } }
-    -- t["zb"] = { "zb", { "250" } }
-    --
-    -- require("neoscroll.config").set_mappings(t)
+    local t = {}
+    t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "200" } }
+    t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "200" } }
+    t["zz"] = { "zz", { "100" } }
+    require("neoscroll.config").set_mappings(t)
   end,
 }
