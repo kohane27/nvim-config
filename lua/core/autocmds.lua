66bd929f2fab4dad8b6c932523fce26774c1f33b
kohane27 <kohane27@protonmail.com>
2024-05-27 11:08:40 +0800

    fix: use `vim.fn.hostname()` over `substitutes--.txt` and convert vimscript to lua
    Former-commit-id: c7af731f441bfc58c364fc4540ab92939648c465
    Former-commit-id: 949e937356839dadd850948e14d681024e2b8a3d

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,65 +1,52 @@
--- ╭──────────────────────────────────────────────────────────╮
--- │ Autocommand functions                                    │
--- │ Define autocommands with Lua APIs                        │
--- │ See: h:api-autocmd, h:augroup                            │
--- ╰──────────────────────────────────────────────────────────╯
+-- stylua: ignore start
 
-vim.cmd([[
-  augroup _general_settings
-    autocmd!
-    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR>
-    autocmd BufWinEnter * :set formatoptions-=cro
-    autocmd FileType qf set nobuflisted
-  augroup end
+-- local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
 
-  augroup _auto_resize
-    autocmd!
-    autocmd VimResized * tabdo wincmd =
-  augroup end
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {'qf', 'help', 'man', 'lspinfo'},
+  callback = function() vim.api.nvim_buf_set_keymap(0, 'n', 'q', ':close<CR>', { noremap = true, silent = true, buffer = true }) end
+})
 
-  augroup _tmux_rename
-    autocmd!
-    if exists('$TMUX')
+vim.api.nvim_create_autocmd('BufWinEnter', { pattern = '*', command = 'set formatoptions-=cro' })
 
-      let data = readfile($HOME .'/.config/nvim/substitutes--.txt')
-      let cwd = fnamemodify(getcwd(), ':t')
-      let shortened_cwd = substitute(cwd, data[0], "", "")
-      autocmd BufReadPost,FileReadPost,BufNewFile,BufEnter * call system("tmux rename-window " . shortened_cwd . "/" . expand("%:t"))
-
-      " autocmd VimLeave,FocusLost * call system("tmux rename-window \"\"") " to enable auto-rename again
-    endif
-    augroup end
-
-    "" show line diagnostics automatically in hover window
-    "autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, { focus = false })
-
-  " clear `jumplist`
-  autocmd VimEnter * :clearjumps
-
-  " clear marks
-  autocmd VimEnter * :delmarks a-zA-Z0-9
-]])
-
-local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
-local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
+-- quickfix buffers are not listed in the buffer list
+vim.api.nvim_create_autocmd('FileType', { pattern = 'qf', command = 'set nobuflisted' })
 
 -- Highlight on yank
-augroup("YankHighlight", { clear = true })
-autocmd("TextYankPost", {
-  group = "YankHighlight",
-  callback = function()
-    vim.highlight.on_yank({ higroup = "IncSearch", timeout = "300" })
-  end,
-})
+autocmd("TextYankPost", { callback = function() vim.highlight.on_yank({ higroup = "IncSearch", timeout = "300" }) end })
 
 -- Enable spellcheck
-autocmd("FileType", {
-  pattern = { "gitcommit", "text" },
-  callback = function()
-    vim.opt_local.spell = true
-  end,
-})
+autocmd("FileType", { pattern = { "gitcommit", "text" }, callback = function() vim.opt_local.spell = true end })
 
 -- Disable diagnostics in node_modules (0 is current buffer only)
-autocmd("BufRead", { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" })
-autocmd("BufNewFile", { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" })
+vim.api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, { pattern = '*/node_modules/*', callback = function() vim.diagnostic.disable(0) end })
+
+-- Show line diagnostics automatically in hover window
+-- vim.api.nvim_create_autocmd({'CursorHold', 'CursorHoldI'},
+-- { pattern = '*', callback = function() vim.diagnostic.open_float(nil, { focus = false }) end
+-- })
+
+-- Clear `jumplist`
+vim.api.nvim_create_autocmd('VimEnter', { pattern = '*', command = 'clearjumps' })
+
+-- Clear marks
+vim.api.nvim_create_autocmd('VimEnter', { pattern = '*', command = 'delmarks a-zA-Z0-9' })
+
+-- Auto resize
+vim.api.nvim_create_autocmd('VimResized', { pattern = '*', command = 'tabdo wincmd =' })
+
+-- Tmux rename
+if vim.fn.exists("$TMUX") == 1 then
+  -- local host = vim.fn.hostname()
+  -- print("host", host)
+  local cwd = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+  -- print("cwd", cwd)
+  -- local shortened_cwd = string.gsub(cwd, host, "")
+  -- print("shortened_cwd", shortened_cwd)
+  vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost", "BufNewFile", "BufEnter" }, {
+    pattern = "*",
+    callback = function() vim.fn.system("tmux rename-window " .. cwd .. "/" .. vim.fn.expand("%:t")) end,
+  })
+end
+-- stylua: ignore end
