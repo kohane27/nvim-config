e65cf65515ee9be532774dcfc995dbe3a5d10b92
kohane27 <kohane27@protonmail.com>
2024-05-29 16:20:18 +0800

    fix: telescope-frecency

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,5 +1,15 @@
 return {
   "rcarriga/nvim-notify",
-  config = true,
   enabled = not vim.g.started_by_firenvim,
+  config = function()
+    local banned_patterns = { "Telescope-Frecency" }
+    vim.notify = function(msg, ...)
+      for _, pattern in ipairs(banned_patterns) do
+        if string.match(msg, pattern) then
+          return
+        end
+      end
+      require("notify")(msg, ...)
+    end
+  end,
 }
