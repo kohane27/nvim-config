cb98da8b7baeb9e1f50574810f061300740f31df
kohane27 <kohane27@protonmail.com>
2024-06-10 09:09:25 +0800

    fix: matching >1 digit

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -2,7 +2,7 @@ return {
   "rcarriga/nvim-notify",
   enabled = not vim.g.started_by_firenvim,
   config = function()
-    local banned_patterns = { "removed %d missing entries." }
+    local banned_patterns = { "removed %d+ missing entries." }
     vim.notify = function(msg, ...)
       for _, pattern in ipairs(banned_patterns) do
         if string.match(msg, pattern) then
