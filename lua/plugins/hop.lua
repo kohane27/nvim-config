5d5204d0e09350e9c48fadaf8a048a240f610bd4
kohane27 <kohane27@protonmail.com>
2024-06-16 14:22:27 +0800

    fix: work in other modes

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -7,45 +7,24 @@ return {
     dim_unmatched = false,
     teasing = false,
   },
+
+   -- stylua: ignore
   config = function(_, opts)
+    vim.api.nvim_set_hl(0, "HopNextKey", { fg = "#000000", bg = "#CCFF88", bold = true })
     local hop = require("hop")
     local directions = require("hop.hint").HintDirection
 
-    vim.keymap.set("", "f", function()
-      hop.hint_char2({
-        direction = directions.AFTER_CURSOR,
-        current_line_only = true,
-        jump_on_sole_occurrence = false,
-      })
-    end, { remap = true })
+    local keymap = {
+      ["f"] = function() hop.hint_char2({ direction = directions.AFTER_CURSOR, current_line_only = true, jump_on_sole_occurrence = false, }) end,
+      ["F"] = function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, jump_on_sole_occurrence = false, }) end,
+      ["t"] = function() hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, jump_on_sole_occurrence = false, hint_offset = -1, }) end,
+      ["T"] = function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, jump_on_sole_occurrence = false, hint_offset = 1, }) end,
+    }
 
-    vim.keymap.set("", "F", function()
-      hop.hint_char1({
-        direction = directions.BEFORE_CURSOR,
-        current_line_only = true,
-        jump_on_sole_occurrence = false,
-      })
-    end, { remap = true })
-
-    vim.keymap.set("", "t", function()
-      hop.hint_char1({
-        direction = directions.AFTER_CURSOR,
-        current_line_only = true,
-        jump_on_sole_occurrence = false,
-        hint_offset = -1,
-      })
-    end, { remap = true })
-
-    vim.keymap.set("", "T", function()
-      hop.hint_char1({
-        direction = directions.BEFORE_CURSOR,
-        current_line_only = true,
-        jump_on_sole_occurrence = false,
-        hint_offset = 1,
-      })
-    end, { remap = true })
-
-    vim.api.nvim_set_hl(0, "HopNextKey", { fg = "#000000", bg = "#CCFF88", bold = true })
+    local modes = { "n", "x", "o" }
+    for key, func in pairs(keymap) do
+      vim.keymap.set(modes, key, func, { remap = true })
+    end
 
     hop.setup(opts)
   end,
