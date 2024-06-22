a913411e8c1b4e8c86a5a55a40597cbd745d37d8
kohane27 <kohane27@protonmail.com>
2024-06-22 12:15:45 +0800

    fix: bufferline with kanagawa

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,43 +1,52 @@
 return {
   "akinsho/bufferline.nvim",
   lazy = false,
+  priority = 3000,
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    options = {
-      numbers = "ordinal",
-      right_mouse_command = false,
-      middle_mouse_command = false,
-
-      -- always show file's immediate parent
-      -- name_formatter = function(buf)
-      --   local path = buf.path
-      --   local parts = {}
-      --   for part in string.gmatch(path, "[^/\\]+") do
-      --     table.insert(parts, part)
-      --   end
-      --
-      --   local len = #parts
-      --   if len > 1 then
-      --     return parts[len - 1] .. "/" .. parts[len]
-      --   else
-      --     return parts[len]
-      --   end
-      -- end,
-
-      max_name_length = 50,
-      max_prefix_length = 30,
-      truncate_names = false,
-      offsets = { { filetype = "NvimTree", text = "File Explorer", text_align = "left", separator = true } },
-      get_element_icon = function(element)
-        local icon, hl = require("nvim-web-devicons").get_icon_by_filetype(element.filetype, { default = false })
-        return icon, hl
-      end,
-      show_buffer_close_icons = false,
-      show_close_icon = false,
-      hover = { enabled = false },
-    },
-  },
   config = function(_, opts)
-    require("bufferline").setup(opts)
+    local groups = require("bufferline.groups")
+    local web_devicons = require("nvim-web-devicons")
+
+    require("bufferline").setup({
+      options = {
+        right_mouse_command = false,
+        middle_mouse_command = false,
+        modified_icon = "",
+        indicator = { style = "underline" },
+
+        -- always show file's immediate parent
+        -- name_formatter = function(buf)
+        --   local path = buf.path
+        --   local parts = {}
+        --   for part in string.gmatch(path, "[^/\\]+") do
+        --     table.insert(parts, part)
+        --   end
+        --
+        --   local len = #parts
+        --   if len > 1 then
+        --     return parts[len - 1] .. "/" .. parts[len]
+        --   else
+        --     return parts[len]
+        --   end
+        -- end,
+
+        max_name_length = 50,
+        max_prefix_length = 30,
+        truncate_names = false,
+        offsets = { { filetype = "NvimTree", text = "File Explorer", text_align = "left", separator = true } },
+
+        show_buffer_icons = false, -- too flashy; need pin
+        -- get_element_icon = function(element)
+        --   local icon, hl = web_devicons.get_icon_by_filetype(element.filetype, { default = false })
+        --   return icon, hl
+        -- end,
+
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        hover = { enabled = false },
+
+        groups = { items = { groups.builtin.pinned:with({ icon = "" }) } },
+      },
+    })
   end,
 }
