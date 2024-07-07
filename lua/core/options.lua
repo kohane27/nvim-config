ae1b88feb1ed1d86d7b64fb9ac0c341c27f7af57
kohane27 <kohane27@protonmail.com>
2024-07-07 09:33:27 +0800

    chore: cleanup
    Former-commit-id: bb607577cee065924c3df4e7410a78510ac9bc0b
    Former-commit-id: 49ea1b5391f74b4b880351eca4de110175d1d94f

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -30,7 +30,7 @@ set listchars=tab:▸\ ,trail:·
 " removes pipes | that act as seperators on splits
 set fillchars+=vert:\
 set wildignore+=**/node_modules/**
-let g:python3_host_prog = '~/.pyenv/versions/3.11.6/bin/python'
+let g:python3_host_prog = '/usr/bin/python3'
 "  " make `-` part of a word
 " set iskeyword-=-
 "  " make _` part of a word
