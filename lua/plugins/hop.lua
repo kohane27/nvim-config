c0ebf282b2bb498ba88e489cc12bc301a5f5c998
kohane27 <kohane27@protonmail.com>
2024-07-11 21:48:16 +0800

    feat: 2-key to go anywhere on current line
    Former-commit-id: 90e84bcee3bc9e41e75b1808b4a4cf164d191271
    Former-commit-id: 8e6ef9bafc542207c37a3407e75e674fb201b37c

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -12,13 +12,13 @@ return {
   config = function(_, opts)
     vim.api.nvim_set_hl(0, "HopNextKey", { fg = "#000000", bg = "#CCFF88", bold = true })
     local hop = require("hop")
-    local directions = require("hop.hint").HintDirection
+    -- local directions = require("hop.hint").HintDirection
 
     local keymap = {
-      ["f"] = function() hop.hint_char2({ direction = directions.AFTER_CURSOR, current_line_only = true, jump_on_sole_occurrence = false, }) end,
-      ["F"] = function() hop.hint_char2({ direction = directions.BEFORE_CURSOR, current_line_only = true, jump_on_sole_occurrence = false, }) end,
-      ["t"] = function() hop.hint_char2({ direction = directions.AFTER_CURSOR, current_line_only = true, jump_on_sole_occurrence = false, hint_offset = -1, }) end,
-      ["T"] = function() hop.hint_char2({ direction = directions.BEFORE_CURSOR, current_line_only = true, jump_on_sole_occurrence = false, hint_offset = 1, }) end,
+      ["f"] = function() hop.hint_char2({ current_line_only = true, jump_on_sole_occurrence = false, }) end,
+      ["t"] = function() hop.hint_char2({ current_line_only = true, jump_on_sole_occurrence = false, hint_offset = -1, }) end,
+      -- ["F"] = function() hop.hint_char2({ direction = directions.BEFORE_CURSOR, current_line_only = true, jump_on_sole_occurrence = false, }) end,
+      -- ["T"] = function() hop.hint_char2({ direction = directions.BEFORE_CURSOR, current_line_only = true, jump_on_sole_occurrence = false, hint_offset = 1, }) end,
     }
 
     local modes = { "n", "x", "o" }
