df5b9f1a88b24f3de936b08d01aab4600cc7f871
kohane27 <kohane27@protonmail.com>
2024-07-11 21:36:39 +0800

    chore: i must be blind
    Former-commit-id: c7d457aa9295e7dd7799a6de3b39219689e16544
    Former-commit-id: a7936a4474a723adeb019b90049a586a38d85ae0

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -16,9 +16,9 @@ return {
 
     local keymap = {
       ["f"] = function() hop.hint_char2({ direction = directions.AFTER_CURSOR, current_line_only = true, jump_on_sole_occurrence = false, }) end,
-      ["F"] = function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, jump_on_sole_occurrence = false, }) end,
-      ["t"] = function() hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, jump_on_sole_occurrence = false, hint_offset = -1, }) end,
-      ["T"] = function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, jump_on_sole_occurrence = false, hint_offset = 1, }) end,
+      ["F"] = function() hop.hint_char2({ direction = directions.BEFORE_CURSOR, current_line_only = true, jump_on_sole_occurrence = false, }) end,
+      ["t"] = function() hop.hint_char2({ direction = directions.AFTER_CURSOR, current_line_only = true, jump_on_sole_occurrence = false, hint_offset = -1, }) end,
+      ["T"] = function() hop.hint_char2({ direction = directions.BEFORE_CURSOR, current_line_only = true, jump_on_sole_occurrence = false, hint_offset = 1, }) end,
     }
 
     local modes = { "n", "x", "o" }
