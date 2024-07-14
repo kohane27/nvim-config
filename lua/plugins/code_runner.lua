f1644599fb1f0895593979f6108209cfb796ec6e
kohane27 <kohane27@protonmail.com>
2024-07-14 17:50:11 +0800

    fix: update code_runner.lua
    Former-commit-id: e9c93f14bc0dabde8c7390b381cd19f333a154f7
    Former-commit-id: cfd6f7ce2feb07c5ba4a944e179f44647088b6dd

--- lua/plugins/code_runner.lua
+++ lua/plugins/code_runner.lua
@@ -5,38 +5,27 @@ return {
   opts = {
     focus = false,
     term = {
-      position = "bot",
-      -- window size
-      size = 8,
+      position = "vertical",
+      size = vim.o.columns * 0.30,
+      -- position = "bot",
+      -- size = vim.o.lines * 0.25,
     },
     before_run_filetype = function()
       vim.cmd(":w")
     end,
     filetype = {
       javascript = "node",
-      -- typescript = "ts-node $fileName",
-      typescript = "deno run $fileName",
-      typescriptreact = "yarn dev$end",
+      typescript = "deno run",
+      -- typescript = "ts-node",
 
-      -- java = "cd $dir && javac $fileName && java $fileNameWithoutExt",
-      -- java = 'cd $dir && cd ../../../../../ && mvn -nsu -e -q compile -T 4 exec:java -Dexec.mainClass="com.prac.App"',
-      java = "mvn -nsu -e -q compile -T 4 exec:java",
-      -- java = "cd $dir && javac $fileName && java $fileNameWithoutExt",
-
-      sh = "bash",
-      -- sh = "cd $dir && ./$fileName",
+      sh = "zsh",
       ledger = "hledger bs -f $dir/$fileName",
       json = "cat $fileName | jq type 1>/dev/null",
-      markdown = "sh ~/.local/bin/neovim-to-anki.sh $dir/$fileName",
-
-      python = "python -u",
-      rust = "cd $dir && rustc $fileName && $dir$fileNameWithoutExt",
-      kotlin = "cd $dir && kotlinc-native $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt.kexe",
-      c = "cd $dir && gcc $fileName -o $fileNameWithoutExt && $dir/$fileNameWithoutExt",
-      cpp = "cd $dir && g++ $fileName -o $fileNameWithoutExt && $dir/$fileNameWithoutExt",
+      -- markdown = "sh ~/.local/bin/neovim-to-anki.sh $dir/$fileName",
+      python = "python3 -u",
+      java = { "cd $dir &&", "javac $fileName &&", "java $fileNameWithoutExt" },
+      rust = { "cd $dir &&", "rustc $fileName &&", "$dir/$fileNameWithoutExt" },
     },
-    -- filetype_path = vim.fn.expand("~/.config/nvim/code_runner.json"),
-    -- project_path = vim.fn.expand("~/.config/nvim/project_manager.json"),
   },
   config = function(_, opts)
     require("code_runner").setup(opts)
