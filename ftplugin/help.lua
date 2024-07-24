86197c4079b7c5ec0cd7bb354fd1b9481f219529
kohane27 <kohane27@protonmail.com>
2024-07-24 14:16:31 +0800

    chore: convert help to lua

---
+++ ftplugin/help.lua
@@ -0,0 +1,18 @@
+-- https://vim.fandom.com/wiki/Learn_to_use_help
+-- Press Enter to jump to the subject (topic) under the cursor.
+-- Press Backspace to return from the last jump.
+-- Press s to find the next subject, or S to find the previous subject.
+-- Press o to find the next option, or O to find the previous option.
+
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true, buffer = 0 }
+
+map("n", "<CR>", "<C-]>", opts)
+map("n", "<BS>", "<C-T>", opts)
+map("n", "o", "/'\\l\\{2,\\}'<CR>", opts)
+map("n", "O", "?'\\l\\{2,\\}'<CR>", opts)
+map("n", "s", "/\\|\\zs\\S\\+\\ze\\|<CR>", opts)
+map("n", "S", "?\\|\\zs\\S\\+\\ze\\|<CR>", opts)
+
+-- ensure the file is only loaded once
+vim.b.did_ftplugin = 1
