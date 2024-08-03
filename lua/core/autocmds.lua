813b8a3fc5714094d63c0a399b0557787a9bad99
kohane27 <kohane27@protonmail.com>
2024-08-03 22:16:21 +0800

    chore: clean up

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -61,7 +61,7 @@ vim.api.nvim_create_autocmd("VimEnter", {
     for i = 97, 122 do -- ASCII for 'a' to 'z'
       vim.fn.setreg(string.char(i), "")
     end
-    for i = 48, 57 do -- ASCII for '0' to '9'
+    for i = 49, 57 do -- ASCII for '1' to '9' (48 is '0')
       vim.fn.setreg(string.char(i), "")
     end
   end,
