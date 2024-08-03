b803ad347352279befe5df297ca70702fa61c72c
kohane27 <kohane27@protonmail.com>
2024-08-04 07:43:36 +0800

    fix: move keys to its own files

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -4,7 +4,6 @@ return {
     "nvim-telescope/telescope.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-    -- optional
     "nvim-treesitter/nvim-treesitter",
     "rcarriga/nvim-notify",
     "nvim-tree/nvim-web-devicons",
@@ -14,7 +13,13 @@ return {
   opts = {
     lang = "javascript",
   },
-  config = function(_, opts)
-    require("leetcode").setup(opts)
-  end,
+  -- stylua: ignore
+  keys = {
+    { "<leader>MXlem", "<cmd>Leet Menu<CR>",    desc = "LeetCode: Opens Menu Dashboard" },
+    { "<leader>MXlec", "<cmd>Leet console<CR>", desc = "LeetCode: Opens Console For Currently Opened Question" },
+    { "<leader>MXleq", "<cmd>Leet tabs<CR>",    desc = "LeetCode: Opens A Picker With All Currently Opened Question Tabs" },
+    { "<leader>MXled", "<cmd>Leet desc<CR>",    desc = "LeetCode: Toggle Question desc" },
+    { "<leader>MXler", "<cmd>Leet run<CR>",     desc = "LeetCode: Run Currently Opened Question" },
+    { "<leader>MXles", "<cmd>Leet submit<CR>",  desc = "LeetCode: Submit Currently Opened Question" },
+  },
 }
--- lua/plugins/nvim-jdtls.lua
+++ lua/plugins/nvim-jdtls.lua
@@ -2,4 +2,15 @@ return {
   "mfussenegger/nvim-jdtls",
   ft = "java",
   event = "VeryLazy",
+  -- stylua: ignore
+  keys = {
+    { "<leader>MXjda", "<cmd>JdtWipeDataAndRestart<CR>",                            ft = "java", desc = "jdtls: Wipe Data and Restart" },
+    { "<leader>MXjdb", "<cmd>JdtUpdateConfig<CR>",                                  ft = "java", desc = "jdtls: Update Config" },
+    { "<leader>MXjdc", "<cmd>lua require'jdtls'.organize_imports()<CR>",            ft = "java", desc = "jdtls: Organize Import" },
+    { "<leader>MXjdd", "<cmd>lua require('jdtls').extract_variable()<CR>",          ft = "java", desc = "jdtls: Extract Variable" },
+    { "<leader>MXjde", "<cmd>lua require('jdtls').extract_variable(true)<CR>",      ft = "java", desc = "jdtls: Extract Variable" },
+    { "<leader>MXjdf", "<cmd>lua require('jdtls').extract_constant()<CR>",          ft = "java", desc = "jdtls: Extract Constant" },
+    { "<leader>MXjdg", "<Esc><cmd>lua require('jdtls').extract_constant(true)<CR>", ft = "java", desc = "jdtls: Extract Constant" },
+    { "<leader>MXjdh", "<Esc><cmd>lua require('jdtls').extract_method(true)<CR>",   ft = "java", desc = "jdtls: Extract Method" }
+  },
 }
