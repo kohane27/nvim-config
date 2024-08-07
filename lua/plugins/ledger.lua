946a495dbee39cfba9bc7133ae705b356e711c30
kohane27 <kohane27@protonmail.com>
2024-08-07 21:15:06 +0800

    fix: use init for vim plugins

--- lua/plugins/ledger.lua
+++ lua/plugins/ledger.lua
@@ -2,25 +2,21 @@ return {
   "ledger/vim-ledger",
   event = "VeryLazy",
   ft = "ledger",
+  init = function()
+    vim.g.caser_no_mappings = 1
+    -- vim.g.ledger_extra_options = '--pedantic --explicit --check-payees'
+    vim.g.ledger_date_format = "%Y-%m-%d"
+    vim.g.ledger_align_at = 60
+    vim.g.ledger_bin = "/usr/bin/hledger"
+    vim.g.ledger_is_hledger = true
+    -- String that will be used to fill the space between account name and amount in the foldtext.
+    -- Set this to get some kind of lines or visual aid.
+    -- vim.g.ledger_fillstring = '    -'
+    vim.g.ledger_default_commodity = "$"
+  end,
+
   config = function()
     vim.cmd([[
-    " let g:ledger_extra_options = '--pedantic --explicit --check-payees'
-
-    let g:ledger_date_format = '%Y-%m-%d'
-    let g:ledger_align_at = 60
-    let g:ledger_bin = '/usr/bin/hledger'
-    let g:ledger_is_hledger = v:true
-
-    " String that will be used to fill the space between account name and amount in the foldtext.
-    " Set this to get some kind of lines or visual aid.
-    " let g:ledger_fillstring = '    -'
-
-    let g:ledger_default_commodity = '$'
-
-    "  ╭──────────────────────────────────────────────────────────╮
-    "  │ Commands                                                 │
-    "  ╰──────────────────────────────────────────────────────────╯
-
     au FileType ledger noremap <silent> { ?^\d<CR>
     au FileType ledger noremap <silent> } /^\d<CR>
 
@@ -29,7 +25,7 @@ return {
 
     "  set today's date of the current transaction.
     " `:call ledger#transaction_date_set(line('.'), "primary")
-    au FileType ledger noremap <silent> o o<BS><CR><ESC>i
+    au FileType ledger noremap <silent> o o<BS><ESC>i
     ]])
   end,
 }
