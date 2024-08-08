d59c7eb7c653175df2554c3c4c0f53376214966b
kohane27 <kohane27@protonmail.com>
2024-08-08 14:08:22 +0800

    chore: add gp
    Former-commit-id: 4124735ea359fbe12348de5fef7ec193db18009b
    Former-commit-id: f7b707f8103a801049930f158dd592030edca689

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -163,6 +163,9 @@ vim.keymap.set("x", "p", function()
 end, { remap = false, expr = true })
 -- map("v", "p", ":<C-U>let @p = @+<CR>gvp:let @+ = @p<CR>")
 
+-- Paste from register 0 (last yanked)
+map("n", "gp", '"0p')
+
 -- yank till end of line
 map("n", "Y", "y$")
 
