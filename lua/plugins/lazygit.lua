206909d6ddbaa73856921d0ab3d239b27094cb38
kohane27 <kohane27@protonmail.com>
2024-08-08 11:48:52 +0800

    fix: do not use plenary.nvim

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -4,8 +4,8 @@ return {
   event = "VeryLazy",
   init = function()
     vim.g.lazygit_floating_window_winblend = 0 -- transparency
-    vim.g.lazygit_floating_window_scaling_factor = 0.9
-    vim.g.lazygit_floating_window_use_plenary = 1 -- use plenary.nvim to manage floating window
+    vim.g.lazygit_floating_window_scaling_factor = 0.95
+    vim.g.lazygit_floating_window_use_plenary = 0 -- double border for some reason
     vim.g.lazygit_use_neovim_remote = 1 -- fallback to 0 if neovim-remote is not installed
 
     vim.g.lazygit_use_custom_config_file_path = 1
