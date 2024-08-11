6e562a2eba8007eb7d71ed372b619cf86701f311
kohane27 <kohane27@protonmail.com>
2024-08-11 20:59:56 +0800

    fix: previous auto-save never worked?

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -1,33 +1,9 @@
 return {
-  "aidenlangley/auto-save.nvim",
-  event = "VeryLazy",
+  "okuuva/auto-save.nvim",
+  event = { "InsertLeave", "TextChanged" },
   opts = {
-    augroup_name = "AutoSavePlug",
-
-    -- events to trigger an auto save
-    events = { "InsertLeave", "TextChanged" },
-
-    -- silence the output of `save_fn`
-    silent = true,
-
-    -- May define a timeout, or a duration to defer the save for - this allows
-    -- for formatters to run, for example if they're configured via an autocmd
-    -- that listens for `BufWritePre` event.
-    timeout = nil,
-
-    -- Define some filetypes to explicitly not save, in case our existing conditions
-    -- don't quite catch all the buffers we'd prefer not to write to.
-    exclude_ft = {
-      "toggleterm",
-      "NvimTree",
-      "qf",
-      "help",
-      "TelescopePrompt",
-      "frecency",
-      "harpoon",
-      "grapple",
-      "fzf",
-      "portal",
+    execution_message = {
+      enabled = false,
     },
   },
 }
