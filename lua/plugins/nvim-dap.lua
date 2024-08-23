0305f6646d43588b155298d20bf7b4be4b350b5e
kohane27 <kohane27@protonmail.com>
2024-08-23 11:33:34 +0800

    fix: firenvim

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,6 +1,7 @@
 return {
   "mfussenegger/nvim-dap",
   event = "VeryLazy",
+  enabled = not vim.g.started_by_firenvim,
   dependencies = {
     { "nvim-neotest/nvim-nio" },
     { "rcarriga/nvim-dap-ui" },
