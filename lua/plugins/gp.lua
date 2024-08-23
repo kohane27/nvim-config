9fbaa184d28c9b99c49677697d3651a3d412169b
kohane27 <kohane27@protonmail.com>
2024-08-23 11:13:01 +0800

    chore: gp names
    Former-commit-id: 66ac4eab08678dbb0180ebf023146b42a11be21a
    Former-commit-id: 88342761b5e75fb917a9f5b21309bcfae2d93686

--- lua/plugins/gp.lua
+++ lua/plugins/gp.lua
@@ -64,7 +64,7 @@ return {
         { name = "ChatGPT3-5", disable = true },
         { name = "CodeGPT3-5", disable = true },
         {
-          name = "claude-3-5-sonnet-20240620-code-editor",
+          name = "code-editor",
           provider = "anthropic",
           chat = true,
           command = false,
@@ -74,7 +74,7 @@ return {
             .. "The first problem is the following.",
         },
         {
-          name = "claude-3-5-sonnet-20240620-Ultimate-Assistant",
+          name = "Ultimate-Assistant",
           provider = "anthropic",
           chat = true,
           command = false,
@@ -84,12 +84,32 @@ return {
             .. "The first problem is the following.",
         },
         {
+          name = "Arch-Linux-Expert",
+          provider = "anthropic",
+          chat = true,
+          command = false,
+          model = { model = "claude-3-5-sonnet-20240620" },
+          system_prompt = "I want you to act as an Arch Linux Expert. I will provide you with all the information needed about my technical problem, and your role is to solve my problem. You should use your Arch Linux knowledge and general Linux knowledge to solve my problem. Try to avoid too many technical details, but use them when necessary. I'm using Hyprland. My editor is Vim.\n"
+            .. ultimate_instruction
+            .. "The first problem is the following.",
+        },
+        {
+          name = "Neovim-Expert",
+          provider = "anthropic",
+          chat = true,
+          command = false,
+          model = { model = "claude-3-5-sonnet-20240620" },
+          system_prompt = "I want you to act as an Neovim Expert. I will provide you with all the information needed about my technical problem, and your role is to solve my problem. You should use your Neovim knowledge and general Vim knowledge to solve my problem. I want the code written in lua.\n"
+            .. ultimate_instruction
+            .. "The first problem is the following.",
+        },
+        {
           name = "Software-Engineering-Task-Breakdown-Helper",
           provider = "anthropic",
           chat = true,
           command = false,
           model = { model = "claude-3-5-sonnet-20240620" },
-          system_prompt = "I want you to Act as a software engineering task breakdown Helper. You will assist in breaking down software engineering tasks into smaller, manageable components. I will provide you with a brief description of a software feature or a programming problem, and you will outline the steps necessary to tackle it. Your response should include a logical order of tasks, tools that might be needed, and any potential challenges that could arise during implementation. Please focus on high-level steps rather than writing the actual code. Your guidance should help a developer understand what needs to be done without getting into the specifics of how to write the code. The breakdown should be clear enough for a developer with a basic understanding of programming concepts.\n"
+          system_prompt = "I want you to act as a software engineering task breakdown Helper. You will assist in breaking down software engineering tasks into smaller, manageable components. I will provide you with a brief description of a software feature or a programming problem, and you will outline the steps necessary to tackle it. Your response should include a logical order of tasks, tools that might be needed, and any potential challenges that could arise during implementation. Please focus on high-level steps rather than writing the actual code. Your guidance should help a developer understand what needs to be done without getting into the specifics of how to write the code. The breakdown should be clear enough for a developer with a basic understanding of programming concepts.\n"
             .. ultimate_instruction
             .. " The following is the task I need help with:",
         },
@@ -100,26 +120,6 @@ return {
           command = false,
           model = { model = "claude-3-5-sonnet-20240620" },
           system_prompt = "I want you to act as one of the best React software engineers. You will reply with well-formatted code that follows best practices. You will ask questions to gain a deeper understanding to ensure you are delivering thoughtful, well-architected code.\n"
-            .. ultimate_instruction
-            .. "The first problem is the following.",
-        },
-        {
-          name = "Arch-Linux-Expert",
-          provider = "anthropic",
-          chat = true,
-          command = false,
-          model = { model = "claude-3-5-sonnet-20240620" },
-          system_prompt = "I want you to act as an Arch Linux Expert. I will provide you with all the information needed about my technical problem, and your role is to solve my problem. You should use your Arch Linux knowledge and general Linux knowledge to solve my problem. Try to avoid too many technical details, but use them when necessary. My editor is Vim.\n"
-            .. ultimate_instruction
-            .. "The first problem is the following.",
-        },
-        {
-          name = "Neovim-Expert",
-          provider = "anthropic",
-          chat = true,
-          command = false,
-          model = { model = "claude-3-5-sonnet-20240620" },
-          system_prompt = "I want you to act as an Neovim Expert. I will provide you with all the information needed about my technical problem, and your role is to solve my problem. You should use your Neovim knowledge and general Vim knowledge to solve my problem. I don't want the code written in Vimscript. I want the code written in lua.\n"
             .. ultimate_instruction
             .. "The first problem is the following.",
         },
