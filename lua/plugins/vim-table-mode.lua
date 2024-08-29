35f5158c14a6a7547705ad897ac47290c8791d84
kohane27 <kohane27@protonmail.com>
2024-08-29 18:15:27 +0800

    feat: use `vim-table-mode.nvim`

---
+++ lua/plugins/vim-table-mode.lua
@@ -0,0 +1,10 @@
+return {
+  "dhruvasagar/vim-table-mode",
+  event = "VeryLazy",
+  ft = "markdown",
+  init = function()
+    -- vim.g.table_mode_disable_mappings = 1
+    -- vim.g.table_mode_disable_tableize_mappings = 0
+    -- TODO: tableize for markdown
+  end,
+}
