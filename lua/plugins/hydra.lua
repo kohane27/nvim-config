2d47faef217dea9b9ce176ea3bd07f1e099676dc
kohane27 <kohane27@protonmail.com>
2024-09-03 10:27:39 +0800

    fix: disable dap for now

--- lua/plugins/hydra.lua
+++ lua/plugins/hydra.lua
@@ -6,74 +6,49 @@ return {
   -- stylua: ignore
   config = function()
     local Hydra = require("hydra")
-    local dap = require("dap")
-
-    local hint = [[
-         ^ ^Step^ ^ ^      ^ ^     Action
-     ----^-^-^-^--^-^----  ^-^-------------------  
-         ^ ^back^ ^ ^      _z_: toggle breakpoint 
-         ^ ^ _K_^ ^         _Z_: Set conditional breakpoint 
-     out _H_ ^ ^ _L_ into   _>_: Continue
-         ^ ^ _J_ ^ ^        _X_: Terminate
-         ^ ^over ^ ^      ^^_s_: open scope
-                      _U_: UI toggle
-                      _g?_: Hydra hint
-                      _gl_: Run last configuration
-                      _*_: Run to cursor
-
-         ^ ^  _<Esc>_: Normal mode
-    ]]
-
-    DapHydra = Hydra({
-      name = "DEBUG",
-      mode = "n",
-      body = "<leader>d",
-
-      hint = hint,
-      config = {
-        color = "pink",
-        desc = "Debug mode",
-        invoke_on_body = true,
-        on_enter = function()
-          require("dapui").toggle()
-        end,
-        hint = {
-          float_opts = {
-            border = "rounded",
-          },
-          hide_on_load = true,
-          show_name = true,
-        },
-      },
-
-      heads = {
-        -- { "u", function() require("dapui").toggle() end },
-        { "b", function() dap.toggle_breakpoint() end },
-        { "B", function() dap.set_breakpoint(vim.fn.input("Breakpoint condition: ")) end },
-        { "c", function() if vim.bo.filetype ~= "dap-float" then dap.continue() end end },
-        { "i", function() if vim.bo.filetype ~= "dap-float" then dap.step_into() end end },
-        { "o", function() if vim.bo.filetype ~= "dap-float" then dap.step_out() end end },
-        { "l", function() if vim.bo.filetype ~= "dap-float" then dap.step_over() end end },
-        -- { "<", function() if vim.bo.filetype ~= "dap-float" then dap.step_back() end end },
-        { "x", function() dap.terminate() end },
-        { "*", function() dap.run_to_cursor() end },
-        { "gl",
-          function()
-            -- Run last: https://github.com/mfussenegger/nvim-dap/issues/1025
-            local last_config = nil
-            dap.listeners.after.event_initialized["store_config"] = function(session)
-              last_config = session.config
-            end
-            if last_config then dap.run(last_config)
-            else dap.continue()
-            end
-          end,
-        },
-        { "s", function() if vim.bo.filetype ~= "dap-float" then require("dap.ui.widgets").centered_float(require("dap.ui.widgets").scopes) end end },
-        { "g?", function() if DapHydra.hint.win then DapHydra.hint:close() else DapHydra.hint:show() end end },
-        { "<Esc>", nil, { exit = true, nowait = true } },
-      },
-    })
+ --    local dap = require("dap")
+ --
+ --    DapHydra = Hydra({
+ --      name = "DEBUG",
+ --      mode = "n",
+ --      body = "<leader>d",
+ --
+ --      hint = [[
+ -- _b_: breakpoint   _c_: continue
+ -- _i_: step into   _o_: step out
+ --    ]],
+ --      config = {
+ --        color = "pink",
+ --        desc = "Debug mode",
+ --        invoke_on_body = true,
+ --        on_enter = function()
+ --          require("dapui").toggle()
+ --        end,
+ --        hint = {
+ --          float_opts = {
+ --            border = "rounded",
+ --          },
+ --          hide_on_load = true,
+ --          show_name = true,
+ --        },
+ --      },
+ --
+ --      heads = {
+ --        -- { "u", function() require("dapui").toggle() end },
+ --        { "b", function() dap.toggle_breakpoint() end },
+ --        { "B", function() dap.set_breakpoint(vim.fn.input("Breakpoint condition: ")) end },
+ --        { "c", function() dap.continue() end },
+ --        { "i", function() dap.step_into() end },
+ --        { "o", function() dap.step_out() end },
+ --        { "n", function() dap.step_over() end },
+ --        -- { "<", function() if vim.bo.filetype ~= "dap-float" then dap.step_back() end end },
+ --        { "x", function() dap.terminate() end },
+ --        { "*", function() dap.run_to_cursor() end },
+ --        { "e", function() require("dapui").eval() end},
+ --        { "?", function() if DapHydra.hint.win then DapHydra.hint:close() else DapHydra.hint:show() end end },
+ --        { "<Esc>", nil, { exit = true, nowait = true } },
+ --      },
+ --    })
 
     -- BUG: can't make it work with neoscroll
     -- MyScroll = hydra({
