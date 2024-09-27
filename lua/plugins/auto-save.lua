597fd7a8e07f23363b735d2843b0ece4817a4a41
kohane27 <kohane27@protonmail.com>
2024-09-27 08:58:45 +0800

    fix: faster debounce

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -5,6 +5,7 @@ return {
     execution_message = {
       enabled = false,
     },
+    debounce_delay = 500, -- delay after which a pending save is executed
     noautocmd = true, -- do not trigger formatting
   },
 }
