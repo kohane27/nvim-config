47994ac3367dd33e61ca63e02018c17a8aa13211
kohane27 <kohane27@protonmail.com>
2024-10-08 11:32:33 +0800

    chore: remove unused default keybindings

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -4,5 +4,18 @@ return {
   event = "VeryLazy",
   config = function()
     require("illuminate").configure({})
+
+    local function map(mode, lhs, rhs)
+      local options = { noremap = true, silent = true }
+      if opts then
+        options = vim.tbl_extend("force", options)
+      end
+      vim.api.nvim_set_keymap("", lhs, rhs, options)
+    end
+
+    map("x", "<M-i>", "<Nop>")
+    map("o", "<M-i>", "<Nop>")
+    map("n", "<M-p>", "<Nop>")
+    map("n", "<M-n>", "<Nop>")
   end,
 }
