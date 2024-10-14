70b63db05c672ff0281d7710e9cfc440c13828a4
kohane27 <kohane27@protonmail.com>
2024-10-14 10:47:24 +0800

    fix: remove auto-save deprecated cmd

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -2,10 +2,7 @@ return {
   "okuuva/auto-save.nvim",
   event = { "InsertLeave", "TextChanged" },
   opts = {
-    execution_message = {
-      enabled = false,
-    },
-    debounce_delay = 500, -- delay after which a pending save is executed
+    debounce_delay = 250, -- delay after which a pending save is executed
     noautocmd = true, -- do not trigger formatting
   },
 }
