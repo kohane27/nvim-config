e19be3c74b61a05470335818efe9c61585633659
kohane27 <kohane27@protonmail.com>
2024-11-05 11:13:59 +0800

    chore: set up debugprint.nvim
    Former-commit-id: 6cc10e4dffd43e126a7be7886ea1681e20dc6f64
    Former-commit-id: 9803d4162f37f3e817e101cccaa3854a92de840c

--- lua/plugins/debugprint.lua
+++ lua/plugins/debugprint.lua
@@ -1,5 +1,23 @@
 return {
   "andrewferrier/debugprint.nvim",
-  dependencies = { "echasnovski/mini.nvim" },
-  opts = {},
+  opts = {
+    keymaps = {
+      normal = {
+        plain_below = "<leader>pp",
+        plain_above = "<leader>pP",
+        variable_below = "<leader>pv",
+        variable_above = "<leader>pV",
+        variable_below_alwaysprompt = nil,
+        variable_above_alwaysprompt = nil,
+        textobj_below = nil,
+        textobj_above = nil,
+        toggle_comment_debug_prints = nil,
+        delete_debug_prints = nil,
+      },
+      visual = {
+        variable_below = nil,
+        variable_above = nil,
+      },
+    },
+  },
 }
