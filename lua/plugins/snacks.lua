8f07fa260337f731683e4dca1b121c30be55a9aa
kohane27 <kohane27@protonmail.com>
2024-11-07 11:31:45 +0800

    feat: use `snacks.lua`
    Former-commit-id: 37145ed08d9756465d0edb555bf26b9c28399817
    Former-commit-id: 2aa5f5a6ba345dde201f89cb44ac6912719f938e

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,20 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = { enabled = true },
+    notifier = {
+      enabled = true,
+      timeout = 3000,
+    },
+    quickfile = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+    styles = {
+      notification = {
+        wo = { wrap = true }, -- Wrap notifications
+      },
+    },
+  },
+}
