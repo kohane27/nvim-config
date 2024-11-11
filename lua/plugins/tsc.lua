c8ab91e94573d2df0cf584090215786d15c08efe
kohane27 <kohane27@protonmail.com>
2024-11-11 16:42:15 +0800

    chore: set up tsc.nvim
    Former-commit-id: 52bd6f1738b84bcb053ef66c7ceac236319a7d60
    Former-commit-id: 14f667c1b160ff0437319bfa801796f90a3836f0

--- lua/plugins/tsc.lua
+++ lua/plugins/tsc.lua
@@ -4,12 +4,16 @@ return {
     auto_open_qflist = true,
     auto_close_qflist = false,
     auto_focus_qflist = true,
-    auto_start_watch_mode = true,
+    auto_start_watch_mode = false,
     use_trouble_qflist = true,
     use_diagnostics = true,
     run_as_monorepo = false,
-    enable_progress_notifications = true,
+    enable_progress_notifications = false,
     enable_error_notifications = true,
     hide_progress_notifications_from_history = true,
+    flags = {
+      noEmit = true,
+      watch = true,
+    },
   },
 }
