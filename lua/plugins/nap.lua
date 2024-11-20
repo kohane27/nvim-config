edd9dd6ea97016aad48fd6736faa49f0fe649bb6
kohane27 <kohane27@protonmail.com>
2024-11-20 17:57:25 +0800

    chore: reorder

--- lua/plugins/nap.lua
+++ lua/plugins/nap.lua
@@ -14,6 +14,10 @@ return {
         next = { rhs = "<cmd>lua require('lspsaga.diagnostic'):goto_next({ severity = vim.diagnostic.severity.ERROR })<CR>", opts = { desc = "Next diagnostic" }, },
         prev = { rhs = "<cmd>lua require('lspsaga.diagnostic'):goto_prev({ severity = vim.diagnostic.severity.ERROR })<CR>", opts = { desc = "Prev diagnostic" }, },
       },
+      ["c"] = {
+        next = { rhs = "<cmd>cnext<CR>zz", opts = { desc = "Next quickfix item" } },
+        prev = { rhs = "<cmd>cprevious<CR>zz", opts = { desc = "Prev quickfix item" } },
+      },
       ["g"] = {
         next = { rhs = "<cmd>Gitsigns next_hunk<CR>", opts = { desc = "git: Next Hunk" } },
         prev = { rhs = "<cmd>Gitsigns prev_hunk<CR>", opts = { desc = "git: Previous Hunk" } },
@@ -21,10 +25,6 @@ return {
       ["t"] = {
         next = { rhs = '<cmd>lua require("todo-comments").jump_next()<CR>', opts = { desc = "Next todo comment" }, },
         prev = { rhs = '<cmd>lua require("todo-comments").jump_prev()<CR>', opts = { desc = "Previous todo comment" }, },
-      },
-      ["c"] = {
-        next = { rhs = "<cmd>cnext<CR>zz", opts = { desc = "Next quickfix item" } },
-        prev = { rhs = "<cmd>cprevious<CR>zz", opts = { desc = "Prev quickfix item" } },
       },
       ["l"] = {
         next = { rhs = "<cmd>lnext<CR>", opts = { desc = "Next loclist item" } },
