9043ca7a99749088be9296608d62a9cde5fd108c
kohane27 <kohane27@protonmail.com>
2024-11-23 09:45:31 +0800

    feat: use neozoom.nvim
    Former-commit-id: 2f7b8fea1829bae33d2b2e9f89aa87097bd6a1ec
    Former-commit-id: 707fe9909796f48513bb5fd4dd0659d1f2cfd5fe

---
+++ lua/plugins/neozoom.lua
@@ -0,0 +1,15 @@
+return {
+  "nyngwang/NeoZoom.lua",
+  opts = {
+    winopts = {
+      offset = {
+        -- NOTE: omit `top`/`left` to center the floating window vertically/horizontally.
+        -- top = 0,
+        -- left = 0.17,
+        width = 150,
+        height = 0.85,
+      },
+      border = "rounded",
+    },
+  },
+}
