9cf6571b49aa0e60a91946483fd7a396d774e2b6
kohane27 <kohane27@protonmail.com>
2024-11-24 22:15:34 +0800

    chore: fmt

--- lua/plugins/hydra.lua
+++ lua/plugins/hydra.lua
@@ -20,45 +20,18 @@ return {
       },
     }
 
-
-    -- TODO: set up a command to start hydra for neotest
-    -- disable floating test
-    Neotest = Hydra({
-      name = "Test",
-      mode = "n",
-      body = "<leader>tt",
-      hint = [[
-_r_ "Run Nearest"
-_T_ "Run all files"
-_f_ "Run current file"
-_d_ "Debug nearest test"
-_l_ "Run Last"
-_s_ "Stop"
-_o_ "Show Output"
-_t_ "Toggle Summary"
-_O_ "Toggle Output Panel"
-_w_ "Toggle Watch"
-      ]],
-
-      config = vim.tbl_extend("force", default_config, {
-        color = "pink",
-      }),
-
-      -- stylua: ignore
-      heads = {
-        { "r", function() neotest.run.run() end, },
-        { "T", function() neotest.run.run(vim.uv.cwd()) end, },
-        { "f", function() neotest.run.run(vim.fn.expand("%")) end, },
-        { "d", function() neotest.run.run({ strategy = "dap" }) end, },
-        { "s", function() neotest.run.stop() end, },
-        { "l", function() neotest.run.run_last() end, },
-        { "t", function() neotest.summary.toggle() end, },
-        { "o", function() neotest.output.open({ enter = true, auto_close = true }) end, },
-        { "O", function() neotest.output_panel.toggle() end, },
-        { "w", function() neotest.watch.toggle(vim.fn.expand("%")) end, },
-        { "<Esc>", nil, { exit = true, nowait = true } },
-      },
-    })
+    -- {
+    --   '<leader>md',
+    --   function()
+    --     if toolbox.is_visual_mode() then
+    --      core_utils.execute_command(":s/foo.*//g<Left><Left><Left><Left><Left>")
+    --     else
+    --       core_utils.execute_command(":%s/foo.*//g<Left><Left><Left><Left><Left>")
+    --     end
+    --   end,
+    --   mode = { 'n', 'v' },
+    --   description = 'Delete Everything After `foo`',
+    -- },
 
     Hydra({
       name = "Buffers",
@@ -98,12 +71,52 @@ _w_ "Toggle Watch"
       },
     })
 
-    -- `Debugger` var is for `require("hydra").activate(Debugger)` in legendary.nvim
+    Neotest = Hydra({
+      name = "Neotest",
+      mode = "n",
+      body = "<leader>tt",
+      config = vim.tbl_extend("force", default_config, {
+        color = "pink",
+      }),
+      hint = [[
+_r_ "Run Nearest"
+_T_ "Run all files"
+_f_ "Run current file"
+_d_ "Debug nearest test"
+_l_ "Run Last"
+_s_ "Stop"
+_o_ "Show Output"
+_t_ "Toggle Summary"
+_O_ "Toggle Output Panel"
+_w_ "Toggle Watch"
+      ]],
+      -- stylua: ignore
+      heads = {
+        { "r", function() neotest.run.run() end, },
+        { "T", function() neotest.run.run(vim.uv.cwd()) end, },
+        { "f", function() neotest.run.run(vim.fn.expand("%")) end, },
+        { "d", function() neotest.run.run({ strategy = "dap" }) end, },
+        { "s", function() neotest.run.stop() end, },
+        { "l", function() neotest.run.run_last() end, },
+        { "t", function() neotest.summary.toggle() end, },
+        { "o", function() neotest.output.open({ enter = true, auto_close = true }) end, },
+        { "O", function() neotest.output_panel.toggle() end, },
+        { "w", function() neotest.watch.toggle(vim.fn.expand("%")) end, },
+        { "<Esc>", nil, { exit = true, nowait = true } },
+      },
+    })
+
+    -- NOTE: `Debugger` var is for `require("hydra").activate(Debugger)` in legendary.nvim
     Debugger = Hydra({
       name = "Debugger",
       mode = "n",
-      -- TODO: fix
-      body = "<leader>d",
+      body = "<leader>mD",
+      config = vim.tbl_extend("force", default_config, {
+        color = "pink",
+        on_enter = function()
+          require("dapui").toggle()
+        end,
+      }),
       hint = [[
  _b_: breakpoint
  _B_: breakpoint condition
@@ -116,14 +129,6 @@ _w_ "Toggle Watch"
  _u_: toggle UI
  _t_: terminate
        ]],
-
-      config = vim.tbl_extend("force", default_config, {
-        color = "pink",
-        on_enter = function()
-          require("dapui").toggle()
-        end,
-      }),
-
       -- stylua: ignore
       heads = {
         { "b", function() dap.toggle_breakpoint() end },
