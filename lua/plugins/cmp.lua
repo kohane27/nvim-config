abc076b0d27d290a615503144625b9cf4e384491
kohane27 <kohane27@protonmail.com>
2024-12-04 15:58:20 +0800

    fix: use tab for minuet in cmp
    Former-commit-id: 464ffd4297875857ed0cd5ce0e067c26cda7b65f
    Former-commit-id: 9beb32380de419ccc4a5ad23fb60122aa9ccc2df

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -35,30 +35,30 @@ return {
       mapping = cmp.mapping.preset.insert({
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-e>"] = cmp.mapping.complete(),
-        ["<C-y>"] = require("minuet").make_cmp_map(),
+        -- ["<C-e>"] = cmp.mapping.complete(),
+        ["<Tab>"] = require("minuet").make_cmp_map(),
         ["<C-Space>"] = cmp.config.disable,
         ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 
         -- Tab and S-Tab for `luasnip`
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if luasnip.expandable() then
-            luasnip.expand()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif check_backspace() then
-            fallback()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
+        -- ["<Tab>"] = cmp.mapping(function(fallback)
+        --   if luasnip.expandable() then
+        --     luasnip.expand()
+        --   elseif luasnip.expand_or_jumpable() then
+        --     luasnip.expand_or_jump()
+        --   elseif check_backspace() then
+        --     fallback()
+        --   else
+        --     fallback()
+        --   end
+        -- end, { "i", "s" }),
+        -- ["<S-Tab>"] = cmp.mapping(function(fallback)
+        --   if luasnip.jumpable(-1) then
+        --     luasnip.jump(-1)
+        --   else
+        --     fallback()
+        --   end
+        -- end, { "i", "s" }),
       }),
 
       formatting = {
@@ -67,7 +67,7 @@ return {
           maxwidth = { menu = 50, abbr = 50 },
           ellipsis_char = "...",
           show_labelDetails = true, -- show labelDetails in menu
-          symbol_map = { Codeium = "" },
+          symbol_map = { Codeium = "🪄", gemini = "󱗻" },
         }),
       },
       sources = {
