49a6d4b086027e994b68fdf8b613f90117abc17e
kohane27 <kohane27@protonmail.com>
2024-12-04 15:56:20 +0800

    fix: remap codeium.nvim

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -6,11 +6,28 @@ return {
   },
   config = function()
     require("codeium").setup({
+      filetypes = {
+        markdown = false,
+      },
+      default_filetype_enabled = true,
       enable_cmp_source = false,
       virtual_text = {
         enabled = true,
-        -- do not auto-complete automatically
-        manual = false,
+        manual = false, -- do not auto-complete automatically
+        key_bindings = {
+          -- Accept the current completion.
+          accept = "<C-e>",
+          -- Accept the next word.
+          accept_word = false,
+          -- Accept the next line.
+          accept_line = false,
+          -- Clear the virtual text.
+          clear = false,
+          -- Cycle to the next completion.
+          next = "<M-]>",
+          -- Cycle to the previous completion.
+          prev = "<M-[>",
+        },
       },
     })
   end,
