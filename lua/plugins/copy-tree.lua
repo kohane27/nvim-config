785f4204e1b04135718352e250a478de72244fd3
kohane27 <kohane27@protonmail.com>
2024-12-04 10:54:06 +0800

    chore: update copy-tree

--- lua/plugins/copy-tree.lua
+++ lua/plugins/copy-tree.lua
@@ -3,5 +3,7 @@ return {
   cmd = { "CopyTree", "SaveTree" },
   opts = {
     additional_extensions = { "%.tf$" },
+    exclude_extensions = { "%.md$", "%.json$" },
+    ignore_dirs = { "^%.", "node_modules", "dist", "undo", "__pycache__" },
   },
 }
