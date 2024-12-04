7c8cd5e16b6bae962272252a80f01750354b6e11
kohane27 <kohane27@protonmail.com>
2024-12-04 12:26:53 +0800

    fix: enable minuet-ai.nvim
    Former-commit-id: 973f6b2cbbef3404e84df0a6bdaa3757ce45b306
    Former-commit-id: 70b30bb509e89d50da190dd559943d594a4a2852

--- lua/plugins/minuet-ai.lua
+++ lua/plugins/minuet-ai.lua
@@ -1,6 +1,6 @@
 return {
   "milanglacier/minuet-ai.nvim",
-  enabled = false,
+  enabled = true,
   dependencies = {
     "nvim-lua/plenary.nvim",
     "hrsh7th/nvim-cmp",
@@ -9,5 +9,10 @@ return {
     provider = "claude",
     proxy = "http://127.0.0.1:8888",
     notify = "error",
+    provider_options = {
+      claude = {
+        model = "claude-3-5-haiku-20241022",
+      },
+    },
   },
 }
