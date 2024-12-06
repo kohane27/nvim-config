97ff03ee9d09c23d105d39c462833f73306384fc
kohane27 <kohane27@protonmail.com>
2024-12-06 12:29:44 +0800

    fix: vim-visual-multi

--- lua/plugins/vim-visual-multi.lua
+++ lua/plugins/vim-visual-multi.lua
@@ -1,28 +1,6 @@
 return {
   "mg979/vim-visual-multi",
   config = function()
-    -- all mappings disabled except <C-n> (<C-n> can't be remapped)
-    vim.g.VM_default_mappings = 0
-
-    -- using noice.nvim or else it'll break multi cursor red change functionality
-    vim.g.VM_set_statusline = 1
-
-    -- disable warning
-    vim.g.VM_show_warnings = 0
-
-    -- don't display "Exited Visual-Multi."
-    vim.g.VM_silent_exit = 1
-
-    -- To change any mapping you must first initialize the variable:
-    vim.g.VM_maps = {}
-    vim.g.VM_maps["Find Under"] = "<C-n>"
-
-    -- https://github.com/mg979/vim-visual-multi/issues/172
-    -- disable backspace mapping
-    vim.g.VM_maps["I BS"] = ""
-    -- disable Ctrl-C mapping
-    vim.g.VM_maps["I CtrlC"] = ""
-
     -- create a column of cursors from visual mode
     vim.api.nvim_set_keymap(
       "n",
@@ -31,7 +9,29 @@ return {
       { noremap = true, silent = true }
     )
 
-    -- disable noice.nvim when triggering this plugin
+    vim.cmd([[
+    " all mappings disabled except <C-n> (<C-n> can't be remapped)
+    let g:VM_default_mappings = 0
+
+    " " using noice.nvim or else it'll break multi cursor red change functionality
+    let g:VM_set_statusline = 1
+
+    " disable warning
+    let g:VM_show_warnings = 0
+
+    " don't display "Exited Visual-Multi."
+     let g:VM_silent_exit = 1
+
+    " To change any mapping you must first initialize the variable:
+    let g:VM_maps = {}
+    let g:VM_maps['Find Under'] = "<C-n>"
+
+    " https://github.com/mg979/vim-visual-multi/issues/172
+    let g:VM_maps['I BS'] = "" " disable backspace mapping
+    let g:VM_maps['I CtrlC'] = "" " disable backspace mapping
+]])
+
+    -- disable noice.nvim when trigger this plugin
     vim.api.nvim_create_autocmd("User", {
       pattern = "visual_multi_start",
       callback = function()
