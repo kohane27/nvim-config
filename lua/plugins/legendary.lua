19d1296c7000292df972f556829b8a5ea640dd13
kohane27 <kohane27@protonmail.com>
2024-12-06 23:04:28 +0800

    chore: remove unused

--- lua/plugins/legendary.lua
+++ lua/plugins/legendary.lua
@@ -587,12 +587,6 @@ return {
           ":w! /tmp/nvim_jqp | silent !tmux new-window \"jqp < /tmp/nvim_jqp\"<CR>",
           description = "jqp: New Window",
         },
-        {
-          "<leader>MXexd",
-          function() vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(":%!tail -n +2 | cut -d',' -f2 | sort | uniq | wc -l<Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left>", true, true, true), "t", true) end,
-          description = "csv: count unique in 2nd column",
-        },
-
       },
     })
   end,
