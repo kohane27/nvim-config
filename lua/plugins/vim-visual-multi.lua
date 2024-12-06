09ac049d40f80e8a6fc71f316548ef1267f10ec1
kohane27 <kohane27@protonmail.com>
2024-12-06 12:10:32 +0800

    fix: disable noice.nvim when triggering vim-visual-multi

--- lua/plugins/vim-visual-multi.lua
+++ lua/plugins/vim-visual-multi.lua
@@ -1,6 +1,28 @@
 return {
   "mg979/vim-visual-multi",
   config = function()
+    -- all mappings disabled except <C-n> (<C-n> can't be remapped)
+    vim.g.VM_default_mappings = 0
+
+    -- using noice.nvim or else it'll break multi cursor red change functionality
+    vim.g.VM_set_statusline = 1
+
+    -- disable warning
+    vim.g.VM_show_warnings = 0
+
+    -- don't display "Exited Visual-Multi."
+    vim.g.VM_silent_exit = 1
+
+    -- To change any mapping you must first initialize the variable:
+    vim.g.VM_maps = {}
+    vim.g.VM_maps["Find Under"] = "<C-n>"
+
+    -- https://github.com/mg979/vim-visual-multi/issues/172
+    -- disable backspace mapping
+    vim.g.VM_maps["I BS"] = ""
+    -- disable Ctrl-C mapping
+    vim.g.VM_maps["I CtrlC"] = ""
+
     -- create a column of cursors from visual mode
     vim.api.nvim_set_keymap(
       "n",
@@ -8,27 +30,20 @@ return {
       ":call vm#commands#add_cursor_down(0, v:count1)<cr>",
       { noremap = true, silent = true }
     )
-    vim.cmd([[
-    " all mappings disabled except <C-n> (<C-n> can't be remapped)
-    let g:VM_default_mappings = 0
 
-    " " using noice.nvim or else it'll break multi cursor red change functionality
-    let g:VM_set_statusline = 1
+    -- disable noice.nvim when triggering this plugin
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "visual_multi_start",
+      callback = function()
+        vim.cmd("Noice disable")
+      end,
+    })
 
-    " disable warning
-    let g:VM_show_warnings = 0
-
-    " don't display "Exited Visual-Multi."
-     let g:VM_silent_exit = 1
-
-    " To change any mapping you must first initialize the variable:
-    let g:VM_maps = {}
-    let g:VM_maps['Find Under'] = "<C-n>"
-
-    " https://github.com/mg979/vim-visual-multi/issues/172
-    let g:VM_maps['I BS'] = "" " disable backspace mapping
-    let g:VM_maps['I CtrlC'] = "" " disable backspace mapping
-
-]])
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "visual_multi_exit",
+      callback = function()
+        vim.cmd("Noice enable")
+      end,
+    })
   end,
 }
