b9b1cc9cd548ecbdef04de126a47eba8c30cf8c4
kohane27 <kohane27@protonmail.com>
2024-12-07 11:26:12 +0800

    chore: set up vim-visual-multi
    Former-commit-id: 4d7b04c08a17cc8467c69791f892bd6420406c1f
    Former-commit-id: 0c4587c84fb1ec38bf47959122591042035e8183

--- lua/plugins/vim-visual-multi.lua
+++ lua/plugins/vim-visual-multi.lua
@@ -1,7 +1,27 @@
 return {
   "mg979/vim-visual-multi",
+  init = function()
+    -- NOTE: do not enable `VM_leader`
+    -- vim.g.VM_leader = " "
+
+    -- all permanent mappings disabled except <C-n> (<C-n> can't be remapped)
+    vim.g.VM_default_mappings = 0
+
+    -- using noice.nvim or else it'll break multi cursor red change functionality
+    vim.g.VM_set_statusline = 1
+
+    -- or else "Press ENTER or type command to continue"
+    vim.g.VM_cmdheight = 2
+
+    -- disable warning
+    vim.g.VM_show_warnings = 0
+
+    -- don't display "Exited Visual-Multi."
+    vim.g.VM_silent_exit = 1
+  end,
+
   config = function()
-    -- create a column of cursors from visual mode
+    -- create a column of cursors
     vim.api.nvim_set_keymap(
       "n",
       "<C-S-n>",
@@ -9,57 +29,27 @@ return {
       { noremap = true, silent = true }
     )
 
-    vim.cmd([[
-    " all mappings disabled except <C-n> (<C-n> can't be remapped)
-    let g:VM_default_mappings = 0
-
-    " " using noice.nvim or else it'll break multi cursor red change functionality
-    let g:VM_set_statusline = 1
-
-    " disable warning
-    let g:VM_show_warnings = 0
-
-    " don't display "Exited Visual-Multi."
-     let g:VM_silent_exit = 1
-
-    " To change any mapping you must first initialize the variable:
-    let g:VM_maps = {}
-    let g:VM_maps['Find Under'] = "<C-n>"
-
-    " https://github.com/mg979/vim-visual-multi/issues/172
-    let g:VM_maps['I BS'] = "" " disable backspace mapping
-    let g:VM_maps['I CtrlC'] = "" " disable backspace mapping
-]])
-
-    -- Create an autocommand group for VM fixes
-    vim.api.nvim_create_augroup("VM_fixes", { clear = true })
-
-    local has_noice, noice = pcall(require, "noice")
-    -- Temporarily modify message display during VM operations
-    vim.api.nvim_create_autocmd("User", {
-      group = "VM_fixes",
-      pattern = "visual_multi_start",
-      callback = function()
-        -- Disable noice message capturing temporarily
-        if has_noice then
-          vim.opt.cmdheight = 1 -- Ensure there's space for VM messages
-          -- noice.disable("message")
-          vim.cmd("Noice disable")
-        end
-      end,
-    })
-
-    -- Restore settings when exiting VM
-    vim.api.nvim_create_autocmd("User", {
-      group = "VM_fixes",
-      pattern = "visual_multi_exit",
-      callback = function()
-        if has_noice then
-          vim.opt.cmdheight = 0 -- Restore original cmdheight
-          -- noice.enable("message")
-          vim.cmd("Noice enable")
-        end
-      end,
-    })
+    -- vim.api.nvim_create_augroup("VM_noice_fix", { clear = true })
+    --
+    -- -- Temporarily modify message display during VM operations
+    -- vim.api.nvim_create_autocmd("User", {
+    --   group = "VM_noice_fix",
+    --   pattern = "visual_multi_start",
+    --   callback = function()
+    --     -- Disable noice message capturing temporarily
+    --     -- noice.disable("message")
+    --     vim.cmd("Noice disable")
+    --   end,
+    -- })
+    --
+    -- -- Restore settings when exiting VM
+    -- vim.api.nvim_create_autocmd("User", {
+    --   group = "VM_noice_fix",
+    --   pattern = "visual_multi_exit",
+    --   callback = function()
+    --     -- noice.enable("message")
+    --     vim.cmd("Noice enable")
+    --   end,
+    -- })
   end,
 }
