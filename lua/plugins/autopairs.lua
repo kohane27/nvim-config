f4d00e987f16c92c119219bf99a26d8d6f82bf7f
kohane27 <kohane27@protonmail.com>
2024-12-07 11:15:05 +0800

    fix: do not remap bs key for vim-visual-multi
    generally it's annoying anyway

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -2,7 +2,8 @@ return {
   "windwp/nvim-autopairs",
   event = "InsertEnter",
   opts = {
+    map_bs = false,
     map_c_h = true, -- map <C-h> key to delete a pair
-    map_c_w = true, -- map <C-w> to delete a pair
+    map_c_w = false, -- map <C-w> to delete a pair
   },
 }
