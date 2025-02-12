4a24ada6aa35d81033b2688a520a29dbccadd0e3
kohane27 <kohane27@protonmail.com>
2025-02-12 10:16:17 +0800

    chore: remove emoji and copy-tree
    Former-commit-id: 108cc136d7fff856d910b29f47887df3471d1501
    Former-commit-id: de74365ca127b00f1728eedf322b3d561906f84a

--- lua/plugins/copy-tree.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "AdiY00/copy-tree.nvim",
-  cmd = { "CopyTree", "SaveTree" },
-  opts = {
-    additional_extensions = { "%.tf$" },
-    exclude_extensions = { "%.md$", "%.json$", "%.min.js$" },
-    ignore_dirs = { "^%.", "node_modules", "dist", "undo", "__pycache__" },
-  },
-}
