d99aad27b51f9ac857f7278479ce53980acc3bce
kohane27 <kohane27@protonmail.com>
2025-11-06 11:45:44 +0800

    chore: update codecompanion.nvim
    Former-commit-id: 3da448eab64c91bbf3f85707055d362d0519d7bc
    Former-commit-id: cce2cca58a589d93d9c302ec3992112e6c93cf17

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -3,44 +3,53 @@ return {
   dependencies = { "stevearc/dressing.nvim" },
   config = function()
     require("codecompanion").setup({
-      display = {
+      strategies = {
         chat = {
-          show_header_separator = false,
-          show_references = true,
-          show_settings = true,
-          show_token_count = false,
+          adapter = "openrouter",
+          keymaps = {
+            send = { modes = { n = "<C-s>" } },
+            clear = { modes = { n = "gXX" } },
+          },
         },
       },
-      strategies = { chat = { adapter = "openrouter" } },
-      opts = { log_level = "TRACE" },
+      opts = { log_level = "ERROR", send_code = true },
       adapters = {
-        opts = {
-          allow_insecure = false,
-          proxy = "http://127.0.0.1:8888",
-        },
-        openrouter = function()
-          return require("codecompanion.adapters").extend("openai_compatible", {
-            env = {
-              url = "https://openrouter.ai/api",
-              api_key = "OPENROUTER_API_KEY",
-              chat_url = "/v1/chat/completions",
-            },
-            schema = {
-              model = {
-                -- default = "liquid/lfm-3b",
-                default = "deepseek/deepseek-r1",
+        http = {
+          opts = { show_defaults = false, proxy = "http://127.0.0.1:8888" },
+          openrouter = function()
+            return require("codecompanion.adapters").extend("openai_compatible", {
+              env = {
+                url = "https://openrouter.ai/api",
+                api_key = "OPENROUTER_API_KEY",
+                chat_url = "/v1/chat/completions",
               },
-            },
-          })
-        end,
+              schema = { model = { default = "openai/gpt-5" } },
+            })
+          end,
+        },
       },
-      keymaps = {
-        ["<C-s>"] = "keymaps.save", -- Save the chat buffer and trigger the LLM
-        ["<C-x>"] = "keymaps.cancel_request",
-        ["gc"] = "keymaps.clear", -- Clear the contents of the chat
-        ["ga"] = "keymaps.codeblock", -- Insert a codeblock into the chat
-        ["gs"] = "keymaps.save_chat", -- Save the current chat
-        ["gt"] = "keymaps.add_tool", -- Add a tool to the current chat buffer
+      display = {
+        action_palette = {
+          provider = "telescope",
+          opts = {
+            show_default_actions = true,
+            show_default_prompt_library = true,
+          },
+        },
+        chat = {
+          intro_message = "",
+          show_settings = false,
+          auto_scroll = false,
+          window = {
+            opts = {
+              cursorline = true,
+            },
+          },
+        },
+        diff = {
+          enabled = true,
+          close_chat_at = 0, -- never close chat buffer
+        },
       },
     })
   end,
