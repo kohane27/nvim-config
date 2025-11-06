9e699cd8ee7f6b904c821c241e29c8005d7780f1
kohane27 <kohane27@protonmail.com>
2025-11-06 11:47:22 +0800

    fix: update diagnostics
    Former-commit-id: 196b7b1c417854fa8a7e47872d6f26af6317c204
    Former-commit-id: f4f1feef42c6ea889c0b6f712f3c38c5975b4954

--- lua/core/diagnostics.lua
+++ lua/core/diagnostics.lua
@@ -1,23 +1,20 @@
--- adding signs to left gitter
-local signs = {
-  { name = "DiagnosticSignError", text = "" },
-  { name = "DiagnosticSignWarn", text = "" },
-  { name = "DiagnosticSignHint", text = "󰌵" },
-  { name = "DiagnosticSignInfo", text = "" },
-}
-
-for _, sign in ipairs(signs) do
-  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-end
-
 -- Diagnostic options
 vim.diagnostic.config({
-  -- disable virtual text
-  virtual_text = false,
-  -- show signs
   signs = {
-    active = signs,
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.INFO] = "󰋼 ",
+      [vim.diagnostic.severity.HINT] = "󰌵 ",
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.HINT] = "",
+      [vim.diagnostic.severity.INFO] = "",
+    },
   },
+  virtual_text = false,
   update_in_insert = false,
   underline = true,
   severity_sort = true,
